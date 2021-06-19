51ff8ec994b2f345788c7235d2c400a3c8ead03f
anna-wro <wro.online@gmail.com>
2021-06-19 23:24:34 +0200

    test if github action runs

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -8,7 +8,7 @@ export default function Box({ href, headline, content }: PropsType) {
   return (
     <a href={href} className="box" data-testid="box">
       <h3 className="text-2xl font-bold">{headline}</h3>
-      <p className="mt-4 text-xl">{content}</p>
+      <p className="mt-4 text-xl">{content}</p>fsdfsdfsd
     </a>
   );
 }
