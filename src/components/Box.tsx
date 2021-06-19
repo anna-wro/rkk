dee8c80678cd1c2ac536c8cfee0feb9d22a14660
anna-wro <wro.online@gmail.com>
2021-06-19 23:17:07 +0200

    setup react testing library

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -6,7 +6,7 @@ type PropsType = {
 
 export default function Box({ href, headline, content }: PropsType) {
   return (
-    <a href={href} className="box">
+    <a href={href} className="box" data-testid="box">
       <h3 className="text-2xl font-bold">{headline}</h3>
       <p className="mt-4 text-xl">{content}</p>
     </a>
