79473eade55df6382300847a45d0ca24d8dbdf50
Anna Wróbel <anna-wro@users.noreply.github.com>
2021-06-20 09:38:10 +0200

    update color scheme (#3)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
+          <a className="text-red-600" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
