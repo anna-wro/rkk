8424394cfdd7acdeadd9619eac086bf54214ae3b
Anna Wróbel <wro.online@gmail.com>
2021-08-11 20:46:57 +0200

    fix displaying list

--- src/components/Prayers/PrayersList.tsx
+++ src/components/Prayers/PrayersList.tsx
@@ -4,7 +4,7 @@ export default function PrayersList({ prayers }) {
   return (
     <div className="mt-8">
       {prayers.map((prayer, index) => (
-        <div key={index}>{prayer[0].data.text}</div>
+        <div key={index}>{prayer.blocks[0].data.text}</div>
       ))}
 
       {/* TODO: display prayer on a separate page */}
