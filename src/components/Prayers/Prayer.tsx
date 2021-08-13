f5b7014bb88c6778bd6b784aaea6fd4a96157abe
Anna Wróbel <wro.online@gmail.com>
2021-08-13 19:22:08 +0200

    remove list indent

--- src/components/Prayers/Prayer.tsx
+++ src/components/Prayers/Prayer.tsx
@@ -8,13 +8,15 @@ const Editor = dynamic(() => import('components/Editor/Editor'), {
 
 export default function Prayer({ prayer }) {
   return (
-    <div className="mt-8 cursor-default">
+    <div className="mt-8">
       <Link href="/" passHref>
         <div className="bg-gray-100 hover:bg-gray-200 w-40 m-auto rounded-md p-2 mt-4 mb-4 cursor-pointer">
           {copy.backToList}
         </div>
       </Link>
-      <Editor data={prayer} />
+      <div className="text-left cursor-default">
+        <Editor data={prayer} />
+      </div>
     </div>
   );
 }
