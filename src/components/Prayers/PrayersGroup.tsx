256a159e5f5243943fb86a6f22dc8cfe522a75a2
Anna Wróbel <wro.online@gmail.com>
2021-08-14 21:28:33 +0200

    spacing

--- src/components/Prayers/PrayersGroup.tsx
+++ src/components/Prayers/PrayersGroup.tsx
@@ -4,8 +4,8 @@ import { makeStartCase } from 'utils/text';
 export default function PrayersGroup({ prayers }) {
   const [groupName, prayersInGroup] = prayers;
   return (
-    <div>
-      {groupName}
+    <div className="mt-2">
+      {makeStartCase(groupName)}
       {prayersInGroup.map(prayer => (
         <Link key={prayer.ID} href={prayer.slug} passHref>
           <div className="font-light hover:underline cursor-pointer">
