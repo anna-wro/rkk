a197ce59a5571b128601f36df38888770929658c
Anna Wróbel <wro.online@gmail.com>
2021-08-14 23:08:57 +0200

    nbsp

--- src/copy.ts
+++ src/copy.ts
@@ -1,6 +1,6 @@
 export const copy = {
   title: 'Liturgia Godzin',
-  subtitle: 'Reformowany Kościół Katolicki w Polsce',
+  subtitle: 'Reformowany Kościół Katolicki w\u00A0Polsce',
   backToList: 'wróć do listy',
   firstWeekTitle: 'Tydzień pierwszy',
   secondWeekTitle: 'Tydzień drugi',
