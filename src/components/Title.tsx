ca628433afb9e2a246d92f894ba95d7f30d806ea
Anna Wróbel <wro.online@gmail.com>
2021-08-15 18:12:51 +0200

    headling on mobile

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -9,7 +9,7 @@ export default function Title() {
       <div className="md:mt-10 mb-2 md:mb-4 flex mx-auto cursor-pointer">
         <Image src={logo} alt={copy.logoAlt} width="100" height="100" />
         <div className="ml-2">
-          <h1 className="md:text-4xl font-bold mt-4 hover:text-gray-700 dark:hover:text-red">
+          <h1 className="text-lg md:text-4xl font-bold mt-2 md:mt-4 hover:text-gray-700 dark:hover:text-red">
             {copy.title}
           </h1>
           <h2 className="text-sm md:text-md text-gray-600 hover:text-gray-800 dark:text-white  dark:hover:text-red">
