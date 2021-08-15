a83b02673a1e0529ee57ea6e028da7f9ddd8ff64
Anna Wróbel <wro.online@gmail.com>
2021-08-15 09:46:00 +0200

    add link to main page

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,15 +1,18 @@
 import copy from 'copy';
 import Image from 'next/image';
+import Link from 'next/link';
 import logo from '../../public/logo.svg';
 
 export default function Title() {
   return (
-    <div className="md:mt-10 mb-2 md:mb-6 flex mx-auto">
-      <Image src={logo} alt={copy.logoAlt} width="100" height="100" />
-      <div className="ml-2">
-        <h1 className="md:text-4xl font-bold mt-4">{copy.title}</h1>
-        <h2 className="text-sm md:text-md text-gray-600">{copy.subtitle}</h2>
+    <Link href="/" passHref>
+      <div className="md:mt-10 mb-2 md:mb-6 flex mx-auto cursor-pointer">
+        <Image src={logo} alt={copy.logoAlt} width="100" height="100" />
+        <div className="ml-2">
+          <h1 className="md:text-4xl font-bold mt-4">{copy.title}</h1>
+          <h2 className="text-sm md:text-md text-gray-600">{copy.subtitle}</h2>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
