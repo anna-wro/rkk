c612835b38f7ea5f58e45bae43cf15550f5b4ef2
Anna Wróbel <wro.online@gmail.com>
2021-08-18 12:37:30 +0200

    refactor

--- src/components/prayers/Psalm.tsx
+++ src/components/prayers/Psalm.tsx
@@ -10,16 +10,14 @@ type PsalmType = Readonly<{
 
 export const Psalm = ({ order, name, antiphon, content }: PsalmType) => {
   return (
-    <div className={cx('mb-2')}>
-      <PsalmWrapper order={order} name={name} antiphon={antiphon}>
-        <div className="space-y-1">
-          {content.map((versicles, index) => (
-            <div key={index} className={index % 2 === 0 ? 'pl-7' : null}>
-              {versicles}
-            </div>
-          ))}
-        </div>
-      </PsalmWrapper>
-    </div>
+    <PsalmWrapper order={order} name={name} antiphon={antiphon}>
+      <div className="space-y-1">
+        {content.map((versicles, index) => (
+          <div key={index} className={index % 2 === 0 ? 'pl-7' : null}>
+            {versicles}
+          </div>
+        ))}
+      </div>
+    </PsalmWrapper>
   );
 };
