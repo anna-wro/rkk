ad626ff6b6691670e18f58b0fecccc57931502ca
Anna Wróbel <wro.online@gmail.com>
2021-08-18 13:59:39 +0200

    bring padding back

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="px-4 md:px-10 md:pt-10 mb-6 md:mb-10 max-w-2xl mx-auto">
+      <main className="px-4 md:px-20 md:pt-10 mb-6 md:mb-10 max-w-2xl mx-auto">
         <div className="mb-2 md:mb-8">
           <Title />
         </div>
