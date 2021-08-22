73ba0e049e13f6772c9713c51a2ebed55b394766
Anna Wróbel <wro.online@gmail.com>
2021-08-22 23:09:30 +0200

    adjust title position

--- src/components/layout/Title.tsx
+++ src/components/layout/Title.tsx
@@ -6,10 +6,10 @@ import logo from '../../../public/logo.svg';
 export default function Title() {
   return (
     <Link href="/" passHref>
-      <div className="flex mx-auto cursor-pointer">
+      <div className="flex items-center cursor-pointer">
         <Image src={logo} alt={copy.logoAlt} width="100" height="100" />
         <div className="ml-2">
-          <h1 className="text-lg md:text-4xl font-bold mt-2 md:mt-4 hover:text-gray-700 dark:hover:text-red">
+          <h1 className="text-2xl md:text-4xl font-bold hover:text-gray-700 dark:hover:text-red">
             {copy.title}
           </h1>
           <h2 className="text-sm md:text-md text-gray-600 hover:text-gray-800 dark:text-white  dark:hover:text-red">
