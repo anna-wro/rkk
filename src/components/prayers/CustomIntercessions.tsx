e42b5486222bec84b548abd13e2e74619fc177c4
Anna Wróbel <wro.online@gmail.com>
2021-08-23 18:15:36 +0200

    Custom intercession text

--- src/components/prayers/CustomIntercessions.tsx
+++ src/components/prayers/CustomIntercessions.tsx
@@ -8,7 +8,11 @@ export const CustomIntercessions = ({ type }: PropsType) => {
       <div className="mt-4 font-bold">[miejsce na Twoją prośbę]</div>
       {type == 'lauds' && (
         <div className="mt-2">
-          Marek help please with our standard morning intercession
+          Ty wzbudzasz w nas chęci oraz popychasz do działania -{' '}
+          <b>
+            spraw, aby chciało się nam tak, jak nam się nie chce i umocnij w nas
+            motywację do działania.
+          </b>
         </div>
       )}
     </>
