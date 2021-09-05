cfcb350d0435a7f4bab8b558acc29770e7b157fc
Anna Wróbel <wro.online@gmail.com>
2021-09-05 22:07:11 +0200

    Refactor components structure

--- src/components/layout/PrayersList.tsx
+++ src/components/layout/PrayersList.tsx
@@ -1,10 +1,7 @@
-import cx from 'classnames';
-import PrayersGroup from './PrayersGroup';
-import { groupByKey } from 'utils/array';
 import { copy } from 'consts/copy';
 import { getCurrentWeekNumber } from 'utils/date';
 import type { PrayerType } from 'components/layout/PrayerPage';
-import CurrentWeekDot from 'components/layout/CurrentWeekDot';
+import PrayersInWeek from 'components/layout/PrayersInWeek';
 
 type PropsType = Readonly<{
   prayers: PrayerType[];
@@ -13,35 +10,23 @@ type PropsType = Readonly<{
 export default function PrayersList({ prayers }: PropsType) {
   const currentWeek = getCurrentWeekNumber();
   const firstWeek = prayers.filter(prayer => prayer.week === '1');
-  const firstWeekGroupedByDay = groupByKey(firstWeek, 'day');
   const secondWeek = prayers.filter(prayer => prayer.week === '2');
-  const secondWeekGroupedByDay = groupByKey(secondWeek, 'day');
 
   return (
     <div className="flex justify-center flex-col md:flex-row md:space-x-24">
       <div>
-        <div
-          className={cx('font-semibold text-lg my-4', {
-            'text-gray-500': currentWeek !== '1',
-          })}
-        >
-          {copy.firstWeekTitle} {currentWeek === '1' && <CurrentWeekDot />}
-        </div>
-        {Object.entries(firstWeekGroupedByDay).map((prayersFromDay, index) => (
-          <PrayersGroup key={index} prayers={prayersFromDay} />
-        ))}
+        <PrayersInWeek
+          prayers={firstWeek}
+          isCurrentWeek={currentWeek === '1'}
+          title={copy.firstWeekTitle}
+        />
       </div>
       <div className="mt-10 md:m-0">
-        <div
-          className={cx('font-semibold text-lg my-4', {
-            'text-gray-500': currentWeek !== '2',
-          })}
-        >
-          {copy.secondWeekTitle} {currentWeek === '2' && <CurrentWeekDot />}
-        </div>
-        {Object.entries(secondWeekGroupedByDay).map((prayersFromDay, index) => (
-          <PrayersGroup key={index} prayers={prayersFromDay} />
-        ))}
+        <PrayersInWeek
+          prayers={secondWeek}
+          isCurrentWeek={currentWeek === '2'}
+          title={copy.secondWeekTitle}
+        />
       </div>
     </div>
   );
