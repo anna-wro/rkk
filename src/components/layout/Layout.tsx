ce7707506d0f881063570cb5cb84eb93d7db2cad
Anna Wróbel <wro.online@gmail.com>
2021-09-07 22:23:57 +0200

    smaller vertical padding

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="px-8 md:px-20 md:pt-10 mb-6 md:mb-10 max-w-2xl mx-auto">
+      <main className="px-6 md:px-20 md:pt-10 mb-6 md:mb-10 max-w-2xl mx-auto">
         <div className="mb-3 md:mb-8">
           <Title />
         </div>
