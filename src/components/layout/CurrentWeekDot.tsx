391103f46234149b98ca44692a663ae3bf56e0fb
Anna Wróbel <wro.online@gmail.com>
2021-09-07 19:35:38 +0200

    smaller dot

--- src/components/layout/CurrentWeekDot.tsx
+++ src/components/layout/CurrentWeekDot.tsx
@@ -1,5 +1,5 @@
 export default function CurrentWeekDot() {
   return (
-    <span className="ml-2 w-4 h-4 bg-green-400 rounded-full inline-block" />
+    <span className="ml-2 w-3 h-3 bg-green-400 rounded-full inline-block" />
   );
 }
