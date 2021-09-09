13454e459c2b7e9cce2d70212b2c1bb45781158b
Anna Wróbel <wro.online@gmail.com>
2021-09-10 00:00:08 +0200

    disable blue higlight on tap

--- src/components/layout/InfoForToday.tsx
+++ src/components/layout/InfoForToday.tsx
@@ -17,7 +17,7 @@ export default function InfoForToday({ data }: { data: CalendarDataType }) {
   const [showMoreInfo, setShowMoreInfo] = useState(false);
 
   return (
-    <div className="font-light md:mb-4 mt-2 text-sm">
+    <div className="font-light md:mb-4 mt-2 text-sm more-info">
       <div
         className="md:flex justify-between items-center"
         onClick={handleClick}
