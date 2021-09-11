e2b07a1237600c9698f1c01be4bc4d5eb2840028
Anna Wróbel <wro.online@gmail.com>
2021-09-12 00:21:48 +0200

    PWA MVP

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,8 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#A1D348" />
         </Head>
         <body className="dark:bg-gray-900 dark:text-white/90 font-main selection:bg-green-200">
           <Main />
