4c2d87099b75c89ef3a4728242634269e6a4359c
Anna Wróbel <wro.online@gmail.com>
2021-09-11 21:23:56 +0200

    gray bg

--- src/components/layout/GoBackButton.tsx
+++ src/components/layout/GoBackButton.tsx
@@ -5,7 +5,7 @@ export default function GoBackButton() {
   return (
     <Link href="/" passHref>
       <div className="flex justify-end">
-        <button className="px-3 py-1 rounded-md text-sm font-medium text-white bg-orange hover:bg-green-600 focus:outline-none transition-colors dark:bg-green-600 dark:hover:bg-green-700 dark:text-white/90">
+        <button className="px-3 py-1 rounded-md text-sm font-medium text-white bg-gray hover:bg-green-600 focus:outline-none transition-colors dark:bg-green-600 dark:hover:bg-green-700 dark:text-white/90">
           {copy.backToList}
         </button>
       </div>
