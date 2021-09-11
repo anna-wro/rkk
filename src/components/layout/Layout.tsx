f441c2947322c5cd175cede581dd57027217e9d2
Anna Wróbel <wro.online@gmail.com>
2021-09-11 21:42:29 +0200

    test - yellow button

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,8 +6,6 @@ import copy from 'consts/copy';
 declare const window: any;
 
 export default function Layout({ children }) {
-  // This hook only run once in browser after the component is rendered for the first time.
-  // It has same effect as the old componentDidMount lifecycle callback.
   useEffect(() => {
     if (
       typeof window !== 'undefined' &&
@@ -15,60 +13,6 @@ export default function Layout({ children }) {
       window.workbox !== undefined
     ) {
       const wb = window.workbox;
-
-      // A common UX pattern for progressive web apps is to show a banner when a service worker has updated and waiting to install.
-      // NOTE: MUST set skipWaiting to false in next.config.js pwa object
-      // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
-      // const promptNewVersionAvailable = event => {
-      //   // eslint-disable-next-line no-console
-      //   console.log('prompt new version available', event);
-      //   // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
-      //   // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
-      //   // You may want to customize the UI prompt accordingly.
-      //   if (confirm('Dostępna jest aktualizacja. Czy chesz ją pobrać?')) {
-      //     wb.addEventListener('controlling', event => {
-      //       // eslint-disable-next-line no-console
-      //       console.log('controlling', event);
-      //       window.location.reload();
-      //     });
-
-      //     // Send a message to the waiting service worker, instructing it to activate.
-      //     wb.messageSkipWaiting();
-      //   } else {
-      //     // eslint-disable-next-line no-console
-      //     console.log(
-      //       'User rejected to reload the web app, keep using old version. New version will be automatically load when user open the app next time.',
-      //     );
-      //   }
-      // };
-
-      // wb.addEventListener('waiting', promptNewVersionAvailable);
-
-      // ISSUE - this is not working as expected, why?
-      // I could only make message event listenser work when I manually add this listenser into sw.js file
-      wb.addEventListener('message', event => {
-        // eslint-disable-next-line no-console
-        console.log(`Event ${event.type} is triggered.`);
-        // eslint-disable-next-line no-console
-        console.log(event);
-      });
-
-      /*
-      wb.addEventListener('redundant', event => {
-        console.log(`Event ${event.type} is triggered.`)
-        console.log(event)
-      })
-      wb.addEventListener('externalinstalled', event => {
-        console.log(`Event ${event.type} is triggered.`)
-        console.log(event)
-      })
-      wb.addEventListener('externalactivated', event => {
-        console.log(`Event ${event.type} is triggered.`)
-        console.log(event)
-      })
-      */
-
-      // never forget to call register as auto register is turned off in next.config.js
       wb.register();
     }
   }, []);
