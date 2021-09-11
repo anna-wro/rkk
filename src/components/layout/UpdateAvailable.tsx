393af09f31231c27243b4de0ee2b712b84f867bd
Anna Wróbel <wro.online@gmail.com>
2021-09-11 23:51:39 +0200

    force update

--- src/components/layout/UpdateAvailable.tsx
+++ src/components/layout/UpdateAvailable.tsx
@@ -2,7 +2,7 @@ import copy from 'consts/copy';
 
 export default function UpdateAvailable() {
   return (
-    <div className="md:hidden bg-yellow-400 px-8 pt-4 pb-5">
+    <div className="md:hidden bg-yellow-400 px-8 pt-4 pb-5 mb-2">
       <h3 className="mb-1"> {copy.updateAvailableHeader}</h3>
       <p className="text-sm font-light"> {copy.updateAvailableDescription}</p>
     </div>
