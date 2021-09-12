d0417ae98968b72dffa9c72e67d213437fff047b
Anna Wróbel <wro.online@gmail.com>
2021-09-12 12:09:55 +0200

    remove it completely

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,4 +1,3 @@
-// import 'regenerator-runtime/runtime';
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
@@ -6,13 +5,8 @@ import { serialize } from 'next-mdx-remote/serialize';
 import Prayer from 'components/layout/PrayerPage';
 import Layout from 'components/layout/Layout';
 import { PRAYERS_PATH, prayersFilePaths } from 'utils/mdxUtils';
-// import { useWakeLock } from 'use-wake-lock';
 
 export default function PrayerPage({ prayer }) {
-  // const { toggleWakeLock } = useWakeLock();
-
-  // toggleWakeLock();
-
   return (
     <Layout>
       <Prayer prayer={prayer} />
