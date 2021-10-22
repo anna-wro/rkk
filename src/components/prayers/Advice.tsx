d9872370aa54c9609e4e18597109f1f02924ce65
Anna Wróbel <wro.online@gmail.com>
2021-10-22 20:21:32 +0200

    styling

--- src/components/prayers/Advice.tsx
+++ src/components/prayers/Advice.tsx
@@ -1,3 +1,3 @@
 export const Advice = ({ children }: { children: React.ReactNode }) => {
-  return <p className="text-accent mb-2">{children}</p>;
+  return <p className="font-light text-accent mb-2">{children}</p>;
 };
--- src/components/prayers/Or.tsx
+++ src/components/prayers/Or.tsx
@@ -1,4 +1,4 @@
 import { copy } from 'consts/copy';
 export const Or = () => {
-  return <p className="text-accent my-2">{copy.or}</p>;
+  return <p className="font-light text-accent my-2">{copy.or}</p>;
 };
