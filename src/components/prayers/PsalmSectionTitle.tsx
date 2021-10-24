e82e9868466939243b04d66673e2d712a73b9694
Anna Wróbel <wro.online@gmail.com>
2021-10-24 19:28:58 +0200

    extract to component

---
+++ src/components/prayers/PsalmSectionTitle.tsx
@@ -0,0 +1,19 @@
+type PsalmSectionTitlePropsType = Readonly<{
+  title: string;
+  subtitle: string;
+  children: React.ReactNode;
+}>;
+
+export const PsalmSectionTitle = ({
+  title,
+  subtitle,
+  children,
+}: PsalmSectionTitlePropsType) => {
+  return (
+    <div className="font-light">
+      <p className="text-center">{title}</p>
+      <p className="text-center mb-2">{subtitle}</p>
+      <p className="mb-2">{children}</p>
+    </div>
+  );
+};
--- src/components/prayers/index.ts
+++ src/components/prayers/index.ts
@@ -18,3 +18,4 @@ export { V } from './V';
 export { R } from './R';
 export { Advice } from './Advice';
 export { Or } from './Or';
+export { PsalmSectionTitle } from './PsalmSectionTitle';
