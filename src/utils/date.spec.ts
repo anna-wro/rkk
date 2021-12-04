eff3e15eefee8abc59d812dd227b93bd47788d9b
Anna Wróbel <anna-wro@users.noreply.github.com>
2021-12-04 15:24:28 +0100

    📆 Get current week number - refactor (#92)

--- src/utils/date.spec.ts
+++ src/utils/date.spec.ts
@@ -1,31 +1,5 @@
-import { getCurrentWeekNumber, getCurrentDate, getCurrentSeason } from './date';
+import { getCurrentDate, getCurrentSeason } from './date';
 import { mockDate } from './mocks';
-
-describe('getCurrentWeekNumber()', () => {
-  it('returns proper values when week 2', () => {
-    mockDate(2021, 9, 7);
-
-    expect(getCurrentWeekNumber()).toEqual(2);
-  });
-
-  it('returns proper values when week 1', () => {
-    mockDate(2021, 9, 4);
-
-    expect(getCurrentWeekNumber()).toEqual(1);
-  });
-
-  it('returns proper values when Saturday evening', () => {
-    mockDate(2021, 9, 4, 18);
-
-    expect(getCurrentWeekNumber()).toEqual(2);
-  });
-
-  it('returns default value if incorrect date', () => {
-    mockDate(2045, 9, 4);
-
-    expect(getCurrentWeekNumber()).toEqual(0);
-  });
-});
 
 describe('getCurrentDate()', () => {
   it('returns proper value', () => {
