e05e7c8b011abf56e523432bc93728dbbade4b4a
Anna Wróbel <wro.online@gmail.com>
2021-12-11 18:43:39 +0100

    👑 Epiphany prayers

--- src/calendar/2022-01.ts
+++ src/calendar/2022-01.ts
@@ -23,4 +23,163 @@ export const January2022: CalendarType = [
       { name: 'nieszpory', slug: 'narodzenie-panskie-nieszpory' },
     ],
   },
+  {
+    date: '2022-01-05',
+    week: 1,
+    season: 'christmas',
+
+    links: [{ name: 'wigilia Epifanii', slug: 'epifania-wigilia' }],
+  },
+  {
+    date: '2022-01-06',
+    week: 1,
+    season: 'christmas',
+    lauds: {
+      sigla: 'Iz 52, 7-10',
+      passage:
+        'Jakże są pożądane kroki posłańca na szczytach górskich, który nam ogłasza pokój, zwiastuje szczęście, obwieszcza zbawienie, który mówi do Syjonu: „Twój Bóg zaczął królować”. Głos! Twoi strażnicy podnoszą głos, razem wznoszą okrzyki radosne, bo oglądają na własne oczy powrót Pana na Syjon. Zabrzmijcie radosnym śpiewem, wszystkie ruiny Jeruzalem! Bo Pan pocieszył swój lud, odkupił Jeruzalem. Pan obnażył już swe ramię święte wobec wszystkich narodów, i wszystkie krańce ziemi zobaczą zbawienie naszego Boga.',
+    },
+    vespers: {
+      sigla: 'Tt 3, 4-5',
+      passage:
+        'Ukazała się dobroć i miłość Zbawiciela, naszego Boga, do ludzi, nie dla uczynków sprawiedliwych, jakie zdziałaliśmy, lecz z miłosierdzia swego zbawił nas przez obmycie odradzające i odnawiające w Duchu Świętym.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+  },
+  {
+    date: '2022-01-07',
+    week: 1,
+    season: 'christmas',
+    lauds: {
+      sigla: 'Iz 9, 5',
+      passage:
+        'Dziecię nam się narodziło, Syn został nam dany, na Jego barkach spoczęła władza. Nazwano Go imieniem: Przedziwny Doradca, Bóg Mocny, Odwieczny Ojciec, Książę Pokoju.',
+    },
+    vespers: {
+      sigla: 'Mdr 7, 26-27',
+      passage:
+        'Mądrość jest odblaskiem wieczystej światłości, zwierciadłem bez skazy działania Boga, obrazem Jego dobroci. Jedna jest, a wszystko może, pozostając sobą, wszystko odnawia, a przez pokolenia zstępując w dusze święte, wzbudza przyjaciół Bożych i proroków.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+  },
+  {
+    date: '2022-01-08',
+    week: 1,
+    season: 'christmas',
+    lauds: {
+      sigla: 'Iz 60, 1-3',
+      passage:
+        'Powstań! Świeć, bo przyszło twe światło i chwała Pańska rozbłyska nad tobą. Bo oto ciemność okrywa ziemię i gęsty mrok spowija ludy, a ponad tobą jaśnieje Pan, i Jego chwała jawi się nad tobą. I pójdą narody do twojego światła, królowie do blasku twojego wschodu.',
+    },
+    vespers: {
+      sigla: 'Iz 60, 4-6',
+      passage:
+        'Podnieś oczy wokoło i popatrz: Ci wszyscy zebrani zdążają do ciebie. Twoi synowie przychodzą z daleka, na rękach niesione twe córki. Wtedy zobaczysz i promienieć będziesz, a serce twe zadrży i rozszerzy się, bo do ciebie napłyną bogactwa zamorskie, zasoby narodów przyjdą ku tobie. Zaleje cię mnogość wielbłądów – dromadery z Madianu i z Efy. Wszyscy oni przybędą ze Saby, przyniosą złoto i kadzidło i będą z radością głosić chwałę Pana.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+  },
+  {
+    date: '2022-01-09',
+    week: 2,
+    season: 'christmas',
+    lauds: {
+      sigla: '1 J 1, 5',
+      passage:
+        'Nowina, którą usłyszeliśmy od Niego i którą wam głosimy, jest taka: Bóg jest światłością, a nie ma w Nim żadnej ciemności.',
+    },
+    vespers: {
+      sigla: 'Kol 1, 13-15',
+      passage:
+        'Bóg nas uwolnił spod władzy ciemności i przeniósł do królestwa swego umiłowanego Syna, w którym mamy odkupienie, odpuszczenie grzechów. On jest obrazem Boga niewidzialnego, Pierworodnym wobec każdego stworzenia.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+  },
+  {
+    date: '2022-01-10',
+    week: 2,
+    season: 'christmas',
+    lauds: {
+      sigla: '1 J 5, 1',
+      passage:
+        'Każdy, kto wierzy, że Jezus jest Mesjaszem, z Boga się narodził, i każdy miłujący Tego, który dał życie, miłuje również tego, który życie od Niego otrzymał.',
+    },
+    vespers: {
+      sigla: 'Iz 2, 3b-4',
+      passage:
+        'Prawo wyjdzie z Syjonu i słowo Pańskie z Jeruzalem. On będzie rozjemcą pomiędzy ludami i wyda wyroki dla licznych narodów. Wtedy swe miecze przekują na lemiesze, a swoje włócznie na sierpy. Naród przeciw narodowi nie podniesie miecza, nie będą się więcej zaprawiać do wojny.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+  },
+  {
+    date: '2022-01-11',
+    week: 2,
+    season: 'christmas',
+    lauds: {
+      sigla: '1 J 4, 9',
+      passage:
+        'W tym objawiła się miłość Boga ku nam, że zesłał Syna swego Jednorodzonego na świat, abyśmy życie mieli dzięki Niemu.',
+    },
+    vespers: {
+      sigla: '1 J 4, 10',
+      passage:
+        'W tym przejawia się miłość, że nie my umiłowaliśmy Boga, ale że On sam nas umiłował i posłał Syna swojego jako ofiarę przebłagalną za nasze grzechy.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+  },
+  {
+    date: '2022-01-12',
+    week: 2,
+    season: 'christmas',
+    lauds: {
+      sigla: 'Rz 11, 33-36',
+      passage:
+        'O głębokości bogactw, mądrości i wiedzy Boga! Jakże niezbadane są Jego wyroki i nie do wyśledzenia Jego drogi! Kto bowiem poznał myśl Pana, albo kto był Jego doradcą? Lub kto Go pierwszy obdarował, aby nawzajem otrzymać odpłatę? Albowiem z Niego i przez Niego, i dla Niego jest wszystko. Jemu chwała na wieki! Amen.',
+    },
+    vespers: {
+      sigla: '1 Krn 29, 11-13',
+      passage:
+        'Twoja jest, o Panie wielkość, moc, sława, majestat i chwała, bo wszystko, co jest na niebie i na ziemi, jest Twoje; do Ciebie, Panie, należy królowanie i ten, co głowę wznosi ponad wszystkich. Bogactwo i chwała od Ciebie pochodzą, Ty nad wszystkim panujesz, a w ręku Twoim siła i moc, i ręką Twoją wywyższasz i utwierdzasz wszystko. Teraz więc, Boże nasz, dzięki Ci składamy i wychwalamy przesławne imię Twoje.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+  },
+  {
+    date: '2022-01-13',
+    week: 2,
+    season: 'christmas',
+    lauds: {
+      sigla: 'Iz 42, 1-4',
+      passage:
+        'Oto mój Sługa, którego podtrzymuję. Wybrany mój, w którym mam upodobanie. Sprawiłem, że Duch mój na Nim spoczął; On przyniesie narodom Prawo. Nie będzie wołał ni podnosił głosu, nie da słyszeć krzyku swego na dworze. Nie złamie trzciny nadłamanej, nie zagasi knotka o nikłym płomyku. On niezachwianie przyniesie Prawo. Nie zniechęci się ani nie załamie, aż utrwali Prawo na ziemi, a Jego pouczenia wyczekują wyspy.',
+    },
+    vespers: {
+      sigla: 'Iz 42, 6-7',
+      passage:
+        'Ja, Pan, powołałem Cię słusznie, ująłem Cię za rękę i ukształtowałem, ustanowiłem Cię przymierzem dla ludzi, światłością dla narodów, abyś otworzył oczy niewidomym, ażebyś z zamknięcia wypuścił jeńców, z więzienia tych, co mieszkają w ciemności.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+  },
 ];
