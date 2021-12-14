0949233a25a6fc8ece1e39d9271e6578bdbec229
Anna Wróbel <anna-wro@users.noreply.github.com>
2021-12-14 22:41:51 +0100

    👆🏻 Clickable Title - improvements (#114)

--- src/components/layout/Title.tsx
+++ src/components/layout/Title.tsx
@@ -5,18 +5,28 @@ import logo from '../../../public/logo.svg';
 
 export default function Title() {
   return (
-    <Link href="/" passHref>
-      <div className="flex items-center cursor-pointer">
-        <Image src={logo} alt={copy.logoAlt} width="100" height="100" />
-        <div className="ml-2">
-          <h1 className="text-2xl md:text-4xl font-bold hover:text-gray-700 dark:hover:text-red-400">
-            {copy.title}
-          </h1>
-          <h2 className="text-sm md:text-md text-gray-600 hover:text-gray-800 dark:text-white/90 dark:hover:text-red-400">
-            {copy.subtitle}
-          </h2>
-        </div>
+    <div className="flex items-center">
+      <Link href="/" passHref>
+        <Image
+          src={logo}
+          alt={copy.logoAlt}
+          width="100"
+          height="100"
+          className="cursor-pointer"
+        />
+      </Link>
+      <div className="ml-2">
+        <Link href="/" passHref>
+          <div className="cursor-pointer">
+            <h1 className="text-2xl md:text-4xl font-bold hover:text-gray-700 dark:hover:text-red-400">
+              {copy.title}
+            </h1>
+            <h2 className="text-sm md:text-md text-gray-600 hover:text-gray-800 dark:text-white/90 dark:hover:text-red-400">
+              {copy.subtitle}
+            </h2>
+          </div>
+        </Link>
       </div>
-    </Link>
+    </div>
   );
 }
