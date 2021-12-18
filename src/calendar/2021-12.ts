24c0aaba201ea3ce03cc6326aa322008a86f2c21
Anna Wróbel <anna-wro@users.noreply.github.com>
2021-12-18 21:41:47 +0100

    Revert "Update 2021-12.ts (#120)"
    This reverts commit eda1978e93be07d8436f1df396c1aa7ebf231e50.

--- src/calendar/2021-12.ts
+++ src/calendar/2021-12.ts
@@ -149,9 +149,7 @@ export const December2021: CalendarType = [
     notes: ['[HTML]Fryderyka von Schulte[HTML] w. i teo.'],
     vespers: {
       antiphon:
-               'O Adonai, Wodzu Izraela, / Tyś w krzaku gorejącym objawił się Mojżeszowi / i na Synaju dałeś mu Prawo; / przyjdź nas wyzwolić swym potężnym ramieniem.',
-
-
+        'O Korzeniu Jessego, / który się wznosisz jako znak dla narodów, przed Tobą zamilkną królowie, a ludy modlić się będą do Ciebie; / przyjdź nas wyzwolić, już dłużej nie zwlekaj.',
     },
   },
   {
