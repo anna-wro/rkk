341e9acec77465698db930bc0f4641ff6f349a9e
Anna Wróbel <anna-wro@users.noreply.github.com>
2021-12-21 19:29:55 +0100

    🌱 Change font size - draft (#123)

--- src/components/settings/SettingsPage.tsx
+++ src/components/settings/SettingsPage.tsx
@@ -2,7 +2,42 @@ import React from 'react';
 import GoBackButton from 'components/layout/GoBackButton';
 import { Header } from 'components/mdxOverrides';
 import copy from 'consts/copy';
+
+const getItem = key => {
+  try {
+    return localStorage.getItem(key);
+  } catch {}
+};
+
+const setItem = (key, value) => {
+  try {
+    return localStorage.setItem(key, value);
+  } catch {}
+};
+
 export default function SettingsPage() {
+  const [fontSize, setFontSize] = React.useState(
+    () => getItem('fontSize') ?? 16,
+  );
+
+  React.useEffect(() => {
+    const root = document.getElementsByTagName('html')[0];
+    root.style.setProperty('font-size', `${fontSize}px`);
+  }, [fontSize]);
+
+  const buttonClass =
+    'flex items-center content-center text-white bg-green hover:bg-green-600 rounded-full p-2 w-6 h-6 cursor-pointer';
+  const handleFontDecrease = () => {
+    setFontSize(Number(fontSize) - 2);
+    setItem('fontSize', fontSize);
+  };
+  const handleFontIncrease = () => {
+    setFontSize(Number(fontSize) + 2);
+    setItem('fontSize', fontSize);
+  };
+  // TODO read it on page load
+  // TODO Format buttons
+  // TODO Reset settings
   return (
     <>
       <div className="relative whitespace-pre-line">
@@ -12,8 +47,18 @@ export default function SettingsPage() {
         <Header>{copy.settings.headline}</Header>
       </div>
       <div className="section">
-        {/* <div onClick={handleFontSizeChange}>Sample text</div> */}
-        {/* <GoBackButton /> */}
+        <div className="flex items-center content-center space-x-2">
+          <div className="flex items-center content-center bg-gray-50 bg-opacity-60 p-4 rounded-lg">
+            <div>Rozmiar tekstu: {fontSize}px</div>
+          </div>
+          <button className={buttonClass} onClick={handleFontDecrease}>
+            -
+          </button>
+          <button className={buttonClass} onClick={handleFontIncrease}>
+            +
+          </button>
+        </div>
+        <GoBackButton />
       </div>
     </>
   );
