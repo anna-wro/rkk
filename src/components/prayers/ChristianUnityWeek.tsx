b34a052d557ad1382ac8238db94c404be017b5f9
Anna Wróbel <wro.online@gmail.com>
2022-01-17 22:07:43 +0100

    🌎 Week of Prayer for Christian Unity

---
+++ src/components/prayers/ChristianUnityWeek.tsx
@@ -0,0 +1,16 @@
+import { R, V } from './index';
+
+export const ChristianUnityWeek = () => (
+  <div className="mt-4">
+    <div className="mb-2 font-light text-center">
+      Modlitwa Wspólnoty Chemin Neuf o Jedność Chrześcijan
+    </div>
+    <V>
+      {`Panie Jezu, który modliłeś się, aby wszyscy byli jedno, prosimy Cię o
+      jedność chrześcijan taką, jakiej Ty chcesz, poprzez środki, jakie Ty
+      wybierasz. 
+      Niech Twój Duch Święty da nam doświadczyć cierpienia podziałów, zobaczyć nasz grzech i mieć nadzieję ponad wszelką nadzieję. `}
+    </V>
+    <R>Amen.</R>
+  </div>
+);
