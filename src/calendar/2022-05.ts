05e5ea44fa0f9e7d518cb2fb40878afa6782903f
Anna Wróbel <anna-wro@users.noreply.github.com>
2022-02-12 18:02:26 +0100

    📆 April 2022 (#150)

---
+++ src/calendar/2022-05.ts
@@ -0,0 +1,12 @@
+import type { CalendarType } from './calendar';
+
+export const May2022: CalendarType = [
+  {
+    date: '2022-05-',
+    week: 2,
+    season: 'ordinary',
+    holidays: [],
+    passages: [],
+    notes: [],
+  },
+];
