034ad72b18b29459bdfccce57e9c0426add6d512
Marek M. Mazur <marek90@gmail.com>
2022-02-23 23:02:02 +0100

    Update PrayerForPeace.tsx
    Improved some things.

--- src/components/prayers/PrayerForPeace.tsx
+++ src/components/prayers/PrayerForPeace.tsx
@@ -4,7 +4,7 @@ export const PrayerForPeace = () => {
   return (
     <div className="mt-5 space-y-2">
       <p>
-        Wszechmogący Boże, powierzamy Tobie bezpieczeństwo tych, którzy żyją w
+        <b>Wszechmogący Boże,</b> powierzamy Tobie bezpieczeństwo tych, którzy żyją w
         sąsiedztwie wojny i konfliktów, oraz zagrożonych ich rozpoczęciem.
         Pamiętajmy w tym szczególnym momencie o ludziach z Europy Wschodniej,
         Ukrainy i Rosji. Modlimy się o pokojowe rozwiązanie tego konfliktu.
@@ -12,15 +12,17 @@ export const PrayerForPeace = () => {
       <p>
         Natchnij nasze serca do gościnności i opieki, a przebacz naszą wrogość i
         nienawiść. Doprowadź wszystkich do jedności, którą nam dałeś, do
-        pojednania i uzdrowienia, za które oddałeś życie swojego Syna. Umocnij
-        nas abyśmy wszyscy razem z Tobą dążyli do budowania sprawiedliwości,
-        pokoju, pojednania i uzdrowienia w naszych sercach i domach, na ulicach,
-        społeczeństwach, sąsiedztwach i nacjach. Obdarz swoim błogosławieństwem
-        wszystkich tych, którzy poświęcają swoje życie dla pokoju i pomocy
+        pojednania i uzdrowienia, za które oddałeś życie swojego umiłowanego Syna.
+      </p>
+      <p>
+        Umocnij nas abyśmy wszyscy razem z Tobą dążyli do budowania sprawiedliwości,
+        pokoju, pojednania i uzdrowienia w naszych sercach, domach, sąsiedztwach, ulicach,
+        społeczeństwach i narodach. Obdarz swoim błogosławieństwem
+        wszystkich tych, którzy poświęcają swoje życie misji dla pokoju i pomocy
         innym, aby ich starania były owocne.
       </p>
       <p>
-        Przez Jezusa Chrystusa, Twojego Syna, który z Tobą żyje i króluje na
+        Przez naszego Pana Jezusa Chrystusa, Twojego Syna, który z Tobą żyje i króluje na
         wieki wieków.
       </p>
       <R>Amen.</R>
