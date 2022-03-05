db6a00a45ee16659ed53d5c16203166bb0cbb4e2
Anna Wróbel <anna-wro@users.noreply.github.com>
2022-03-05 18:32:34 +0100

    ☀️ Hide Alleluia during Lent (#164)

--- src/components/prayers/OpeningVersicle.tsx
+++ src/components/prayers/OpeningVersicle.tsx
@@ -1,13 +1,17 @@
 import { V, R } from './index';
+import { getCalendarData } from 'utils/date';
 
 export const OpeningVersicle = () => {
+  const calendar = getCalendarData();
+  const displayAlleluia = calendar?.season !== 'lent';
+
   return (
     <>
       <V>† Boże, wejrzyj ku wspomożeniu memu.</V>
       <R>{`Panie, pośpiesz ku ratunkowi memu.
         Chwała Ojcu i Synowi, i Duchowi Świętemu.
         Jak była na początku, teraz i zawsze,
-        i na wieki wieków. Amen. Alleluja.`}</R>
+        i na wieki wieków. Amen. ${displayAlleluia ? 'Alleluja.' : ''}`}</R>
     </>
   );
 };
