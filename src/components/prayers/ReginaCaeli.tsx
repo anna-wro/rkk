fa02386d8b8cd982e2060bee57f5bcfb51c1bd23
Anna Wróbel <wro.online@gmail.com>
2022-04-23 18:23:14 +0200

    🌤 Regina Caeli

---
+++ src/components/prayers/ReginaCaeli.tsx
@@ -0,0 +1,15 @@
+import { Section } from 'components/mdxOverrides';
+
+export const ReginaCaeli = () => {
+  return (
+    <>
+      <Section>Regina Caeli</Section>
+      <div className="italic font-light">
+        {`Królowo nieba, wesel się, alleluja,
+Bo Ten, któregoś nosiła, alleluja,
+Zmartwychwstał, jak powiedział, alleluja,
+Módl się za nami do Boga, alleluja.`}
+      </div>
+    </>
+  );
+};
