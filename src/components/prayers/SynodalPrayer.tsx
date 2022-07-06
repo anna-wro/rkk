14d55190a25fe1f0bd6e2bcf0d8c1c50ac3e6706
Anna Wróbel <wro.online@gmail.com>
2022-07-06 17:09:59 +0200

    🐞 July & August readings

--- src/components/prayers/SynodalPrayer.tsx
+++ src/components/prayers/SynodalPrayer.tsx
@@ -15,9 +15,9 @@ export const SynodalPrayer = () => {
       </p>
       <p>
         Ty jesteś Tym, który ożywia i oczyszcza. Ty jesteś Prawdą i Mądrością.
-        Jako Twoi uczniowie i uczennice, prosimy, uczyń nas gotowymi do słuchania
-        Nauki. Pouczaj nas, Twój Kościół, abyśmy rozpoznali drogę, na którą nas
-        posyłasz i żyli życiem, do którego nas wzywasz.
+        Jako Twoi uczniowie i uczennice, prosimy, uczyń nas gotowymi do
+        słuchania Nauki. Pouczaj nas, Twój Kościół, abyśmy rozpoznali drogę, na
+        którą nas posyłasz i żyli życiem, do którego nas wzywasz.
       </p>
       <p>
         Ty, który mówiłeś przez proroków, a dziś przemawiasz przez Twój Kościół,
