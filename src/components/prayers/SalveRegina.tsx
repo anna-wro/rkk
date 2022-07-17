1858d145c83cefba2887fb4dfe30d763621ba2ea
Anna Wróbel <wro.online@gmail.com>
2022-07-17 20:43:27 +0200

    🇵🇱 Salve Regina - PL lyrics

--- src/components/prayers/SalveRegina.tsx
+++ src/components/prayers/SalveRegina.tsx
@@ -1,4 +1,5 @@
 import { Section } from 'components/mdxOverrides';
+import { Or } from './Or';
 
 export const SalveRegina = () => {
   return (
@@ -15,6 +16,23 @@ misericordes oculos ad nos converte;
 Et Jesum, benedictum fructum ventris tui,
 nobis post hoc exsilium ostende.
 O clemens, O pia, O dulcis Virgo Maria.`}
+      </div>
+      <Or />
+      <div className="italic font-light">
+        {`Witaj Królowo Matko Miłosierdzia
+        Życie słodyczy i nadziejo nasza witaj
+        Do Ciebie wołamy
+        Wygnańcy synowie Ewy
+        Do Ciebie wzdychamy jęcząc i płacząc
+        Na tym łez padole
+        Przeto Orędowniczko nasza
+        One miłosierne oczy Twoje na nas zwróć
+        A Jezusa błogosławiony owoc żywota Twojego
+        Po tym wygnaniu nam okaż
+        O łaskawa
+        O litościwa
+        O słodka
+        Panno Maryjo`}
       </div>
     </>
   );
