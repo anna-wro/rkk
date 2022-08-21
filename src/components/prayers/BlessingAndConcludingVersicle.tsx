6267a82d2931065dd00c2e9b131157143f58340d
Anna Wróbel <anna-wro@users.noreply.github.com>
2022-08-21 13:08:09 +0200

    Remove Synodal prayer

--- src/components/prayers/BlessingAndConcludingVersicle.tsx
+++ src/components/prayers/BlessingAndConcludingVersicle.tsx
@@ -31,7 +31,6 @@ export const BlessingAndConcludingVersicle = ({ withAlleluia }: PropsType) => {
           withAlleluia ? ' Alleluja.' : ''
         }`}
       </R>
-      <SynodalPrayer />
     </div>
   );
 };
