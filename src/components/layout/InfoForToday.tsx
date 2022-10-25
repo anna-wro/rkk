b5114411ac8453ca858f69f8277d568bc84cc246
Anna Wróbel <anna-wro@users.noreply.github.com>
2022-10-25 14:24:08 +0200

    Update InfoForToday.tsx

--- src/components/layout/InfoForToday.tsx
+++ src/components/layout/InfoForToday.tsx
@@ -23,9 +23,7 @@ export default function InfoForToday({ data }: { data: CalendarDataType }) {
     <div className="font-light md:mb-4 mt-3 text-sm more-info">
       <div className="md:flex justify-between items-center">
         <div>
-          <div className="font-medium">
-            {makeStartCase(data.prettyDate)}
-          </div>
+          <div className="font-medium">{makeStartCase(data.prettyDate)}</div>
           {data?.holidays?.map(holiday => (
             <div className="font-bold" key={holiday}>
               {holiday.toUpperCase()}
