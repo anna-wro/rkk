121a133670d416e0c40a70c1fa3646eab263f81f
Anna Wróbel <wro.online@gmail.com>
2022-11-25 13:44:58 +0100

    Add cors lib

--- src/pages/api/config.ts
+++ src/pages/api/config.ts
@@ -1,5 +1,34 @@
 import { calendar } from 'calendar/calendar';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import Cors from 'cors';
 
-export default function handler(req, res) {
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ['GET'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, result => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
+  // Rest of the API logic
   res.status(200).json(calendar);
 }
