b2e9b36b7ab6eaa36c955171152e735bc2237cc3
Anna Wróbel <wro.online@gmail.com>
2023-02-01 08:05:08 +0100

    🐞 February2023

---
+++ src/calendar/2023-02.ts
@@ -0,0 +1,22 @@
+import type { CalendarType } from './calendar';
+
+export const February2023: CalendarType = [
+  {
+    date: '2023-02-01',
+    week: 2,
+    season: 'christmas',
+    passages: [],
+  },
+  {
+    date: '2023-02-02',
+    week: 2,
+    season: 'christmas',
+    passages: [],
+  },
+  {
+    date: '2023-02-03',
+    week: 2,
+    season: 'christmas',
+    passages: [],
+  },
+];
