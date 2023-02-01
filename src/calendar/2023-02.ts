147f1493caf04dd65ca028c56a9501c101255c65
Anna Wróbel <anna-wro@users.noreply.github.com>
2023-02-01 08:11:07 +0100

    Update 2023-02.ts

--- src/calendar/2023-02.ts
+++ src/calendar/2023-02.ts
@@ -3,19 +3,19 @@ import type { CalendarType } from './calendar';
 export const February2023: CalendarType = [
   {
     date: '2023-02-01',
-    week: 2,
+    week: 1,
     season: 'christmas',
     passages: [],
   },
   {
     date: '2023-02-02',
-    week: 2,
+    week: 1,
     season: 'christmas',
     passages: [],
   },
   {
     date: '2023-02-03',
-    week: 2,
+    week: 1,
     season: 'christmas',
     passages: [],
   },
