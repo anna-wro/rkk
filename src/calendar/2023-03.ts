06b8a542552f1fd475b278c44b55eb48e58b72dd
Anna Wróbel <wro.online@gmail.com>
2023-02-13 18:02:00 +0100

    📆 March 2023

---
+++ src/calendar/2023-03.ts
@@ -0,0 +1,263 @@
+import type { CalendarType } from './calendar';
+
+export const March2023: CalendarType = [
+  {
+    date: '2023-03-01',
+    week: 2,
+    season: 'ordinary',
+    passages: ['1 P 1, 10-16', 'Ps 98', 'Mk 10, 28-31'],
+  },
+  {
+    date: '2023-03-02',
+    week: 2,
+    season: 'lent',
+    passages: ['Jl 2, 12-18', 'Ps 51', '2 Kor 5, 20-6, 2', 'Mt 6, 1-6.16-18'],
+    holidays: ['ŚRODA POPIELCOWA'],
+  },
+  {
+    date: '2023-03-03',
+    week: 2,
+    season: 'lent',
+    passages: ['Pwt 30, 15-20', 'Ps 1', 'Łk 9, 22-25'],
+    notes: ['[HTML]Jana i Karola Wesleyów[HTML] prezb.'],
+  },
+  {
+    date: '2023-03-04',
+    week: 2,
+    season: 'lent',
+    passages: ['Iz 58, 1-9a', 'Ps 51', 'Mt 9, 14-15'],
+  },
+  {
+    date: '2023-03-05',
+    week: 2,
+    season: 'lent',
+    passages: ['Iz 58, 9b-14', 'Ps 86', 'Łk 5, 27-32'],
+    notes: ['[HTML]Gerazyma[HTML] mn.'],
+  },
+  {
+    date: '2023-03-06',
+    week: 1,
+    season: 'lent',
+    passages: ['Pwt 26, 4-10', 'Ps 91', 'Rz 10, 8-13', 'Łk 4, 1-13'],
+    holidays: ['1. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
+    lauds: {
+      antiphon:
+        'Duch wyprowadził Jezusa na pustynię, * aby kuszony był przez diabła. / A gdy przepościł czterdzieści dni i nocy, / głód wielki potem odczuwał.',
+    },
+    eve: {
+      antiphon:
+        'Nie samym chlebem żyje człowiek, * lecz każdym słowem, / które pochodzi z ust Bożych.',
+    },
+    vespers: {
+      antiphon:
+        'Czuwaj nad nami, o Zbawicielu, * by nas nie pochwycił przebiegły kusiciel; / Ty bowiem jesteś naszą odwieczną pomocą.',
+    },
+  },
+  {
+    date: '2023-03-07',
+    week: 1,
+    season: 'lent',
+    passages: ['Kpł 19, 1-2. 11-18', 'Ps 19', 'Mt 25, 31-46'],
+    notes: ['[HTML]Perpetuy i Felicyty[HTML] m.'],
+  },
+  {
+    date: '2023-03-08',
+    week: 1,
+    season: 'lent',
+    passages: ['Iz 55, 10-11', 'Ps 34', 'Mt 6, 7-15'],
+  },
+  {
+    date: '2023-03-09',
+    week: 1,
+    season: 'lent',
+    passages: ['Jon 3, 1-10', 'Ps 51', 'Łk 11, 29-32'],
+  },
+  {
+    date: '2023-03-10',
+    week: 1,
+    season: 'lent',
+    passages: ['Est 4, 17k-17m.17r-17u', 'Ps 138', 'Mt 7, 7-12'],
+  },
+  {
+    date: '2023-03-11',
+    week: 1,
+    season: 'lent',
+    passages: ['Ez 18, 21-28', 'Ps 130', 'Mt 5, 20-26'],
+    notes: ['[HTML]Matek i ojców monastycyzmu[HTML]'],
+  },
+  {
+    date: '2023-03-12',
+    week: 1,
+    season: 'lent',
+    passages: ['Pwt 26, 16-19', 'Ps 119', 'Mt 5, 43-48'],
+    notes: ['[HTML]Grzegorza Wielkiego[HTML] bp.'],
+  },
+  {
+    date: '2023-03-13',
+    week: 2,
+    season: 'lent',
+    passages: [
+      'Rdz 15, 5-12. 17-18',
+      'Ps 27',
+      'Flp 3, 17–4, 1',
+      'Łk 9, 28b-36',
+    ],
+    holidays: ['2. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
+    eve: {
+      antiphon:
+        'Odezwał się głos z obłoku: * To jest mój Syn umiłowany, / w którym mam upodobanie, / Jego słuchajcie.',
+    },
+    lauds: {
+      antiphon:
+        'Nasz Pan, Jezus Chrystus, śmierć zniweczył * i przez Ewangelię dał nam światło życia wiecznego.',
+    },
+    vespers: {
+      antiphon:
+        'Nie mówcie nikomu o tym widzeniu, * aż Syn Człowieczy zmartwychwstanie.',
+    },
+  },
+  {
+    date: '2023-03-14',
+    week: 2,
+    season: 'lent',
+    passages: ['Dn 9, 4b-10', 'Ps 78', 'Łk 6, 36-38'],
+  },
+  {
+    date: '2023-03-15',
+    week: 2,
+    season: 'lent',
+    passages: ['Iz 1, 10. 16-20', 'Ps 50', 'Mt 23, 1-12'],
+  },
+  {
+    date: '2023-03-16',
+    week: 2,
+    season: 'lent',
+    passages: ['Jr 18, 18-20', 'Ps 31', 'Mt 20, 17-28'],
+  },
+  {
+    date: '2023-03-17',
+    week: 2,
+    season: 'lent',
+    passages: ['Jr 17, 5-10', 'Ps 1', 'Łk 16, 19-31'],
+    notes: ['[HTML]Patryka[HTML] bp.'],
+  },
+  {
+    date: '2023-03-18',
+    week: 2,
+    season: 'lent',
+    passages: ['Rdz 37, 3-4. 12-13a. 17b-28a', 'Ps 105', 'Mt 21, 33-43. 45-46'],
+    notes: ['[HTML]Cyryla Jerozolimskiego[HTML] bp. teo.'],
+  },
+  {
+    date: '2023-03-19',
+    week: 2,
+    season: 'lent',
+    passages: [
+      '2 Sm 7, 4-5a. 12-14a. 16',
+      'Ps 89',
+      'Rz 4, 13. 16-18. 22',
+      'Mt 1, 16. 18-21. 24',
+    ],
+    holidays: ['JÓZEFA Z NAZARETU'],
+  },
+  {
+    date: '2023-03-20',
+    week: 1,
+    season: 'lent',
+    passages: ['Wj 17, 3-7', 'Ps 95', 'Rz 5, 1-2.5-8', 'J 4, 5-42'],
+    holidays: ['3. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
+    eve: {
+      antiphon:
+        'Usprawiedliwieni przez wiarę * żyjmy w pokoju z Bogiem / przez naszego Pana, Jezusa Chrystusa.',
+    },
+    lauds: {
+      antiphon:
+        'Pan mówi: * kto będzie pił wodę, którą Ja mu dam, / nie będzie pragnął na wieki.',
+    },
+    vespers: {
+      antiphon:
+        'Wierzymy i jesteśmy przekonani, * że Chrystus jest prawdziwie Zbawicielem świata.',
+    },
+  },
+  {
+    date: '2023-03-21',
+    week: 1,
+    season: 'lent',
+    passages: ['2 Krl 5, 1-15a', 'Ps 42', 'Łk 4, 24-30'],
+  },
+  {
+    date: '2023-03-22',
+    week: 1,
+    season: 'lent',
+    passages: ['Dn 3, 25. 34-43', 'Ps 25', 'Mt 18, 21-35'],
+  },
+  {
+    date: '2023-03-23',
+    week: 1,
+    season: 'lent',
+    passages: ['Pwt 4, 1. 5-9', 'Ps 147', 'Mt 5, 17-19'],
+  },
+  {
+    date: '2023-03-24',
+    week: 1,
+    season: 'lent',
+    passages: ['Jr 7, 23-28', 'Ps 95', 'Łk 11, 14-23'],
+  },
+  {
+    date: '2023-03-25',
+    week: 1,
+    season: 'lent',
+    passages: ['Iz 7, 10-14; 8, 10', 'Ps 40', 'Hbr 10, 4-10', 'Łk 1, 26-38'],
+    holidays: ['UROCZYSTOŚĆ ZWIASTOWANIA PAŃSKIEGO'],
+  },
+  {
+    date: '2023-03-26',
+    week: 1,
+    season: 'lent',
+    passages: ['Oz 6, 1-6', 'Ps 51', 'Łk 18, 9-14'],
+  },
+  {
+    date: '2023-03-27',
+    week: 2,
+    season: 'lent',
+    passages: ['1 Sm 16, 1b. 6-7.10-13a', 'Ps 23', 'Ef 5, 8-14', 'J 9, 1-41'],
+    holidays: ['4. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
+    eve: {
+      antiphon:
+        'Niegdyś byliście ciemnością, * lecz teraz jesteście światłością w Panu.',
+    },
+    lauds: {
+      antiphon:
+        'Panie, kto zawinił, * ten człowiek, czy jego rodzice, że się urodził niewidomy? / Jezus odpowiedział: Ani on nie zawinił, ani jego rodzice: / ale stało się tak, aby się na nim objawiły sprawy Boże.',
+    },
+    vespers: {
+      antiphon:
+        'Człowiek imieniem Jezus * pomazał mi oczy błotem, / i kazał mi się obmyć, i teraz widzę.',
+    },
+  },
+  {
+    date: '2023-03-28',
+    week: 2,
+    season: 'lent',
+    passages: ['Iz 65, 17-21', 'Ps 30', 'J 4, 43-54'],
+  },
+  {
+    date: '2023-03-29',
+    week: 2,
+    season: 'lent',
+    passages: ['Ez 47, 1-9. 12', 'Ps 46', 'J 5, 1-3a. 5-16'],
+  },
+  {
+    date: '2023-03-30',
+    week: 2,
+    season: 'lent',
+    passages: ['Iz 49, 8-15', 'Ps 145', 'J 5, 17-30'],
+    notes: ['[HTML]Jana Klimaka[HTML] mn.'],
+  },
+  {
+    date: '2023-03-31',
+    week: 2,
+    season: 'lent',
+    passages: ['Wj 32, 7-14', 'Ps 106', 'J 5, 31-47'],
+  },
+];
