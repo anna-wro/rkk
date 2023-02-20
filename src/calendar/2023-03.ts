4101eb27e22fc391ac590516863863a6fd208dbe
Petraszka <74956643+Petraszka@users.noreply.github.com>
2023-02-20 15:40:09 +0100

    Update 2023-03.ts
    Na razie tylko połowa zrobiona

--- src/calendar/2023-03.ts
+++ src/calendar/2023-03.ts
@@ -4,87 +4,90 @@ export const March2023: CalendarType = [
   {
     date: '2023-03-01',
     week: 2,
-    season: 'ordinary',
-    passages: ['1 P 1, 10-16', 'Ps 98', 'Mk 10, 28-31'],
+    season: 'lent',
+    passages: ['Jon 3, 1-10', 'Ps 51', 'Łk 11, 29-32'],
   },
   {
     date: '2023-03-02',
     week: 2,
     season: 'lent',
-    passages: ['Jl 2, 12-18', 'Ps 51', '2 Kor 5, 20-6, 2', 'Mt 6, 1-6.16-18'],
-    holidays: ['ŚRODA POPIELCOWA'],
+    passages: ['Est 4, 17k.l-m.r-u', 'Ps 138', 'Mt 7, 7-12'],
   },
   {
     date: '2023-03-03',
     week: 2,
     season: 'lent',
-    passages: ['Pwt 30, 15-20', 'Ps 1', 'Łk 9, 22-25'],
+    passages: ['Ez 18, 21-28', 'Ps 130', 'Mt 5, 20-265'],
     notes: ['[HTML]Jana i Karola Wesleyów[HTML] prezb.'],
   },
   {
     date: '2023-03-04',
     week: 2,
     season: 'lent',
-    passages: ['Iz 58, 1-9a', 'Ps 51', 'Mt 9, 14-15'],
+    passages: ['Pwt 26, 16-19', 'Ps 119', 'Mt 5, 43-48'],
   },
   {
     date: '2023-03-05',
     week: 2,
     season: 'lent',
-    passages: ['Iz 58, 9b-14', 'Ps 86', 'Łk 5, 27-32'],
+    passages: ['Rdz 12, 1-4a', 'Ps 33', '2 Tm 1, 8b-10', 'Mt 17, 1-9'],
     notes: ['[HTML]Gerazyma[HTML] mn.'],
-  },
-  {
-    date: '2023-03-06',
-    week: 1,
-    season: 'lent',
-    passages: ['Pwt 26, 4-10', 'Ps 91', 'Rz 10, 8-13', 'Łk 4, 1-13'],
-    holidays: ['1. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
-    lauds: {
-      antiphon:
-        'Duch wyprowadził Jezusa na pustynię, * aby kuszony był przez diabła. / A gdy przepościł czterdzieści dni i nocy, / głód wielki potem odczuwał.',
-    },
+    holidays: ['2. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
     eve: {
       antiphon:
-        'Nie samym chlebem żyje człowiek, * lecz każdym słowem, / które pochodzi z ust Bożych.',
+        'Odezwał się głos z obłoku: * To jest mój Syn umiłowany, / w którym mam upodobanie, / Jego słuchajcie.',
+    },
+    lauds: {
+      antiphon:
+        'Nasz Pan, Jezus Chrystus, śmierć zniweczył * i przez Ewangelię dał nam światło życia wiecznego.',
     },
     vespers: {
       antiphon:
-        'Czuwaj nad nami, o Zbawicielu, * by nas nie pochwycił przebiegły kusiciel; / Ty bowiem jesteś naszą odwieczną pomocą.',
+        'Nie mówcie nikomu o tym widzeniu, * aż Syn Człowieczy zmartwychwstanie.',
+    }
+  },
+  {
+    date: '2023-03-06',
+    week: 2,
+    season: 'lent',
+    passages: ['Dn 9, 4b-10', 'Ps 78', 'Łk 6, 36-38'],
     },
   },
   {
     date: '2023-03-07',
-    week: 1,
+    week: 2,
     season: 'lent',
-    passages: ['Kpł 19, 1-2. 11-18', 'Ps 19', 'Mt 25, 31-46'],
+    passages: ['Iz 1, 10.16-20', 'Ps 50', 'Mt 23, 1-12'],
     notes: ['[HTML]Perpetuy i Felicyty[HTML] m.'],
   },
   {
     date: '2023-03-08',
-    week: 1,
+    week: 2,
     season: 'lent',
-    passages: ['Iz 55, 10-11', 'Ps 34', 'Mt 6, 7-15'],
+    passages: ['Jr 18, 18-20', 'Ps 31', 'Mt 20, 17-28'],
   },
   {
     date: '2023-03-09',
-    week: 1,
+    week: 2,
     season: 'lent',
-    passages: ['Jon 3, 1-10', 'Ps 51', 'Łk 11, 29-32'],
+    passages: ['Jr 17, 5-10', 'Ps1', 'Łk 16, 19-31'],
   },
   {
     date: '2023-03-10',
-    week: 1,
+    week: 2,
     season: 'lent',
-    passages: ['Est 4, 17k-17m.17r-17u', 'Ps 138', 'Mt 7, 7-12'],
+    passages: ['Rdz 37, 3-4.12-13a.17b-28a', 'Ps 105', 'Mt 21, 33-43.45-46'],
   },
   {
     date: '2023-03-11',
-    week: 1,
+    week: 2,
     season: 'lent',
-    passages: ['Ez 18, 21-28', 'Ps 130', 'Mt 5, 20-26'],
+    passages: ['Mi 7, 14-15.18-20', 'Ps 103', 'Łk 15, 1-3.11-32'],
     notes: ['[HTML]Matek i ojców monastycyzmu[HTML]'],
   },
+  
+  
+  
   {
     date: '2023-03-12',
     week: 1,
