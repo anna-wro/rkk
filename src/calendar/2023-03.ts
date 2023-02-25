e30d93ef1279a4e0a75303353c0ea6bc4b8790e5
Petraszka <74956643+Petraszka@users.noreply.github.com>
2023-02-25 22:01:52 +0100

    Skończony marzec

--- src/calendar/2023-03.ts
+++ src/calendar/2023-03.ts
@@ -85,89 +85,12 @@ export const March2023: CalendarType = [
     passages: ['Mi 7, 14-15.18-20', 'Ps 103', 'Łk 15, 1-3.11-32'],
     notes: ['[HTML]Matek i ojców monastycyzmu[HTML]'],
   },
-  
-  
-  
   {
     date: '2023-03-12',
     week: 1,
     season: 'lent',
-    passages: ['Pwt 26, 16-19', 'Ps 119', 'Mt 5, 43-48'],
-    notes: ['[HTML]Grzegorza Wielkiego[HTML] bp.'],
-  },
-  {
-    date: '2023-03-13',
-    week: 2,
-    season: 'lent',
-    passages: [
-      'Rdz 15, 5-12. 17-18',
-      'Ps 27',
-      'Flp 3, 17–4, 1',
-      'Łk 9, 28b-36',
-    ],
-    holidays: ['2. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
-    eve: {
-      antiphon:
-        'Odezwał się głos z obłoku: * To jest mój Syn umiłowany, / w którym mam upodobanie, / Jego słuchajcie.',
-    },
-    lauds: {
-      antiphon:
-        'Nasz Pan, Jezus Chrystus, śmierć zniweczył * i przez Ewangelię dał nam światło życia wiecznego.',
-    },
-    vespers: {
-      antiphon:
-        'Nie mówcie nikomu o tym widzeniu, * aż Syn Człowieczy zmartwychwstanie.',
-    },
-  },
-  {
-    date: '2023-03-14',
-    week: 2,
-    season: 'lent',
-    passages: ['Dn 9, 4b-10', 'Ps 78', 'Łk 6, 36-38'],
-  },
-  {
-    date: '2023-03-15',
-    week: 2,
-    season: 'lent',
-    passages: ['Iz 1, 10. 16-20', 'Ps 50', 'Mt 23, 1-12'],
-  },
-  {
-    date: '2023-03-16',
-    week: 2,
-    season: 'lent',
-    passages: ['Jr 18, 18-20', 'Ps 31', 'Mt 20, 17-28'],
-  },
-  {
-    date: '2023-03-17',
-    week: 2,
-    season: 'lent',
-    passages: ['Jr 17, 5-10', 'Ps 1', 'Łk 16, 19-31'],
-    notes: ['[HTML]Patryka[HTML] bp.'],
-  },
-  {
-    date: '2023-03-18',
-    week: 2,
-    season: 'lent',
-    passages: ['Rdz 37, 3-4. 12-13a. 17b-28a', 'Ps 105', 'Mt 21, 33-43. 45-46'],
-    notes: ['[HTML]Cyryla Jerozolimskiego[HTML] bp. teo.'],
-  },
-  {
-    date: '2023-03-19',
-    week: 2,
-    season: 'lent',
-    passages: [
-      '2 Sm 7, 4-5a. 12-14a. 16',
-      'Ps 89',
-      'Rz 4, 13. 16-18. 22',
-      'Mt 1, 16. 18-21. 24',
-    ],
-    holidays: ['JÓZEFA Z NAZARETU'],
-  },
-  {
-    date: '2023-03-20',
-    week: 1,
-    season: 'lent',
     passages: ['Wj 17, 3-7', 'Ps 95', 'Rz 5, 1-2.5-8', 'J 4, 5-42'],
+    notes: ['[HTML]Grzegorza Wielkiego[HTML] bp.'],
     holidays: ['3. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
     eve: {
       antiphon:
@@ -175,7 +98,7 @@ export const March2023: CalendarType = [
     },
     lauds: {
       antiphon:
-        'Pan mówi: * kto będzie pił wodę, którą Ja mu dam, / nie będzie pragnął na wieki.',
+        'Pan mówi: * kto będzie pił wodę, którą Ja mu dam, / nie będzie pragnął na wieki.',
     },
     vespers: {
       antiphon:
@@ -183,47 +106,48 @@ export const March2023: CalendarType = [
     },
   },
   {
-    date: '2023-03-21',
+    date: '2023-03-13',
     week: 1,
     season: 'lent',
-    passages: ['2 Krl 5, 1-15a', 'Ps 42', 'Łk 4, 24-30'],
+    passages: ['2 Krl 5, 1-15a', 'Ps 42', ' Łk 4, 24-30'],
   },
   {
-    date: '2023-03-22',
+    date: '2023-03-14',
     week: 1,
     season: 'lent',
-    passages: ['Dn 3, 25. 34-43', 'Ps 25', 'Mt 18, 21-35'],
+    passages: ['Dn 3, 25.34-43', 'Ps 25', 'Mt 18, 21-35'],
   },
   {
-    date: '2023-03-23',
+    date: '2023-03-15',
     week: 1,
     season: 'lent',
-    passages: ['Pwt 4, 1. 5-9', 'Ps 147', 'Mt 5, 17-19'],
+    passages: ['Pwt 4, 1.5-9', 'Ps 147', 'Mt 5, 17-19'],
   },
   {
-    date: '2023-03-24',
+    date: '2023-03-16',
     week: 1,
     season: 'lent',
     passages: ['Jr 7, 23-28', 'Ps 95', 'Łk 11, 14-23'],
   },
   {
-    date: '2023-03-25',
+    date: '2023-03-17',
     week: 1,
     season: 'lent',
-    passages: ['Iz 7, 10-14; 8, 10', 'Ps 40', 'Hbr 10, 4-10', 'Łk 1, 26-38'],
-    holidays: ['UROCZYSTOŚĆ ZWIASTOWANIA PAŃSKIEGO'],
+    passages: ['Oz 14, 2-10', 'Ps 81', 'Mk 12, 28b-34'],
+    notes: ['[HTML]Patryka[HTML] bp.'],
   },
   {
-    date: '2023-03-26',
+    date: '2023-03-18',
     week: 1,
     season: 'lent',
-    passages: ['Oz 6, 1-6', 'Ps 51', 'Łk 18, 9-14'],
+    passages: ['Oz 6, 1-6', 'Ps 51', ' Łk 18, 9-14'],
+    notes: ['[HTML]Cyryla Jerozolimskiego[HTML] bp. teo.'],
   },
   {
-    date: '2023-03-27',
+    date: '2023-03-19',
     week: 2,
     season: 'lent',
-    passages: ['1 Sm 16, 1b. 6-7.10-13a', 'Ps 23', 'Ef 5, 8-14', 'J 9, 1-41'],
+    passages: ['1Sm 16, 1-13', 'Ps 23', 'Ef 5, 8-14', 'Ef 5, 8-14', 'J 9, 1-41'],
     holidays: ['4. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
     eve: {
       antiphon:
@@ -239,28 +163,94 @@ export const March2023: CalendarType = [
     },
   },
   {
-    date: '2023-03-28',
+    date: '2023-03-20',
     week: 2,
     season: 'lent',
-    passages: ['Iz 65, 17-21', 'Ps 30', 'J 4, 43-54'],
+    passages: ['2Sm 7, 4-5a.12-14a.16', 'Ps 89', 'Rz 4, 13.16-18.22', 'Mt 1, 16.18-21.24'],
+    holidays: ['JÓZEFA Z NAZARETU'],
   },
   {
-    date: '2023-03-29',
+    date: '2023-03-21',
     week: 2,
     season: 'lent',
-    passages: ['Ez 47, 1-9. 12', 'Ps 46', 'J 5, 1-3a. 5-16'],
+    passages: ['Ez 47, 1-9.12', 'Ps 46', 'J 5, 1-3a.5-16'],
   },
   {
-    date: '2023-03-30',
+    date: '2023-03-22',
     week: 2,
     season: 'lent',
     passages: ['Iz 49, 8-15', 'Ps 145', 'J 5, 17-30'],
+  },
+  {
+    date: '2023-03-23',
+    week: 2,
+    season: 'lent',
+    passages: ['Wj 32, 7-14', 'Ps 106', 'J 5, 31-47'],
+  },
+  {
+    date: '2023-03-24',
+    week: 2,
+    season: 'lent',
+    passages: ['Mdr 2, 1a.12-22', 'Ps 34', 'J 7, 1-2.10.25-30'],
+    notes: ['[HTML]Oskara Romero[HTML] bp. m.'],
+  },
+  {
+    date: '2023-03-25',
+    week: 2,
+    season: 'lent',
+    passages: ['Iz 7, 10-14; 8, 10', 'Ps 40', 'Hbr 10, 4-10', 'Łk 1, 26-38'],
+    holidays: ['UROCZYSTOŚĆ ZWIASTOWANIA PAŃSKIEGO'],
+  },
+  {
+    date: '2023-03-26',
+    week: 1,
+    season: 'lent',
+    passages: ['Ez 37, 12-14', 'Ps 130', 'Rz 8, 8-11', 'J 11, 1-45'],
+    notes: ['[HTML]Dobrego Łotra[HTML]']
+    holidays: ['5. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
+    passages: ['Ez 37, 12-14', 'Ps 130', 'Rz 8, 8-11', 'J 11, 1-45'],
+    eve: {
+      antiphon:
+        'Jeśli ziarno pszenicy * nie wpadnie w ziemię i nie obumrze, / zostanie tylko samo; / lecz jeśli obumrze, / przynosi plon obfity.',
+    },
+    lauds: {
+      antiphon: 'Zasnął przyjaciel nasz Łazarz, * lecz idę, aby go obudzić.',
+    },
+    vespers: {
+      antiphon:
+        'Panie, gdybyś był tutaj, Łazarz by nie umarł, * ale ja i tak wierzę, że jesteś Synem Bożym.',
+    },
+  },
+  {
+    date: '2023-03-27',
+    week: 1,
+    season: 'lent',
+    passages: ['Dn 13, 1-9.15-17.19-30.33-62', 'Ps 23', 'J 8, 1-11'],
+    notes: ['[HTML]Męczenników Trapistów z Tibihirine[HTML]'],
+  },
+  {
+    date: '2023-03-28',
+    week: 1,
+    season: 'lent',
+    passages: ['Lb 21, 4-9', 'Ps 102', 'J 8, 21-30'],
+  },
+  {
+    date: '2023-03-29',
+    week: 1,
+    season: 'lent',
+    passages: ['Dn 3, 14-20.91-92.95', 'Dn 3', ' J 8, 31-42'],
+  },
+  {
+    date: '2023-03-30',
+    week: 1,
+    season: 'lent',
+    passages: ['Rdz 17, 3-9', 'Ps 105', ' J 8, 51-59'],
     notes: ['[HTML]Jana Klimaka[HTML] mn.'],
   },
   {
     date: '2023-03-31',
-    week: 2,
+    week: 1,
     season: 'lent',
-    passages: ['Wj 32, 7-14', 'Ps 106', 'J 5, 31-47'],
+    passages: ['Jr 20, 10-13', 'Ps 18', ' J 10, 31-42'],
   },
 ];
