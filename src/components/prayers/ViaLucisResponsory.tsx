ff835e00c0d690795d083e6391af8bce0307ffde
Anna Wróbel <anna-wro@users.noreply.github.com>
2023-04-18 21:07:31 +0200

    🕯️ Via Lucis (#257)

---
+++ src/components/prayers/ViaLucisResponsory.tsx
@@ -0,0 +1,10 @@
+import { V, R } from './index';
+
+export const ViaLucisResponsory = () => {
+  return (
+    <div className="mb-4">
+      <V>Witaj, Jezu Zmartwychwstały.</V>
+      <R>I od Ojca poślij nam Ducha Świętego.</R>
+    </div>
+  );
+};
