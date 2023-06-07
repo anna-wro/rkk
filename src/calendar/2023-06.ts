5077ce2bed9bdc6e0da45c8e10bfbc6dbd4b4f3b
Anna Wróbel <wro.online@gmail.com>
2023-06-07 22:17:25 +0200

    change year

--- src/calendar/2023-06.ts
+++ src/calendar/2023-06.ts
@@ -74,95 +74,76 @@ export const June2023: CalendarType = [
     ],
     variant: 2,
   },
-   {
+  {
     date: '2023-06-04',
     week: 2,
     season: 'easter',
     holidays: ['NAJŚWIĘTSZEJ TRÓJCY'],
     notes: ['Święto patronalne wspólnoty w Warszawie'],
-    passages: ['Wj 34, 4b-6.8-9',
-               'Dn 3',
-               '2 Kor 13, 11-13',
-               'J 3, 16-18'],
+    passages: ['Wj 34, 4b-6.8-9', 'Dn 3', '2 Kor 13, 11-13', 'J 3, 16-18'],
     links: [
       { name: 'wigilia', slug: 'niedziela-trojcy-swietej-wigilia' },
       { name: 'jutrznia', slug: 'niedziela-trojcy-swietej-jutrznia' },
       { name: 'nieszpory', slug: 'niedziela-trojcy-swietej-nieszpory' },
     ],
   },
-   {
+  {
     date: '2023-06-05',
     week: 2,
     season: 'ordinary',
     notes: ['[HTML]Bonifacego Winfryda[HTML] bp. ap. m.'],
-    passages: ['Tb 1, 3; 2, 1a-8',
-               'Ps 112',
-               'Mk 12, 1-12'],
+    passages: ['Tb 1, 3; 2, 1a-8', 'Ps 112', 'Mk 12, 1-12'],
   },
-   {
+  {
     date: '2023-06-06',
     week: 2,
     season: 'ordinary',
     notes: ['[HTML]Marii Karłowskiej[HTML] zak. '],
-    passages: ['Tb 2, 9-14',
-               'Ps 112',
-               'Mk 12, 13-17'],
+    passages: ['Tb 2, 9-14', 'Ps 112', 'Mk 12, 13-17'],
   },
-   {
+  {
     date: '2023-06-07',
     week: 2,
     season: 'ordinary',
-    passages: ['Tb 3, 1-11a.16-17a',
-               'Ps 25',
-               'Mk 12, 18-27'],
+    passages: ['Tb 3, 1-11a.16-17a', 'Ps 25', 'Mk 12, 18-27'],
   },
-   {
+  {
     date: '2023-06-08',
     week: 2,
     season: 'ordinary',
     holidays: ['NAJŚWIĘTSZEGO CIAŁA I KRWI PAŃSKIEJ'],
-    passages: ['Pwt 8, 2-3.14b-16a',
-               'Ps 147',
-               '1 Kor 10, 16-17',
-               'J 6, 51-58'],
+    passages: ['Pwt 8, 2-3.14b-16a', 'Ps 147', '1 Kor 10, 16-17', 'J 6, 51-58'],
   },
   {
     date: '2023-06-09',
     week: 2,
     season: 'ordinary',
-    passages: ['Tb 11, 5-17',
-               'Ps 146',
-               'Mk 12, 35-37'],
+    passages: ['Tb 11, 5-17', 'Ps 146', 'Mk 12, 35-37'],
   },
-   {
+  {
     date: '2023-06-10',
     week: 2,
     season: 'ordinary',
-    notes: ['[HTML]Efrema Syryjczyka[HTML] dk. teo.', 
-            '[HTML]Bogumiła z Bobrowa[HTML] bp.'],
-    passages: ['Tb 12, 1.5-15.20',
-               'Tb 13',
-               'Mk 12, 38-44'],
+    notes: [
+      '[HTML]Efrema Syryjczyka[HTML] dk. teo.',
+      '[HTML]Bogumiła z Bobrowa[HTML] bp.',
+    ],
+    passages: ['Tb 12, 1.5-15.20', 'Tb 13', 'Mk 12, 38-44'],
   },
-   {
-    date: '2022-06-11',
+  {
+    date: '2023-06-11',
     week: 1,
     season: 'ordinary',
     holidays: ['2. NIEDZIELA PO PIĘĆDZIESIĄTNICY'],
     notes: ['[HTML]BARNABY[HTML] ap.'],
-    passages: ['Oz 6, 3-6',
-               'Ps 50',
-               'Rz 4, 18-25',
-               'Mt 9, 9-13']
+    passages: ['Oz 6, 3-6', 'Ps 50', 'Rz 4, 18-25', 'Mt 9, 9-13'],
   },
-   {
-    date: '2022-06-12',
+  {
+    date: '2023-06-12',
     week: 1,
     season: 'ordinary',
     holidays: ['2. NIEDZIELA PO PIĘĆDZIESIĄTNICY'],
     notes: ['[HTML]BARNABY[HTML] ap.'],
-    passages: ['2 Kor 1, 1-7',
-               'Ps 34',
-               'Mt 5, 1-12']
+    passages: ['2 Kor 1, 1-7', 'Ps 34', 'Mt 5, 1-12'],
   },
 ];
