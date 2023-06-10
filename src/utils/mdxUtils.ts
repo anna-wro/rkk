3b2114af3d291df1c4ce45baff56e5618299c685
Anna Wróbel <wro.online@gmail.com>
2023-06-11 00:39:37 +0200

    Support readings from different directories

--- src/utils/mdxUtils.ts
+++ src/utils/mdxUtils.ts
@@ -2,6 +2,42 @@ import fs from 'fs';
 import path from 'path';
 import { getCurrentSeason } from 'utils/date';
 
+export const READINGS_PATH = path.join(process.cwd(), 'src/data/readings');
+
+// Get all reading file paths recursively
+export function getReadingsFilePaths() {
+  const readingsFilePaths = [];
+  const volumes = ['vol-2', 'vol-3', 'vol-6'];
+
+  volumes.forEach(volume => {
+    const volumePath = path.join(READINGS_PATH, volume);
+    const volumeFiles = fs.readdirSync(volumePath);
+
+    volumeFiles.forEach(file => {
+      const filePath = path.join(READINGS_PATH, volume, file);
+      const fileStat = fs.statSync(filePath);
+      const isDirectory = fileStat.isDirectory();
+
+      if (isDirectory) {
+        const nestedFiles = fs.readdirSync(filePath);
+        nestedFiles.forEach(nestedFile => {
+          const nestedFilePath = path.join(
+            READINGS_PATH,
+            volume,
+            file,
+            nestedFile,
+          );
+          readingsFilePaths.push(nestedFilePath);
+        });
+      } else {
+        readingsFilePaths.push(filePath);
+      }
+    });
+  });
+
+  return readingsFilePaths;
+}
+
 const currentSeason = getCurrentSeason();
 
 export const SEASON_PRAYERS_PATH = path.join(
@@ -10,11 +46,6 @@ export const SEASON_PRAYERS_PATH = path.join(
 );
 
 export const CUSTOM_PRAYERS_PATH = path.join(process.cwd(), `src/data/custom`);
-// FIXME - this is hardcoded to vol-3
-export const READINGS_PATH = path.join(
-  process.cwd(),
-  `src/data/readings/vol-3`,
-);
 
 export const seasonPrayersFilePaths = fs
   .readdirSync(SEASON_PRAYERS_PATH)
@@ -22,8 +53,4 @@ export const seasonPrayersFilePaths = fs
 
 export const customPrayersFilePaths = fs
   .readdirSync(CUSTOM_PRAYERS_PATH)
-  .filter(path => /\.mdx?$/.test(path));
-
-export const readingsFilePaths = fs
-  .readdirSync(READINGS_PATH)
   .filter(path => /\.mdx?$/.test(path));
