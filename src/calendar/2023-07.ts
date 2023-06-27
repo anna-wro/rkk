4baffe58df31aeeba1b96256b684608d4b310a5f
Anna Wróbel <anna-wro@users.noreply.github.com>
2023-06-27 19:10:28 +0200

    Update 2023-07.ts

--- src/calendar/2023-07.ts
+++ src/calendar/2023-07.ts
@@ -11,22 +11,27 @@ export const July2023: CalendarType = [
       '[HTML]Antonio Rosimini-Serbatiego[HTML] prezb.',
     ],
   },
-   {
+  {
     date: '2023-07-02',
     week: 2,
     season: 'ordinary',
     holidays: ['5. NIEDZIELA PO PIĘĆDZIESIĄTNICY'],
-    passages: ['2 Krl 4, 8-11.14-16a', 'Ps 89', 'Rz 6, 3-4.8-11', 'Mt 10, 37-42'],
+    passages: [
+      '2 Krl 4, 8-11.14-16a',
+      'Ps 89',
+      'Rz 6, 3-4.8-11',
+      'Mt 10, 37-42',
+    ],
     notes: ['[HTML]Jana z Szanghaju i San Francisco[HTML] bp.'],
   },
-   {
+  {
     date: '2023-07-03',
     week: 2,
     season: 'ordinary',
     holidays: ['TOMASZA APOSTOŁA'],
     passages: ['Ef 2, 19-22', 'Ps 117', 'J 20, 24-29'],
   },
-   {
+  {
     date: '2023-07-04',
     week: 2,
     season: 'ordinary',
@@ -46,27 +51,27 @@ export const July2023: CalendarType = [
     passages: ['Rdz 22, 1b-19', 'Ps 115', 'Mt 9, 1-8'],
     notes: ['[HTML]Jana Husa[HTML] prezb. m.'],
   },
-   {
+  {
     date: '2023-07-07',
     week: 2,
     season: 'ordinary',
     passages: ['Rdz 23, 1-4.19; 24, 1-8.62-67', 'Ps 106', 'Mt 9, 9-13'],
     notes: ['[HTML]Atemagprasa I [HTML] bp.', 'DZIEŃ WSPÓLNEGO POSTU'],
   },
-    {
+  {
     date: '2023-07-08',
     week: 2,
     season: 'ordinary',
     passages: ['Rdz 27, 1-5.15-29', 'Ps 135', 'Mt 9, 14-17'],
   },
-   {
+  {
     date: '2023-07-09',
     week: 1,
     season: 'ordinary',
     holidays: ['6. NIEDZIELA PO PIĘĆDZIESIĄTNICY'],
     passages: ['Za 9, 9-10', 'Ps 145', 'Rz 8, 9, 11-13', 'Mt 11, 25-30'],
   },
-   {
+  {
     date: '2023-07-10',
     week: 1,
     season: 'ordinary',
@@ -78,7 +83,7 @@ export const July2023: CalendarType = [
       },
     ],
   },
-   {
+  {
     date: '2023-07-11',
     week: 1,
     season: 'ordinary',
@@ -95,8 +100,10 @@ export const July2023: CalendarType = [
     week: 1,
     season: 'ordinary',
     passages: ['Rdz 41, 55-57; 42, 5-7a.17-24a', 'Ps 33', 'Mt 10, 1-7'],
-    notes: ['[HTML]Brunona z Kwerfurtu[HTML] bp. m.',
-            '[HTML]Sergiusza Bułgakowa[HTML] teo.'],
+    notes: [
+      '[HTML]Brunona z Kwerfurtu[HTML] bp. m.',
+      '[HTML]Sergiusza Bułgakowa[HTML] teo.',
+    ],
   },
   {
     date: '2023-07-13',
