3be843b75d21f30e8a94180e252b3e67a2d3347b
Anna Wróbel <anna-wro@users.noreply.github.com>
2023-08-05 18:42:52 +0200

    🌱 Implement settings page and adjusting font size & family (#283)

---
+++ src/utils/useFontSize.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+
+const useFontSize = () => {
+  const getItem = key => {
+    try {
+      return localStorage.getItem(key);
+    } catch {}
+  };
+
+  const setItem = (key, value) => {
+    try {
+      return localStorage.setItem(key, value);
+    } catch {}
+  };
+
+  const [fontSize, setFontSize] = useState(null);
+
+  useEffect(() => {
+    const initialFontSize = Number(getItem('fontSize')) || 16;
+    setFontSize(initialFontSize);
+  }, []);
+
+  useEffect(() => {
+    if (fontSize !== null) {
+      const root = document.getElementsByTagName('html')[0];
+      root.style.setProperty('font-size', `${fontSize}px`);
+      setItem('fontSize', fontSize);
+    }
+  }, [fontSize]);
+
+  const increaseFontSize = () => {
+    if (fontSize < 60) {
+      setFontSize(prevFontSize => prevFontSize + 2);
+    }
+  };
+
+  const decreaseFontSize = () => {
+    if (fontSize > 10) {
+      setFontSize(prevFontSize => prevFontSize - 2);
+    }
+  };
+
+  const resetFontSize = () => setFontSize(16);
+
+  return { fontSize, increaseFontSize, decreaseFontSize, resetFontSize };
+};
+
+export default useFontSize;
