560c53003e1cd957b22928be55b8e420e423091d
anna-wro <wro.online@gmail.com>
2023-08-05 15:11:48 +0200

    Align labels to left

--- src/components/settings/SettingsPage.tsx
+++ src/components/settings/SettingsPage.tsx
@@ -43,7 +43,9 @@ export default function SettingsPage() {
           <div className="flex items-center content-center bg-gray-50 bg-opacity-60 p-4 rounded-lg">
             <div>{copy.preview}</div>
           </div>
-          <div className="mb-2 font-semibold">{copy.fontSizeLabel}</div>
+          <div className="mb-2 font-semibold self-start">
+            {copy.fontSizeLabel}
+          </div>
           <div className="flex space-x-4">
             <button
               className={`${cssClasses.fontSizeButton} ${
@@ -65,7 +67,7 @@ export default function SettingsPage() {
               +
             </button>
           </div>
-          <div className="mb-2 font-semibold">{copy.fontLabel}</div>
+          <div className="mb-2 font-semibold self-start">{copy.fontLabel}</div>
           <div className="flex space-x-4">
             <button
               className={`${cssClasses.fontButton} ${
