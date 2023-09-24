871434d15fc730b6f7b2a251f2598f25e2cb7eff
anna-wro <wro.online@gmail.com>
2023-09-24 13:37:27 +0200

    Move controls to separate components

---
+++ src/components/settings/ResetControls.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { styles } from './styles';
+
+interface ResetControlsProps {
+  resetSettings: () => void;
+}
+
+export const ResetControls: React.FC<ResetControlsProps> = ({
+  resetSettings,
+}) => (
+  <button className={styles.resetButton} onClick={resetSettings}>
+    Reset Settings
+  </button>
+);
--- src/pages/ustawienia/index.tsx
+++ src/pages/ustawienia/index.tsx
@@ -1,13 +1,13 @@
 import Layout from 'components/layout/Layout';
 import { InfoForDayFacade } from 'components/layout/InfoForDayFacade';
-import SettingsPage from 'components/settings/SettingsPage';
+import SettingsPageFacade from 'components/settings/SettingsPageFacade';
 import { copy } from 'consts/copy';
 
 export default function CustomPrayersIndex() {
   return (
     <Layout title={copy.settings.headline}>
       <InfoForDayFacade />
-      <SettingsPage />
+      <SettingsPageFacade />
     </Layout>
   );
 }
