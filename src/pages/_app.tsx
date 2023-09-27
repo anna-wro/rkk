710386710092d9464896079a914bff53c34e4180
anna-wro <wro.online@gmail.com>
2023-09-27 20:14:32 +0200

    🐞 Fix dark mode not working on main page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import 'tailwindcss/tailwind.css';
 import 'styles/index.css';
+import useTheme from 'utils/useTheme';
 
 function MyApp({ Component, pageProps }) {
+  useTheme();
+
   return <Component {...pageProps} />;
 }
 
