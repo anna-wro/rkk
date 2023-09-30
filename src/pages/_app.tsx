9f9b0939f4ef4de08c7e481916c287f5ca394113
anna-wro <wro.online@gmail.com>
2023-09-30 20:35:35 +0200

    remove onSave from localStorage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import 'tailwindcss/tailwind.css';
 import 'styles/index.css';
 import useTheme from 'utils/useTheme';
+import { FlashMessageProvider } from 'components/FlashMessageContext';
+
+function MyAppContent({ Component, pageProps }) {
+  useTheme();
+  return <Component {...pageProps} />;
+}
 
 function MyApp({ Component, pageProps }) {
-  useTheme();
-
-  return <Component {...pageProps} />;
+  return (
+    <FlashMessageProvider>
+      <MyAppContent Component={Component} pageProps={pageProps} />
+    </FlashMessageProvider>
+  );
 }
 
 export default MyApp;
--- src/utils/useLocalStorage.tsx
+++ src/utils/useLocalStorage.tsx
@@ -1,6 +1,6 @@
 import { useCallback } from 'react';
 
-export const useLocalStorage = (onSave?: () => void) => {
+export const useLocalStorage = () => {
   const getItem = useCallback((key: string): string | null => {
     try {
       return localStorage.getItem(key);
@@ -9,15 +9,11 @@ export const useLocalStorage = (onSave?: () => void) => {
     }
   }, []);
 
-  const setItem = useCallback(
-    (key: string, value: string): void => {
-      try {
-        localStorage.setItem(key, value);
-        onSave?.();
-      } catch {}
-    },
-    [onSave],
-  );
+  const setItem = useCallback((key: string, value: string): void => {
+    try {
+      localStorage.setItem(key, value);
+    } catch {}
+  }, []);
 
   return { getItem, setItem };
 };
