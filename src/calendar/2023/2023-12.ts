7653b15dfbf220e025a721c5e685ed10d900db6a
Anna Wróbel <anna-wro@users.noreply.github.com>
2023-11-26 19:07:22 +0100

    📆 December 2023  (#310)
    * 📆 December 2023 - draft
    
    * December-2023
    
    * add info about advent
    
    ---------
    
    Co-authored-by: Klaudia Sowiak <klaudia.sowiak@gmail.com>

---
+++ src/calendar/2023/2023-12.ts
@@ -0,0 +1,379 @@
+import type { CalendarType } from '../calendar';
+
+export const December2023: CalendarType = [
+  {
+    date: '2023-12-01',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Dn 7, 2-14', 'Dn 3', ' Łk 21, 29-33'],
+    notes: ['[HTML]Karola de Foucauld[HTML] mn.'],
+  },
+  {
+    date: '2023-12-02',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Dn 7, 15-27', 'Dn 3', 'Łk 21, 34-36'],
+    notes: ['Po zachodzie słońca zaczyna się Adwent'],
+  },
+  {
+    date: '2023-12-03',
+    week: 1,
+    season: 'advent',
+    passages: ['Iz 2, 1-5', 'Ps 122', 'Rz 13, 11-14', ' Mt 24, 37-44'],
+    notes: ['[HTML]Franciszka Ksawerego[HTML] prezb. mis.'],
+    holidays: ['1. NIEDZIELA ADWENTU'],
+    links: [
+      { name: 'Lucenarium', slug: 'lucenarium-1' },
+      { name: 'liturgia słowa', slug: 'czytania/1-niedziela-adwentu-rok-b' },
+    ],
+  },
+  {
+    date: '2023-12-04',
+    week: 1,
+    season: 'advent',
+    passages: ['Iz 4, 2-6', 'Ps 122', 'Mt 8, 5-11'],
+    notes: ['[HTML]Jana z Damaszku[HTML] mn. teo.'],
+  },
+  {
+    date: '2023-12-05',
+    week: 1,
+    season: 'advent',
+    passages: ['Iz 11, 1-10', ' Ps 72', 'Łk 10, 21-24'],
+    notes: ['[HTML]Klemensa z Aleksandrii[HTML] mn. teo.'],
+  },
+  {
+    date: '2023-12-06',
+    week: 1,
+    season: 'advent',
+    passages: ['Iz 25, 6-10a', 'Ps 23', ' Mt 15, 29-37'],
+    notes: ['[HTML]Mikołaja[HTML] bp.'],
+  },
+  {
+    date: '2023-12-07',
+    week: 1,
+    season: 'advent',
+    passages: ['Iz 26, 1-6', 'Ps 118', 'Mt 7, 21, 24-27'],
+    notes: ['[HTML]Ambrożego[HTML] bp. teo.'],
+  },
+  {
+    date: '2023-12-08',
+    week: 1,
+    season: 'advent',
+    passages: ['Rdz 3, 9-15.20', 'Ps 98', 'Ef 1, 3-6.11-12', 'Łk 1, 26-38'],
+    holidays: ['POCZĘCIE BOGURODZICY'],
+  },
+  {
+    date: '2023-12-09',
+    week: 1,
+    season: 'advent',
+    passages: ['Iz 30, 19-21, 23-26', 'Ps 147', ' Mt 9, 35-10, 1, 5-8'],
+  },
+  {
+    date: '2023-12-10',
+    week: 2,
+    season: 'advent',
+    passages: ['Iz 11, 1-10', 'Ps 72', 'Rz 15, 4-9', 'Mt 3, 1-12'],
+    notes: ['[HTML]Tomasza Mertona[HTML] mn.'],
+    holidays: ['3. NIEDZIELA ADWENTU'],
+    links: [
+      { name: 'Lucenarium', slug: 'lucenarium-2' },
+      { name: 'liturgia słowa', slug: 'czytania/2-niedziela-adwentu-rok-b' },
+    ],
+  },
+  {
+    date: '2023-12-11',
+    week: 2,
+    season: 'advent',
+    passages: ['Iz 35, 1-10', 'Ps 85', 'Łk 5, 17-26'],
+  },
+  {
+    date: '2023-12-12',
+    week: 2,
+    season: 'advent',
+    passages: ['Iz 40, 1-11', 'Ps 96', 'Mt 18, 12-14'],
+  },
+  {
+    date: '2023-12-13',
+    week: 2,
+    season: 'advent',
+    passages: ['Iz 40, 25-31', 'Ps 103', 'Mt 11, 28-30'],
+    notes: ['[HTML]Łucji[HTML] m.'],
+  },
+  {
+    date: '2023-12-14',
+    week: 2,
+    season: 'advent',
+    passages: ['Iz 41, 13-20', ' Ps 145', 'Mt 11, 11-15'],
+    notes: ['[HTML]Jana od Krzyża[HTML] zak.'],
+  },
+  {
+    date: '2023-12-15',
+    week: 2,
+    season: 'advent',
+    passages: ['Iz 48, 17-19', 'Ps 1', 'Mt 11, 16-19'],
+    notes: ['[HTML]Catherine de Hueck Doherty[HTML] w.'],
+    isFastingDay: true,
+  },
+  {
+    date: '2023-12-16',
+    week: 2,
+    season: 'advent',
+    passages: ['Syr 48, 1-4.9-11', 'Ps 80', 'Mt 17, 9a.10-13'],
+  },
+  {
+    date: '2023-12-17',
+    week: 1,
+    season: 'advent',
+    passages: ['Iz 35, 1-6a.10', 'Ps 146', 'Jk 5, 7-10', ' Mt 11, 2-11'],
+    notes: ['[HTML]Jana de Matha[HTML] zak.'],
+    vespers: {
+      antiphon:
+        'O Mądrości, która wyszłaś z ust Najwyższego, / obejmując wszechświat od krańca do krańca, / i wszystkim rządzisz z mocą i słodyczą; / przyjdź i naucz nas dróg roztropności.',
+    },
+    holidays: ['3. NIEDZIELA ADWENTU'],
+    links: [
+      { name: 'Lucenarium', slug: 'lucenarium-3' },
+      { name: 'liturgia słowa', slug: 'czytania/3-niedziela-adwentu-rok-b' },
+    ],
+  },
+  {
+    date: '2023-12-18',
+    week: 1,
+    season: 'advent',
+    passages: ['Jr 23, 5-8', 'Ps 72', ' Mt 1, 18-24'],
+    vespers: {
+      antiphon:
+        'O Adonai, Wodzu Izraela, / Tyś w krzaku gorejącym objawił się Mojżeszowi / i na Synaju dałeś mu Prawo; / przyjdź nas wyzwolić swym potężnym ramieniem.',
+    },
+  },
+  {
+    date: '2023-12-19',
+    week: 1,
+    season: 'advent',
+    passages: ['Sdz 13, 2-7.24-25a', 'Ps 71', 'Łk 1, 5-25'],
+    notes: ['[HTML]Fryderyka von Schulte[HTML] teo. w.'],
+    vespers: {
+      antiphon:
+        'O Korzeniu Jessego, / który się wznosisz jako znak dla narodów, przed Tobą zamilkną królowie, a ludy modlić się będą do Ciebie; / przyjdź nas wyzwolić, już dłużej nie zwlekaj.',
+    },
+  },
+  {
+    date: '2023-12-20',
+    week: 1,
+    season: 'advent',
+    passages: ['Iz 7, 10-14', 'Ps 24', 'Łk 1, 26-38'],
+    vespers: {
+      antiphon:
+        'O Kluczu Dawida i Berło domu Izraela, / Ty, który otwierasz, a nikt zamknąć nie zdoła, zamykasz, a nikt nie otworzy; / przyjdź i wyprowadź z więzienia człowieka, pogrążonego w mroku i cieniu śmierci.',
+    },
+  },
+  {
+    date: '2023-12-21',
+    week: 1,
+    season: 'advent',
+    passages: ['Pnp 2, 8-14', 'Ps 33', 'Łk 1, 39-45'],
+    vespers: {
+      antiphon:
+        'O Wschodzie, / blasku Światła wiecznego i Słońce sprawiedliwości, / przyjdź i oświeć siedzących w mroku i cieniu śmierci.',
+    },
+  },
+  {
+    date: '2023-12-22',
+    week: 1,
+    season: 'advent',
+    passages: ['1 Sm 1, 24-28', '1 Sm 2', 'Łk 1, 46-56'],
+    vespers: {
+      antiphon:
+        'O Królu narodów, przez nie upragniony; / Kamieniu węgielny Kościoła, / przyjdź i zbaw człowieka, którego utworzyłeś z prochu ziemi.',
+    },
+  },
+  {
+    date: '2023-12-23',
+    week: 1,
+    season: 'advent',
+    passages: ['Ml 3, 1-4.23-24', 'Ps 25', 'Łk 1, 57-66'],
+    vespers: {
+      antiphon:
+        'O Emmanuelu, / nasz Królu i Prawodawco, oczekiwany Zbawicielu narodów, / przyjdź, aby nas zbawić, nasz Panie i Boże.',
+    },
+  },
+  {
+    date: '2023-12-24',
+    week: 2,
+    season: 'advent',
+    passages: ['Iz 7, 10-14', 'Ps 24', ' Rz 1, 1-7', 'Mt 1, 18-24'],
+    notes: [
+      '[HTML]Adama i Ewy[HTML] pierwszych rodziców',
+      '[HTML]Charbela[HTML] mn.',
+    ],
+    links: [
+      {
+        name: 'wigilia Narodzenia Pańskiego',
+        slug: 'narodzenie-panskie-wigilia',
+      },
+      {
+        name: 'liturgia słowa',
+        slug: 'czytania/4-niedziela-adwentu-rok-b',
+      },
+    ],
+  },
+  {
+    date: '2023-12-25',
+    week: 2,
+    season: 'christmas',
+    passages: ['Iz 62, 1-5', 'Ps 89', 'Dz 13, 16-17.22-25', ' Mt 1, 1-25'],
+    holidays: ['UROCZYSTOŚĆ NARODZENIA PAŃSKIEGO'],
+    lauds: {
+      sigla: 'Hbr 1, 1-2',
+      passage:
+        'Wielokrotnie i na różne sposoby przemawiał niegdyś Bóg do ojców przez proroków, a w tych ostatecznych dniach przemówił do nas przez Syna. Jego to ustanowił dziedzicem wszystkich rzeczy, przez Niego też stworzył wszechświat.',
+    },
+    vespers: {
+      sigla: '1 J 1, 1-3',
+      passage:
+        'To wam oznajmiamy, co było od początku, cośmy słyszeli o Słowie życia, co ujrzeliśmy własnymi oczami, na co patrzyliśmy i czego dotykały nasze ręce, bo życie objawiło się. Myśmy je widzieli, o nim świadczymy i głosimy wam życie wieczne, które było w Ojcu, a nam zostało objawione. Oznajmiamy wam, cośmy ujrzeli i słyszeli, abyście i wy mieli łączność z nami. A mieć z nami łączność znaczy: mieć ją z Ojcem i z Jego Synem Jezusem Chrystusem.',
+    },
+    links: [
+      { name: 'wigilia', slug: 'narodzenie-panskie-wigilia' },
+      { name: 'jutrznia', slug: 'narodzenie-panskie-jutrznia' },
+      { name: 'nieszpory', slug: 'narodzenie-panskie-nieszpory' },
+      { name: 'liturgia słowa', slug: 'narodzenie-panskie-wigilia' },
+    ],
+  },
+  {
+    date: '2023-12-26',
+    week: 2,
+    season: 'christmas',
+    passages: ['Dz 6, 8-10; 7, 54-59', 'Ps 31', 'Mt 10, 17-22'],
+    holidays: ['OKTAWA NARODZENIA PAŃSKIEGO'],
+    notes: ['[HTML]Szczepana[HTML] diak. i m'],
+    lauds: {
+      sigla: 'Iz 49, 8b-9a',
+      passage:
+        'Ustanowiłem Cię przymierzem dla ludu, aby odnowić kraj, aby rozdzielić spustoszone dziedzictwa, aby rzec więźniom: „Wyjdźcie na wolność!”, a marniejącym w ciemnościach: „Ukażcie się!”',
+    },
+    vespers: {
+      sigla: '1 J 1, 5b. 7',
+      passage:
+        'Bóg jest światłością, a nie ma w Nim żadnej ciemności. Jeżeli zaś chodzimy w światłości, tak jak On sam trwa w światłości, wtedy mamy łączność między sobą, a krew Jezusa, Syna Jego, oczyszcza nas z wszelkiego grzechu.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'narodzenie-panskie-jutrznia' },
+      { name: 'nieszpory', slug: 'narodzenie-panskie-nieszpory' },
+    ],
+  },
+  {
+    date: '2023-12-27',
+    week: 2,
+    season: 'christmas',
+    passages: ['1 J 1, 1-4', 'Ps 97', 'J 20, 1a, 2-8'],
+    holidays: ['OKTAWA NARODZENIA PAŃSKIEGO', 'JANA APOSTOŁA'],
+    lauds: {
+      sigla: 'Iz 45, 22-23',
+      passage:
+        'Nawróćcie się do Mnie, by się zbawić, wszystkie krańce świata, bo Ja jestem Bogiem, i nikt inny! Przysięgam na Siebie samego, z moich ust wychodzi sprawiedliwość, słowo nieodwołalne. Tak, przede Mną się zegnie każde kolano, wszelki język na Mnie przysięgać będzie.',
+    },
+    vespers: {
+      sigla: 'Rz 8, 3b-4',
+      passage:
+        'Bóg zesłał Syna swego w ciele podobnym do ciała grzesznego i dla usunięcia grzechu wydał w tym ciele wyrok potępiający na grzech, aby to, co nakazuje Prawo, wypełniło się w nas, o ile nie postępujemy według ciała, ale według Ducha.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'narodzenie-panskie-jutrznia' },
+      { name: 'nieszpory', slug: 'narodzenie-panskie-nieszpory' },
+    ],
+  },
+  {
+    date: '2023-12-28',
+    week: 2,
+    season: 'christmas',
+    passages: ['1 J 1, 5-2.2', 'Ps 124', ' Mt 2, 13-18'],
+    holidays: ['OKTAWA NARODZENIA PAŃSKIEGO'],
+    notes: ['[HTML]Niewinnych Dzieci Betlejemskich[HTML]'],
+    lauds: {
+      sigla: 'Za 8, 7-8',
+      passage:
+        'Oto wyzwolę lud mój spod władzy kraju na wschodzie i kraju na zachodzie słońca. Sprowadzę ich i mieszkać będą w Jeruzalem. I będą moim ludem, a Ja będę ich Bogiem, wiernym i sprawiedliwym.',
+    },
+    vespers: {
+      sigla: '2 Kor 8, 9',
+      passage:
+        'Znacie łaskę Pana naszego Jezusa Chrystusa, który będąc bogaty, dla was stał się ubogim, aby was ubóstwem swoim ubogacić.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'narodzenie-panskie-jutrznia' },
+      { name: 'nieszpory', slug: 'narodzenie-panskie-nieszpory' },
+    ],
+  },
+  {
+    date: '2023-12-29',
+    week: 2,
+    season: 'christmas',
+    passages: ['1 J 2, 3-11', 'Ps 96', ' Łk 2, 22-35'],
+    holidays: ['OKTAWA NARODZENIA PAŃSKIEGO'],
+    notes: ['[HTML]Tomasza Becketa[HTML] bp. i m.'],
+    lauds: {
+      sigla: 'Iz 12, 5-6',
+      passage:
+        'Śpiewajcie Panu, bo czynów wspaniałych dokonał i cała ziemia niech o tym się dowie. Wznoś okrzyki i wołaj radośnie, mieszkanko Syjonu, bo wielki jest wśród ciebie Święty Izraela.',
+    },
+    vespers: {
+      sigla: 'Flp 2, 6-7',
+      passage:
+        'Chrystus Jezus, istniejąc w postaci Bożej, nie skorzystał ze sposobności, by na równi być z Bogiem, lecz ogołocił samego siebie przyjąwszy postać sługi i stał się podobnym do ludzi.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'narodzenie-panskie-jutrznia' },
+      { name: 'nieszpory', slug: 'narodzenie-panskie-nieszpory' },
+    ],
+  },
+  {
+    date: '2023-12-30',
+    week: 2,
+    season: 'christmas',
+    passages: ['1 J 2, 12-17', 'Ps 96', 'Łk 2, 36-40'],
+    holidays: ['OKTAWA NARODZENIA PAŃSKIEGO'],
+    lauds: {
+      sigla: 'Iz 9, 5',
+      passage:
+        'Dziecię nam się narodziło, Syn został nam dany, na Jego barkach spoczęła władza. Nazwano Go imieniem: Przedziwny Doradca, Bóg Mocny, Odwieczny Ojciec, Książę pokoju.',
+    },
+    vespers: {
+      sigla: 'por. 2 P 1, 3-4',
+      passage:
+        'Chrystus swoją Boską wszechmocą udzielił nam tego wszystkiego, co się odnosi do życia i pobożności, przez poznanie Tego, który powołał nas swoją chwałą i doskonałością. Przez nie zostały nam udzielone drogocenne i największe obietnice, abyście przez nie stali się uczestnikami Boskiej natury, gdy już wyrwaliście się z zepsucia wynikającego z pożądliwości na świecie.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'narodzenie-panskie-jutrznia' },
+      { name: 'nieszpory', slug: 'narodzenie-panskie-nieszpory' },
+    ],
+  },
+  {
+    date: '2023-12-31',
+    week: 1,
+    season: 'christmas',
+    passages: [
+      'Rdz 15, 1-6; 21, 1-3',
+      'Ps 105',
+      'Hbr 11, 8.11-12.17-19',
+      'Łk 2, 22-40',
+    ],
+    holidays: ['OKTAWA NARODZENIA PAŃSKIEGO'],
+    notes: ['[HTML]Sylwestra[HTML] bp. Rzymu'],
+    lauds: {
+      sigla: 'Iz 4, 2-3',
+      passage:
+        'W owym dniu Odrośl Pana stanie się ozdobą i chwałą, a owoc ziemi chlubą i pięknem dla ocalałych z Izraela. Każdy kto pozostał żywy na Syjonie i kto się ostał w Jeruzalem, będzie nazwany świętym i wpisany do księgi życia w Jeruzalem.',
+    },
+    vespers: {
+      sigla: 'Ga 4, 4-5',
+      passage:
+        'Gdy nadeszła pełnia czasu, zesłał Bóg Syna swego, zrodzonego z niewiasty, zrodzonego pod Prawem, aby wykupił tych, którzy podlegali Prawu, abyśmy mogli otrzymać przybrane synostwo.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'narodzenie-panskie-jutrznia' },
+      { name: 'nieszpory', slug: 'narodzenie-panskie-nieszpory' },
+      { name: 'liturgia słowa', slug: 'swietej-rodziny-rok-b' },
+    ],
+  },
+];
