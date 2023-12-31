97c481a364a44d5b6ce612293c882cc3c7823a17
Anna Wróbel <anna-wro@users.noreply.github.com>
2023-12-31 14:24:24 +0100

    📆 January 2024 (#317)

---
+++ src/calendar/2024/2024-01.ts
@@ -0,0 +1,362 @@
+import type { CalendarType } from '../calendar';
+
+export const January2024: CalendarType = [
+  {
+    date: '2024-01-01',
+    week: 1,
+    season: 'christmas',
+    passages: ['Iz 9, 1-3, 5-6', 'Ps 8', 'Flp 2, 1-11', 'Łk 2, 21-24'],
+    holidays: ['NADANIE IMIENIA JEZUSOWI'],
+    lauds: {
+      sigla: 'Iz 7, 14',
+      passage:
+        'Dlatego Pan sam da wam znak: Oto Panna pocznie i porodzi Syna, i nazwie Go imieniem Emmanuel.',
+    },
+    vespers: {
+      sigla: 'Flp 2, 9-11',
+      passage:
+        'Dlatego też Bóg Go nad wszystko wywyższył i darował Mu imię ponad wszelkie imię, aby na imię Jezusa zgięło się każde kolano istot niebieskich i ziemskich i podziemnych. I aby wszelki język wyznał, że Jezus Chrystus jest PANEM – ku chwale Boga Ojca.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'narodzenie-panskie-jutrznia' },
+      { name: 'nieszpory', slug: 'narodzenie-panskie-nieszpory' },
+    ],
+  },
+  {
+    date: '2024-01-02',
+    week: 1,
+    season: 'christmas',
+    passages: ['1 J 2, 22-28', 'Ps 98', 'J 1, 19-28'],
+    notes: [
+      '[HTML]Bazylego Wielkiego[HTML] bp',
+      '[HTML]Grzegorza z Nazjanzu[HTML] bp',
+    ],
+  },
+  {
+    date: '2024-01-03',
+    week: 1,
+    season: 'christmas',
+    passages: ['1 J 2, 29-3, 6', 'Ps 98', 'J 1, 29-34'],
+    notes: ['[HTML]Mariana Piątkowskiego[HTML] prezb.'],
+  },
+  {
+    date: '2024-01-04',
+    week: 1,
+    season: 'christmas',
+    passages: ['1 J 3, 7-10', 'Ps 98', 'J 1, 35-42'],
+    notes: ['[HTML]Grzegorza z Langres[HTML] bp'],
+  },
+  {
+    date: '2024-01-05',
+    week: 1,
+    season: 'christmas',
+    passages: ['1 J 3, 11-21', 'Ps 100', 'J 1, 43-51'],
+    notes: ['[HTML]Synkletyki[HTML] mn.'],
+    links: [{ name: 'wigilia Epifanii', slug: 'epifania-wigilia' }],
+  },
+  {
+    date: '2024-01-06',
+    week: 1,
+    season: 'christmas',
+    holidays: ['EPIFANIA'],
+    passages: ['Iz 60, 1-6', 'Ps 72', 'Ef 3, 2-3a, 5-6', 'Mt 2, 1-12'],
+    lauds: {
+      sigla: 'Iz 52, 7-10',
+      passage:
+        'Jakże są pożądane kroki posłańca na szczytach górskich, który nam ogłasza pokój, zwiastuje szczęście, obwieszcza zbawienie, który mówi do Syjonu: „Twój Bóg zaczął królować”. Głos! Twoi strażnicy podnoszą głos, razem wznoszą okrzyki radosne, bo oglądają na własne oczy powrót Pana na Syjon. Zabrzmijcie radosnym śpiewem, wszystkie ruiny Jeruzalem! Bo Pan pocieszył swój lud, odkupił Jeruzalem. Pan obnażył już swe ramię święte wobec wszystkich narodów, i wszystkie krańce ziemi zobaczą zbawienie naszego Boga.',
+    },
+    vespers: {
+      sigla: 'Tt 3, 4-5',
+      passage:
+        'Ukazała się dobroć i miłość Zbawiciela, naszego Boga, do ludzi, nie dla uczynków sprawiedliwych, jakie zdziałaliśmy, lecz z miłosierdzia swego zbawił nas przez obmycie odradzające i odnawiające w Duchu Świętym.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+  },
+  {
+    date: '2024-01-07',
+    week: 2,
+    season: 'christmas',
+    holidays: ['NIEDZIELA W OKTAWIE EPIFANII'],
+    passages: ['Iz 2, 1-5', 'Ps 98', 'Rz 10, 9-18', 'Mt 2, 13-23'],
+    lauds: {
+      sigla: 'Iz 9, 5',
+      passage:
+        'Dziecię nam się narodziło, Syn został nam dany, na Jego barkach spoczęła władza. Nazwano Go imieniem: Przedziwny Doradca, Bóg Mocny, Odwieczny Ojciec, Książę Pokoju.',
+    },
+    vespers: {
+      sigla: 'Mdr 7, 26-27',
+      passage:
+        'Mądrość jest odblaskiem wieczystej światłości, zwierciadłem bez skazy działania Boga, obrazem Jego dobroci. Jedna jest, a wszystko może, pozostając sobą, wszystko odnawia, a przez pokolenia zstępując w dusze święte, wzbudza przyjaciół Bożych i proroków.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+  },
+  {
+    date: '2024-01-08',
+    week: 2,
+    season: 'christmas',
+    holidays: ['OKTAWA EPIFANII'],
+    passages: ['1 J 4, 7-10', 'Ps 72', 'Mk 6, 34-44'],
+    lauds: {
+      sigla: 'Iz 60, 1-3',
+      passage:
+        'Powstań! Świeć, bo przyszło twe światło i chwała Pańska rozbłyska nad tobą. Bo oto ciemność okrywa ziemię i gęsty mrok spowija ludy, a ponad tobą jaśnieje Pan, i Jego chwała jawi się nad tobą. I pójdą narody do twojego światła, królowie do blasku twojego wschodu.',
+    },
+    vespers: {
+      sigla: 'Iz 60, 4-6',
+      passage:
+        'Podnieś oczy wokoło i popatrz: Ci wszyscy zebrani zdążają do ciebie. Twoi synowie przychodzą z daleka, na rękach niesione twe córki. Wtedy zobaczysz i promienieć będziesz, a serce twe zadrży i rozszerzy się, bo do ciebie napłyną bogactwa zamorskie, zasoby narodów przyjdą ku tobie. Zaleje cię mnogość wielbłądów – dromadery z Madianu i z Efy. Wszyscy oni przybędą ze Saby, przyniosą złoto i kadzidło i będą z radością głosić chwałę Pana.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+  },
+  {
+    date: '2024-01-09',
+    week: 2,
+    season: 'christmas',
+    holidays: ['OKTAWA EPIFANII'],
+    passages: ['1 J 4, 11-18', 'Ps 72', 'Mk 6, 45-52'],
+    lauds: {
+      sigla: '1 J 1, 5',
+      passage:
+        'Nowina, którą usłyszeliśmy od Niego i którą wam głosimy, jest taka: Bóg jest światłością, a nie ma w Nim żadnej ciemności.',
+      antiphon:
+        'Ty jesteś moim Synem umiłowanym, * w Tobie złożyłem swoje postanowienia.',
+    },
+    vespers: {
+      sigla: 'Kol 1, 13-15',
+      passage:
+        'Bóg nas uwolnił spod władzy ciemności i przeniósł do królestwa swego umiłowanego Syna, w którym mamy odkupienie, odpuszczenie grzechów. On jest obrazem Boga niewidzialnego, Pierworodnym wobec każdego stworzenia.',
+      antiphon:
+        'Oto Baranek Boży, * który gładzi grzechy świata. On przez wodę i Ducha uświęca wszystkich ludzi. / Oto znaleźliśmy Mesjasza. / Alleluja.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+  },
+  {
+    date: '2024-01-10',
+    week: 2,
+    season: 'christmas',
+    passages: ['1 J 4, 19-5, 4', 'Ps 72', 'Łk 4, 14-22a'],
+    holidays: ['OKTAWA EPIFANII'],
+    lauds: {
+      sigla: '1 J 5, 1',
+      passage:
+        'Każdy, kto wierzy, że Jezus jest Mesjaszem, z Boga się narodził, i każdy miłujący Tego, który dał życie, miłuje również tego, który życie od Niego otrzymał.',
+    },
+    vespers: {
+      sigla: 'Iz 2, 3b-4',
+      passage:
+        'Prawo wyjdzie z Syjonu i słowo Pańskie z Jeruzalem. On będzie rozjemcą pomiędzy ludami i wyda wyroki dla licznych narodów. Wtedy swe miecze przekują na lemiesze, a swoje włócznie na sierpy. Naród przeciw narodowi nie podniesie miecza, nie będą się więcej zaprawiać do wojny.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+  },
+  {
+    date: '2024-01-11',
+    week: 2,
+    season: 'christmas',
+    holidays: ['OKTAWA EPIFANII'],
+    passages: ['1 J 5, 5-13', 'Ps 147', 'Łk 5, 12-16'],
+    lauds: {
+      sigla: '1 J 4, 9',
+      passage:
+        'W tym objawiła się miłość Boga ku nam, że zesłał Syna swego Jednorodzonego na świat, abyśmy życie mieli dzięki Niemu.',
+    },
+    vespers: {
+      sigla: '1 J 4, 10',
+      passage:
+        'W tym przejawia się miłość, że nie my umiłowaliśmy Boga, ale że On sam nas umiłował i posłał Syna swojego jako ofiarę przebłagalną za nasze grzechy.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+  },
+  {
+    date: '2024-01-12',
+    passages: ['1 J 5, 14-21', 'Ps 149', 'J 3, 22-30'],
+    holidays: ['OKTAWA EPIFANII'],
+    week: 2,
+    season: 'christmas',
+    lauds: {
+      sigla: 'Rz 11, 33-36',
+      passage:
+        'O głębokości bogactw, mądrości i wiedzy Boga! Jakże niezbadane są Jego wyroki i nie do wyśledzenia Jego drogi! Kto bowiem poznał myśl Pana, albo kto był Jego doradcą? Lub kto Go pierwszy obdarował, aby nawzajem otrzymać odpłatę? Albowiem z Niego i przez Niego, i dla Niego jest wszystko. Jemu chwała na wieki! Amen.',
+    },
+    vespers: {
+      sigla: '1 Krn 29, 11-13',
+      passage:
+        'Twoja jest, o Panie wielkość, moc, sława, majestat i chwała, bo wszystko, co jest na niebie i na ziemi, jest Twoje; do Ciebie, Panie, należy królowanie i ten, co głowę wznosi ponad wszystkich. Bogactwo i chwała od Ciebie pochodzą, Ty nad wszystkim panujesz, a w ręku Twoim siła i moc, i ręką Twoją wywyższasz i utwierdzasz wszystko. Teraz więc, Boże nasz, dzięki Ci składamy i wychwalamy przesławne imię Twoje.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+    notes: ['[HTML]Elreda z Rievaulx[HTML] mn.'],
+  },
+  {
+    date: '2024-01-13',
+    week: 2,
+    season: 'christmas',
+    holidays: ['EPIFANIA NAD JORDANEM'],
+    passages: ['Iz 42, 1-7', 'Ps 29', 'Dz 10, 34-38', 'Mk 1, 7-11'],
+    lauds: {
+      sigla: 'Iz 42, 1-4',
+      passage:
+        'Oto mój Sługa, którego podtrzymuję. Wybrany mój, w którym mam upodobanie. Sprawiłem, że Duch mój na Nim spoczął; On przyniesie narodom Prawo. Nie będzie wołał ni podnosił głosu, nie da słyszeć krzyku swego na dworze. Nie złamie trzciny nadłamanej, nie zagasi knotka o nikłym płomyku. On niezachwianie przyniesie Prawo. Nie zniechęci się ani nie załamie, aż utrwali Prawo na ziemi, a Jego pouczenia wyczekują wyspy.',
+    },
+    vespers: {
+      sigla: 'Iz 42, 6-7',
+      passage:
+        'Ja, Pan, powołałem Cię słusznie, ująłem Cię za rękę i ukształtowałem, ustanowiłem Cię przymierzem dla ludzi, światłością dla narodów, abyś otworzył oczy niewidomym, ażebyś z zamknięcia wypuścił jeńców, z więzienia tych, co mieszkają w ciemności.',
+    },
+    links: [
+      { name: 'jutrznia', slug: 'epifania-jutrznia' },
+      { name: 'nieszpory', slug: 'epifania-nieszpory' },
+    ],
+    notes: ['[HTML]Hilarego[HTML] bp i teo'],
+  },
+  {
+    date: '2024-01-14',
+    week: 1,
+    holidays: ['EPIFANIA W KANIE'],
+    season: 'christmas',
+    passages: ['Iz 62, 1-5', 'Ps 96', '1 Kor 12, 4-11', 'J 2, 1-12'],
+    notes: ['[HTML]Nino[HTML] ap'],
+    vespers: {
+      antiphon:
+        'Kiedy zabrakło wina, * Jezus kazał napełnić stągwie wodą, / a ona stała się winem, / alleluja.',
+    },
+    lauds: {
+      antiphon:
+        'Pierwszy z cudownych znaków * uczynił Jezus w Kanie Galilejskiej. / Ukazał swą chwałę. / Jego uczniowie uwierzyli w Niego.',
+    },
+  },
+  {
+    date: '2024-01-15',
+    week: 1,
+    season: 'christmas',
+    passages: ['1 Sm 15, 16-23', 'Ps 50', 'Mk 2, 18-22'],
+    notes: ['[HTML]Marcina Lutra Kinga[HTML] m.'],
+  },
+  {
+    date: '2024-01-16',
+    week: 1,
+    season: 'christmas',
+    passages: ['1 Sm 16, 1-13', 'Ps 89', 'Mk 2, 23-28'],
+  },
+  {
+    date: '2024-01-17',
+    week: 1,
+    season: 'christmas',
+    passages: ['1 Sm 17, 32-33, 37, 40-51', 'Ps 144', 'Mk 3, 1-6'],
+    notes: ['[HTML]Antoniego[HTML] mn.', '[HTML]Pachomiusza[HTML] mn.'],
+  },
+  {
+    date: '2024-01-18',
+    week: 1,
+    season: 'christmas',
+    passages: ['Iz 52, 7-10', 'Ps 23', 'Mt 16, 13-19'],
+    holidays: ['WYZNANIE ŚWIĘTEGO PIOTRA'],
+    notes: ['Rozpoczyna się Tydzień modlitw o jedność chrześcijan'],
+  },
+  {
+    date: '2024-01-19',
+    week: 1,
+    season: 'christmas',
+    passages: ['1 Sm 24, 3-21', 'Ps 57', 'Mk 3, 13-19'],
+    isFastingDay: true,
+  },
+  {
+    date: '2024-01-20',
+    week: 1,
+    season: 'christmas',
+    passages: ['2 Sm 1, 1-4, 11-12, 19, 23-27', 'Ps 80', 'Mk 3, 20-21'],
+    notes: ['[HTML]Sebastiana[HTML] m.'],
+  },
+  {
+    date: '2024-01-21',
+    week: 2,
+    season: 'christmas',
+    holidays: ['3. NIEDZIELA PO EPIFANII'],
+    passages: ['Jon 3, 1-5, 10', 'Ps 25', '1 Kor 7, 29-31', 'Mk 1, 14-20'],
+  },
+  {
+    date: '2024-01-22',
+    week: 2,
+    season: 'christmas',
+    passages: ['2 Sm 5, 1-7, 10', 'Ps 89', 'Mk 3, 22-30'],
+  },
+  {
+    date: '2024-01-23',
+    week: 2,
+    season: 'christmas',
+    passages: ['2 Sm 6, 12b-15, 17-19', 'Ps 24', 'Mk 3, 31-35'],
+  },
+  {
+    date: '2024-01-24',
+    week: 2,
+    season: 'christmas',
+    passages: ['2 Sm 7, 4-17', 'Ps 89', 'Mk 4, 1-20'],
+  },
+  {
+    date: '2024-01-25',
+    week: 2,
+    season: 'christmas',
+    passages: ['Dz 9, 1-22', 'Ps 117', 'Mk 16, 15-18'],
+    holidays: ['NAWRÓCENIE ŚWIĘTEGO PAWŁA'],
+    notes: ['Kończy się Tydzień modlitw o jedność chrześcijan'],
+  },
+  {
+    date: '2024-01-26',
+    week: 2,
+    season: 'christmas',
+    passages: ['2 Sm 11, 1-4a, 5-10a, 13-17', 'Ps 51', 'Mk 4, 26-34'],
+    notes: ['[HTML]Tymoteusza[HTML] bp', '[HTML]Tytusa[HTML] bp'],
+  },
+  {
+    date: '2024-01-27',
+    week: 2,
+    season: 'christmas',
+    passages: ['2 Sm 12, 1-7a, 10-17', 'Ps 51', 'Mk 4, 35-41'],
+    notes: ['[HTML]Jana Chryzostoma[HTML] bp'],
+  },
+  {
+    date: '2024-01-28',
+    week: 1,
+    season: 'christmas',
+    passages: ['Pwt 18, 15-20', 'Ps 95', '1 Kor 7, 32-35', 'Mk 1, 21-28'],
+    holidays: ['4. NIEDZIELA PO EPIFANII'],
+    notes: ['[HTML]Tomasza z Akwinu[HTML] prezb. teo'],
+  },
+  {
+    date: '2024-01-29',
+    week: 1,
+    season: 'christmas',
+    passages: ['2 Sm 15, 13-14, 30; 16, 5-13', 'Ps 3', 'Mk 5, 1-20'],
+  },
+  {
+    date: '2024-01-30',
+    week: 1,
+    season: 'christmas',
+    passages: ['2 Sm 18, 9-10, 14b, 24-25a, 30-19, 3', 'Ps 86', 'Mk 5, 21-43'],
+  },
+  {
+    date: '2024-01-31',
+    week: 1,
+    season: 'christmas',
+    passages: ['2 Sm 24, 2, 9-17', 'Ps 32', 'Mk 6, 1-6'],
+  },
+];
