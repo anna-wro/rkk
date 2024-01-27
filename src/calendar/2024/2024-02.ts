ac6386948170067cedb2af0a2c8908a171a62694
Klaudia Sowiak <klaudia.sowiak@gmail.com>
2024-01-27 21:38:42 +0100

    february2023

---
+++ src/calendar/2024/2024-02.ts
@@ -0,0 +1,266 @@
+import type { CalendarType } from '../calendar';
+
+export const February2024: CalendarType = [
+  {
+    date: '2024-02-01',
+    week: 1,
+    season: 'christmas',
+    passages: ['1 Krl 2.1-4.10-12', '1 Krn 29', 'Mk 6, 7-13'],
+    notes: ['[HTML]Brygidy z Kildare[HTML] mn.'],
+    links: [{ name: 'wigilia Hypapante', slug: 'hypapante-wigilia' }],
+  },
+  {
+    date: '2024-02-02',
+    week: 1,
+    season: 'christmas',
+    passages: ['Ml 3, 1-4', 'Ps 24', 'Hbr 2, 14-18', 'Łk 2, 22-40'],
+    holidays: ['HYPAPANTE'],
+    links: [
+      { name: 'jutrznia', slug: 'hypapante-jutrznia' },
+      { name: 'nieszpory', slug: 'hypapante-nieszpory' },
+      {
+        name: 'liturgia słowa',
+        slug: 'czytania/hypapante-2-luetgo',
+      },
+    ],
+  },
+  {
+    date: '2024-02-03',
+    week: 1,
+    season: 'ordinary',
+    passages: ['1 Krl 3.4-13', 'Ps 119', 'Mk 6, 30-34'],
+    notes: ['[HTML]Błażeja[HTML] bp. i m.', '[HTML]Oskara[HTML] bp.'],
+  },
+  {
+    date: '2024-02-04',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Hi 7, 1-4.6-7', 'Ps 147', '1Kor 9, 16-19.22-23', 'Mk 1, 29-39'],
+    holidays: ['5. NIEDZIELA PO EPIFANII'],
+    links: [
+      {
+        name: 'liturgia słowa',
+        slug: 'czytania/5-niedziela-w-ciagu-roku-rok-b',
+      },
+    ],
+  },
+  {
+    date: '2024-02-05',
+    week: 2,
+    season: 'ordinary',
+    passages: ['1Krl 8, 1-7.9-13', 'Ps 132', 'Mk 6, 53-56'],
+  },
+  {
+    date: '2024-02-06',
+    week: 2,
+    season: 'ordinary',
+    passages: ['1Krl 8, 22-23.27-30', 'Ps 84', ' Mk 7, 1-13'],
+  },
+  {
+    date: '2024-02-07',
+    week: 2,
+    season: 'ordinary',
+    passages: ['1Krl 10, 1-10', 'Ps 37', 'Mk 7, 14-23'],
+    notes: ['[HTML]Felicji Sieradzkiej[HTML] zak.'],
+  },
+  {
+    date: '2024-02-08',
+    week: 2,
+    season: 'ordinary',
+    passages: ['1 Krl 11, 4-13', 'Ps 106', ' Mk 7, 24-30'],
+    notes: ['[HTML]Józefiny Bakhity[HTML] zak.'],
+  },
+  {
+    date: '2024-02-09',
+    week: 2,
+    season: 'ordinary',
+    passages: ['1Krl 11, 29-32; 12, 19', ' Ps 81', ' Mk 7, 31-37'],
+  },
+  {
+    date: '2024-02-10',
+    week: 2,
+    season: 'ordinary',
+    passages: ['1 Krl 12, 26-32; 13, 33-34', 'Ps 106', 'Mk 8, 1-10'],
+    notes: ['[HTML]Scholastyki[HTML] mn.'],
+  },
+  {
+    date: '2024-02-11',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Kpł 13, 1-2.44-46', 'Ps 32', '1Kor 10, 31-11.1', 'Mk 1, 40-45'],
+    holidays: ['6. NIEDZIELA PO EPIFANII'],
+    links: [
+      {
+        name: 'liturgia słowa',
+        slug: 'czytania/6-niedziela-w-ciagu-roku-rok-b',
+      },
+    ],
+  },
+  {
+    date: '2024-02-12',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Jk 1, 1-11', 'Ps 119', 'Mk 8, 11-13'],
+    notes: ['[HTML]Dorothy Stang[HTML] zak. i m.'],
+  },
+  {
+    date: '2024-02-13',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Jk 1, 12-18', 'Ps 94', 'Mk 8, 14-21'],
+  },
+  {
+    date: '2024-02-14',
+    week: 1,
+    season: 'lent',
+    passages: ['Jl 2, 12-18', 'Ps 51', '2 Kor 5, 20-6.2', 'Mt 6, 1-6, 16-18'],
+    holidays: ['ŚRODA POPIELCOWA'],
+    isFastingDay: true,
+    links: [
+      {
+        name: 'liturgia słowa',
+        slug: 'czytania/sroda-popielcowa',
+      },
+    ],
+  },
+  {
+    date: '2024-02-15',
+    week: 1,
+    season: 'lent',
+    passages: ['Pwt 30, 15-20', 'Ps 1', 'Łk 9, 22-25'],
+    notes: ['[HTML]Filemona[HTML] misj.', '[HTML]Onezyma[HTML] bp.'],
+  },
+  {
+    date: '2024-02-16',
+    week: 1,
+    season: 'lent',
+    passages: ['Iz 58, 1-9a', 'Ps 51', 'Mt 9, 14-15'],
+    isFastingDay: true,
+  },
+  {
+    date: '2024-02-17',
+    week: 1,
+    season: 'lent',
+    passages: ['Dz 13, 46-49', 'Ps 117 (116), 1-2', 'Łk 10, 1-9'],
+    notes: [
+      '[HTML]Cyryla[HTML] mn.',
+      '[HTML]Metodego[HTML] bp.',
+      'Święto patronalne Parafii w Poznaniu',
+    ],
+  },
+  {
+    date: '2024-02-18',
+    week: 2,
+    season: 'lent',
+    passages: ['Rdz 9, 8-15', 'Ps 25', '1 P 3, 18-22', ' Mk 1, 12-15'],
+    holidays: ['1. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
+    lauds: {
+      antiphon:
+        'Duch wyprowadził Jezusa na pustynię, * aby kuszony był przez diabła. / A gdy przepościł czterdzieści dni i nocy, / głód wielki potem odczuwał.',
+    },
+    eve: {
+      antiphon:
+        'Nie samym chlebem żyje człowiek, * lecz każdym słowem, / które pochodzi z ust Bożych.',
+    },
+    vespers: {
+      antiphon:
+        'Czuwaj nad nami, o Zbawicielu, * by nas nie pochwycił przebiegły kusiciel; / Ty bowiem jesteś naszą odwieczną pomocą.',
+    },
+    links: [
+      {
+        name: 'liturgia słowa',
+        slug: 'czytania/1-niedziela-przygotowania-paschalnego-rok-b',
+      },
+    ],
+  },
+  {
+    date: '2024-02-19',
+    week: 1,
+    season: 'lent',
+    passages: ['Kpł 19, 1-2.11-18', 'Ps 19', 'Mt 25, 31-46'],
+  },
+  {
+    date: '2024-02-20',
+    week: 1,
+    season: 'lent',
+    passages: ['Iz 55, 10-11', 'Ps 34', 'Mt 6, 7-15'],
+  },
+  {
+    date: '2024-02-21',
+    week: 1,
+    season: 'lent',
+    passages: ['Jon 3, 1-10', 'Ps 51', 'Łk 11, 29-32'],
+    notes: ['[HTML]Jerzego Nowosielskiego[HTML] w.'],
+  },
+  {
+    date: '2024-02-22',
+    week: 1,
+    season: 'lent',
+    passages: ['Est 4, 17k.l-m.r-u', 'Ps 138', ' Mt 7, 7-12'],
+    notes: ['[HTML]Polikarpa[HTML] bp. i m.'],
+  },
+  {
+    date: '2024-02-23',
+    week: 1,
+    season: 'lent',
+    passages: ['Ez 18, 21-28', 'Ps 130', 'Mt 5, 20-26'],
+  },
+  {
+    date: '2024-02-24',
+    week: 1,
+    season: 'lent',
+    passages: ['Pwt 26, 16-19', 'Ps 119', 'Mt 5, 43-48'],
+  },
+  {
+    date: '2024-02-25',
+    week: 2,
+    season: 'lent',
+    // eslint-disable-next-line prettier/prettier
+    passages: ['Rdz 22, 1-2.9a.10-13.15-18', 'Ps 116', 'Rz 8, 31b-34', 'Mk 9, 2-10'],
+    holidays: ['2. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
+    eve: {
+      antiphon:
+        'Odezwał się głos z obłoku: * To jest mój Syn umiłowany, / w którym mam upodobanie, / Jego słuchajcie.',
+    },
+    lauds: {
+      antiphon:
+        'Nasz Pan, Jezus Chrystus, śmierć zniweczył * i przez Ewangelię dał nam światło życia wiecznego.',
+    },
+    vespers: {
+      antiphon:
+        'Nie mówcie nikomu o tym widzeniu, * aż Syn Człowieczy zmartwychwstanie.',
+    },
+    links: [
+      {
+        name: 'liturgia słowa',
+        slug: 'czytania/2-niedziela-przygotowania-paschalnego-rok-b',
+      },
+    ],
+  },
+  {
+    date: '2024-02-26',
+    week: 2,
+    season: 'lent',
+    passages: ['Dn 9, 4b-10', 'Ps 78', 'Łk 6, 36-38'],
+  },
+  {
+    date: '2024-02-27',
+    week: 2,
+    season: 'lent',
+    passages: ['Iz 1, 10.16-20', 'Ps 50', 'Mt 23, 1-12'],
+  },
+  {
+    date: '2024-02-28',
+    week: 2,
+    season: 'lent',
+    passages: ['Jr 18, 18-20', 'Ps 31', 'Mt 20, 17-28'],
+    notes: ['[HTML]Johanna Josepha Ignazego von Döllingera[HTML] prezb.'],
+  },
+
+  {
+    date: '2024-02-29',
+    week: 2,
+    season: 'lent',
+    passages: ['Jr 17, 5-10', 'Ps 1', 'Łk 16, 19-31'],
+  },
+];
--- src/calendar/2024/index.ts
+++ src/calendar/2024/index.ts
@@ -1,3 +1,4 @@
 import { January2024 } from './2024-01';
+import { February2024 } from './2024-02';
 
-export const Calendar2024 = [January2024];
+export const Calendar2024 = [January2024, February2024];
