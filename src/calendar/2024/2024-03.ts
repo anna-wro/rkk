281b3993fb7e3a47b22d29591496e2f26a0a2ddb
Anna Wróbel <wro.emails@gmail.com>
2024-03-01 02:51:15 +0100

    📆 March 2023

---
+++ src/calendar/2024/2024-03.ts
@@ -0,0 +1,337 @@
+import type { CalendarType } from '../calendar';
+
+export const March2024: CalendarType = [
+  {
+    date: '2024-03-01',
+    week: 2,
+    season: 'lent',
+    passages: ['Rdz 37, 3-4, 12-13a, 17b-28a', 'Ps 105', 'Mt 21, 33-43, 45-46'],
+    isFastingDay: true,
+  },
+  {
+    date: '2024-03-02',
+    week: 2,
+    season: 'lent',
+    passages: ['Mi 7, 14-15, 18-20', 'Ps 103', 'Łk 15, 1-3, 11-32'],
+  },
+  {
+    date: '2024-03-03',
+    week: 1,
+    season: 'lent',
+    passages: ['Wj 17, 3-7', 'Ps 95', 'Rz 5, 1-2.5-8', 'J 4, 5-42'],
+    holidays: ['3. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
+    eve: {
+      antiphon:
+        'Usprawiedliwieni przez wiarę * żyjmy w pokoju z Bogiem / przez naszego Pana, Jezusa Chrystusa.',
+    },
+    lauds: {
+      antiphon:
+        'Pan mówi: * kto będzie pił wodę, którą Ja mu dam, / nie będzie pragnął na wieki.',
+    },
+    vespers: {
+      antiphon:
+        'Wierzymy i jesteśmy przekonani, * że Chrystus jest prawdziwie Zbawicielem świata.',
+    },
+    notes: ['[HTML]Jana i Karola Wesleyów[HTML] prezb.'],
+  },
+  {
+    date: '2024-03-04',
+    week: 1,
+    season: 'lent',
+    passages: ['2 Krl 5, 1-15a', 'Ps 42', 'Łk 4, 24-30'],
+  },
+  {
+    date: '2024-03-05',
+    week: 1,
+    season: 'lent',
+    passages: ['Dn 3, 25, 34-43', 'Ps 25', 'Mt 18, 21-35'],
+  },
+  {
+    date: '2024-03-06',
+    week: 1,
+    season: 'lent',
+    passages: ['Pwt 4, 1, 5-9', 'Ps 147', 'Mt 5, 17-19'],
+  },
+  {
+    date: '2024-03-07',
+    week: 1,
+    season: 'lent',
+    passages: ['Jr 7, 23-28', 'Ps 95', 'Łk 11, 14-23'],
+    notes: ['[HTML]Perpetuy i Felicyty[HTML] m.'],
+  },
+  {
+    date: '2024-03-08',
+    week: 1,
+    season: 'lent',
+    passages: ['Oz 14, 2-10', 'Ps 81', 'Mk 12, 28b-34'],
+    isFastingDay: true,
+  },
+  {
+    date: '2024-03-09',
+    week: 1,
+    season: 'lent',
+    passages: ['Oz 6, 1-6', 'Ps 51', 'Łk 18, 9-14'],
+  },
+  {
+    date: '2024-03-10',
+    week: 2,
+    season: 'lent',
+    passages: ['1Sm 16, 1-13', 'Ps 23', 'Ef 5, 8-14', 'J 9, 1-41'],
+    holidays: ['4. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
+    eve: {
+      antiphon:
+        'Niegdyś byliście ciemnością, * lecz teraz jesteście światłością w Panu.',
+    },
+    lauds: {
+      antiphon:
+        'Panie, kto zawinił, * ten człowiek, czy jego rodzice, że się urodził niewidomy? / Jezus odpowiedział: Ani on nie zawinił, ani jego rodzice: / ale stało się tak, aby się na nim objawiły sprawy Boże.',
+    },
+    vespers: {
+      antiphon:
+        'Człowiek imieniem Jezus * pomazał mi oczy błotem, / i kazał mi się obmyć, i teraz widzę.',
+    },
+  },
+  {
+    date: '2024-03-11',
+    week: 2,
+    season: 'lent',
+    passages: ['Iz 65, 17-21', 'Ps 30', 'J 4, 43-54'],
+    notes: ['[HTML]Matek i ojców monastycyzmu[HTML]'],
+  },
+  {
+    date: '2024-03-12',
+    week: 2,
+    season: 'lent',
+    passages: ['Ez 47, 1-9, 12', 'Ps 46', 'J 5, 1-3a, 5-16'],
+    notes: ['[HTML]Grzegorza Wielkiego[HTML] bp.'],
+  },
+  {
+    date: '2024-03-13',
+    week: 2,
+    season: 'lent',
+    passages: ['Iz 49, 8-15', 'Ps 145', 'J 5, 17-30'],
+  },
+  {
+    date: '2024-03-14',
+    week: 2,
+    season: 'lent',
+    passages: ['Wj 32, 7-14', 'Ps 106', 'J 5, 31-47'],
+  },
+  {
+    date: '2024-03-15',
+    week: 2,
+    season: 'lent',
+    passages: ['Mdr 2, 1a, 12-22', 'Ps 34', 'J 7, 1-2, 10, 25-30'],
+    isFastingDay: true,
+  },
+  {
+    date: '2024-03-16',
+    week: 2,
+    season: 'lent',
+    passages: ['Jr 11, 18-20', 'Ps 7', 'J 7, 40-53'],
+  },
+  {
+    date: '2024-03-17',
+    week: 1,
+    season: 'lent',
+    passages: ['Ez 37, 12-14', 'Ps 130', 'Rz 8, 8-11', 'J 11, 1-45'],
+    holidays: ['5. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
+    eve: {
+      antiphon:
+        'Jeśli ziarno pszenicy * nie wpadnie w ziemię i nie obumrze, / zostanie tylko samo; / lecz jeśli obumrze, / przynosi plon obfity.',
+    },
+    lauds: {
+      antiphon: 'Zasnął przyjaciel nasz Łazarz, * lecz idę, aby go obudzić.',
+    },
+    vespers: {
+      antiphon:
+        'Panie, gdybyś był tutaj, Łazarz by nie umarł, * ale ja i tak wierzę, że jesteś Synem Bożym.',
+    },
+    notes: ['[HTML]Patryka[HTML] bp.'],
+  },
+  {
+    date: '2024-03-18',
+    week: 1,
+    season: 'lent',
+    passages: ['Dn 13, 1-9, 15-17, 19-30, 33-62', 'Ps 23', 'J 8, 1-11'],
+    notes: ['[HTML]Cyryla Jerozolimskiego[HTML] bp. teo.'],
+  },
+  {
+    holidays: ['JÓZEFA Z NAZARETU'],
+    date: '2024-03-19',
+    week: 1,
+    season: 'lent',
+    passages: [
+      '2 Sm 7, 4-5a, 12-14a, 16',
+      'Ps 89',
+      'Rz 4, 13, 16-18, 22',
+      'Mt 1, 16, 18-21, 24',
+    ],
+  },
+  {
+    date: '2024-03-20',
+    week: 1,
+    season: 'lent',
+    passages: ['Dn 3, 14-20, 91-92, 95', 'Dn 3', 'J 8, 31-42'],
+  },
+  {
+    date: '2024-03-21',
+    week: 1,
+    season: 'lent',
+    passages: ['Rdz 17, 3-9', 'Ps 105', 'J 8, 51-59'],
+  },
+  {
+    date: '2024-03-22',
+    week: 1,
+    season: 'lent',
+    passages: ['Jr 20, 10-13', 'Ps 18', 'J 10, 31-42'],
+    isFastingDay: true,
+  },
+  {
+    date: '2024-03-23',
+    week: 1,
+    season: 'lent',
+    passages: ['Ez 37, 21-28', 'Jr 31', 'J 11, 45-56'],
+  },
+  {
+    date: '2024-03-24',
+    week: 2,
+    season: 'pascha',
+    holidays: ['NIEDZIELA PALMOWA'],
+    notes: ['[HTML]Oskara Romero[HTML] bp. m.'],
+    passages: [
+      'Procesja z palmami: Mk 11, 1-10',
+      'Iz 50, 4-7',
+      'Ps 22',
+      'Flp 2, 6-11',
+      'Mk 14, 1-15, 47',
+    ],
+    links: [
+      { name: 'wigilia', slug: 'niedziela-palmowa-wigilia' },
+      { name: 'kompleta', slug: 'niedziela-palmowa-kompleta-1' },
+      { name: 'jutrznia', slug: 'niedziela-palmowa-jutrznia' },
+      { name: 'nieszpory', slug: 'niedziela-palmowa-nieszpory' },
+      { name: 'kompleta', slug: 'niedziela-palmowa-kompleta-2' },
+    ],
+  },
+  {
+    date: '2024-03-25',
+    week: 2,
+    holidays: ['WIELKI PONIEDZIAŁEK'],
+    season: 'pascha',
+    passages: ['Iz 42, 1-7', 'Ps 27', 'J 12, 1-11'],
+    links: [
+      { name: 'jutrznia', slug: 'wielki-poniedzialek-jutrznia' },
+      { name: 'nieszpory', slug: 'wielki-poniedzialek-nieszpory' },
+      { name: 'kompleta', slug: 'wielki-poniedzialek-kompleta' },
+    ],
+  },
+  {
+    date: '2024-03-26',
+    week: 2,
+    season: 'pascha',
+    holidays: ['WIELKI WTOREK'],
+    passages: ['Iz 49, 1-6', 'Ps 71', 'J 13, 21-33. 36-38'],
+    links: [
+      { name: 'jutrznia', slug: 'wielki-wtorek-jutrznia' },
+      { name: 'nieszpory', slug: 'wielki-wtorek-nieszpory' },
+      { name: 'kompleta', slug: 'wielki-wtorek-kompleta' },
+    ],
+    notes: ['[HTML]Dobrego Łotra[HTML]'],
+  },
+  {
+    date: '2024-03-27',
+    week: 2,
+    season: 'pascha',
+    holidays: ['WIELKA ŚRODA'],
+    passages: ['Iz 50, 4-9a', 'Ps 69', 'Mt 26, 14-25'],
+    links: [
+      { name: 'jutrznia', slug: 'wielka-sroda-jutrznia' },
+      { name: 'nieszpory', slug: 'wielka-sroda-nieszpory' },
+      { name: 'kompleta', slug: 'wielka-sroda-kompleta' },
+    ],
+    notes: ['[HTML]Męczenników Trapistów z Tibihirine[HTML]'],
+  },
+  {
+    date: '2024-03-28',
+    week: 2,
+    season: 'pascha',
+    holidays: ['WIELKI CZWARTEK'],
+    passages: [
+      'Iz 61, 1-3a.6a.8b-9',
+      'Ps 89',
+      'Ap 1, 5-8',
+      'Łk 4, 16-21',
+      'LITURGIA WIECZERZY PAŃSKIEJ',
+      'Wj 12, 1-8, 11-14',
+      'Ps 116',
+      '1 Kor 11, 23-26',
+      'J 13, 1-15',
+    ],
+    links: [
+      { name: 'jutrznia', slug: 'wielki-czwartek-jutrznia' },
+      { name: 'wigilia Wielkiego Piątku', slug: 'wielki-piatek-wigilia' },
+      { name: 'godzina czytań', slug: 'wielki-piatek-godzina-czytan' },
+      { name: 'kompleta', slug: 'wielki-piatek-kompleta-1' },
+    ],
+    notes: [
+      'Sprawuje się Liturgię Krzyżma',
+      'Wieczorem rozpoczyna się Triduum Paschalne',
+    ],
+  },
+  {
+    date: '2024-03-29',
+    week: 2,
+    season: 'pascha',
+    holidays: ['WIELKI PIĄTEK', 'LITURGIA MĘKI PAŃSKIEJ'],
+    passages: [
+      'Iz 52, 13-53, 12',
+      'Ps 31',
+      'Hbr 4, 14-16; 5, 7-9',
+      'J 18, 1-19, 42',
+    ],
+    links: [
+      { name: 'jutrznia', slug: 'wielki-piatek-jutrznia' },
+      { name: 'godzina czytań', slug: 'wielki-piatek-godzina-czytan' },
+      { name: 'nieszpory', slug: 'wielki-piatek-nieszpory' },
+      { name: 'kompleta', slug: 'wielki-piatek-kompleta-2' },
+    ],
+    isFastingDay: true,
+  },
+  {
+    date: '2024-03-30',
+    week: 2,
+    season: 'pascha',
+    holidays: ['WIELKA SOBOTA', 'WIGILIA PASCHALNA'],
+    notes: [
+      '[HTML]Jana Klimaka[HTML] mn.',
+      'Tego dnia sprawuje się wyłącznie Liturgię Godzin. Po zmroku rozpoczyna się Wigilia Paschalna',
+    ],
+    passages: ['Czytania wg lekcjonarza Tom 2 str. 101 – 117'],
+    linksTitle: 'Wielka Sobota',
+    links: [
+      { name: 'jutrznia', slug: 'wielka-sobota-jutrznia' },
+      { name: 'godzina czytań', slug: 'wielka-sobota-godzina-czytan' },
+      { name: 'w ciągu dnia', slug: 'wielka-sobota-w-ciagu-dnia' },
+    ],
+    isFastingDay: true,
+  },
+  {
+    date: '2024-03-31',
+    week: 1,
+    season: 'easter',
+    holidays: ['PASCHA'],
+    passages: ['Dz 10, 34a. 37-43', 'Ps 118', '1 Kor 5,6-8', 'J 20, 1-9'],
+    notes: [
+      'Wieczorem kończy się Triduum Paschalna i rozpoczyna się Oktawa Paschalna ',
+    ],
+    links: [
+      { name: 'nieszpory Wielkiej Soboty', slug: 'wielka-sobota-nieszpory' },
+      { name: 'godzina czytań', slug: 'wielka-sobota-godzina-czytan' },
+      { name: 'kompleta', slug: 'wielka-sobota-kompleta' },
+      { name: 'jutrznia', slug: 'niedziela-zmartwychwstania-jutrznia' },
+      { name: 'nieszpory', slug: 'niedziela-zmartwychwstania-nieszpory' },
+      { name: 'kompleta', slug: 'niedziela-zmartwychwstania-kompleta' },
+    ],
+  },
+];
