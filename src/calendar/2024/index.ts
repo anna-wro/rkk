0abdb6935f368e12c80977077da07a64fdddf6d9
Anna Wróbel <anna-wro@users.noreply.github.com>
2024-03-01 02:52:40 +0100

    📆 March 2023 (#324)
    Co-authored-by: Anna Wróbel <wro.emails@gmail.com>

--- src/calendar/2024/index.ts
+++ src/calendar/2024/index.ts
@@ -1,4 +1,5 @@
 import { January2024 } from './2024-01';
 import { February2024 } from './2024-02';
+import { March2024 } from './2024-03';
 
-export const Calendar2024 = [January2024, February2024];
+export const Calendar2024 = [January2024, February2024, March2024];
