a8f15d709d980aea9e7095f1ac4caf9a68f02dbc
Petraszka <74956643+Petraszka@users.noreply.github.com>
2024-04-30 17:11:11 +0200

    1-22.04.2024 added to the calendar (#334)
    Co-authored-by: Klaudia Sowiak <klaudia.sowiak@binarapps.com>

---
+++ src/calendar/2024/2024-05.ts
@@ -0,0 +1,316 @@
+import type { CalendarType } from '../calendar';
+
+export const May2024: CalendarType = [
+  {
+    date: '2024-05-01',
+    week: 1,
+    season: 'easter',
+    passages: ['Dz 15, 1-6', 'Ps 122', 'J 15, 1-8'],
+    notes: ['[HTML]Filipa[HTML] ap.', '[HTML]Jakuba[HTML] ap.'],
+    links: [{ name: 'Litanie maryjne', slug: 'litanie-maryjne' }],
+  },
+  {
+    date: '2024-05-02',
+    week: 2,
+    season: 'easter',
+    passages: ['Dz 15, 7-21', 'Ps 96', 'J 15, 9-11'],
+    notes: ['[HTML]Atanazego z Aleksandrii[HTML] bp. i teo.'],
+    links: [{ name: 'Litanie maryjne', slug: 'litanie-maryjne' }],
+  },
+  {
+    date: '2024-05-03',
+    week: 1,
+    season: 'easter',
+    holidays: ['WSPOMNIENIE PIERWSZEGO NABOŻEŃSTWA RKK W POLSCE'],
+    passages: ['1 P 2, 4-9', 'J 4, 19-24'],
+    links: [
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/pierwsze-nabożenstwo-rkk-w-polsce-3-maja',
+      },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+    ],
+  },
+  {
+    date: '2024-05-04',
+    week: 1,
+    season: 'easter',
+    passages: ['Dz 16, 1-10', 'Ps 100', 'J 15, 18-21'],
+    notes: ['[HTML]Michała Giedroycia[HTML] zak.'],
+    links: [{ name: 'Litanie maryjne', slug: 'litanie-maryjne' }],
+  },
+  {
+    date: '2024-05-05',
+    week: 2,
+    season: 'easter',
+    passages: ['Dz 10 25-26.34-35.44-48', 'Ps 98', '1 J 4, 7-10', 'J 15, 9-17'],
+    notes: ['[HTML]Pauli z Rzymu[HTML] w.'],
+    holidays: ['6. NIEDZIELA PASCHALNA'],
+    links: [
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/6-niedziela-zmartwychwstania-panskiego-rok-b',
+      },
+    ],
+  },
+  {
+    date: '2024-05-06',
+    week: 2,
+    season: 'easter',
+    passages: ['Dz 16, 11-15', 'Ps 149', 'J 15, 26-16, 4a'],
+    notes: [
+      '[HTML]Ewodiusza[HTML] bp.',
+      '[HTML]Léona-Josepha Suenensa[HTML] bp.',
+    ],
+    links: [{ name: 'Litanie maryjne', slug: 'litanie-maryjne' }],
+  },
+  {
+    date: '2024-05-07',
+    week: 2,
+    season: 'easter',
+    passages: ['Dz 16, 22-34', 'Ps 138', 'J 16, 5-11'],
+    links: [{ name: 'Litanie maryjne', slug: 'litanie-maryjne' }],
+  },
+  {
+    date: '2024-05-08',
+    week: 2,
+    season: 'easter',
+    passages: ['Dz 17, 15.22-18.1', 'Ps 148', 'J 16, 12-15'],
+    notes: ['[HTML]Julianny z Norwich[HTML] mist.'],
+    links: [
+      {
+        name: 'Wniebowstąpienie Pańskie - wigilia',
+        slug: 'wniebowstapienie-panskie-wigilia',
+      },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+    ],
+  },
+  {
+    date: '2024-05-09',
+    week: 2,
+    season: 'pentecost',
+    holidays: ['WNIEBOWSTĄPIENIE'],
+    passages: ['Dz 1, 1-11', 'Ps 47', 'Ef 1, 17-23', 'Mk 16, 15-20'],
+    notes: ['Rozpoczyna się Nowenna przed Pięćdziesiątnicą.'],
+    links: [
+      {
+        name: 'Jutrznia',
+        slug: 'wniebowstapienie-panskie-jutrznia',
+      },
+      {
+        name: 'Nieszpory',
+        slug: 'wniebowstapienie-panskie-nieszpory',
+      },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/wniebowstapienie-panskie-rok-b',
+      },
+    ],
+  },
+  {
+    date: '2024-05-10',
+    week: 2,
+    season: 'pentecost',
+    passages: ['Dz 18, 9-18', 'Ps 47', 'J 16, 20-23'],
+    notes: ['[HTML]Józefa Padewskiego-Podeszwy[HTML] bp. i m.'],
+    links: [
+      { name: 'Jutrznia', slug: 'wniebowstapienie-nowenna-1-jutrznia' },
+      { name: 'Nieszpory', slug: 'wniebowstapienie-nowenna-1-nieszpory' },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+    ],
+  },
+  {
+    date: '2024-05-11',
+    week: 2,
+    season: 'pentecost',
+    passages: ['Dz 18, 23-28', 'Ps 47', 'J 16, 23b-28'],
+    links: [
+      { name: 'Jutrznia', slug: 'wniebowstapienie-nowenna-2-jutrznia' },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+    ],
+  },
+  {
+    date: '2024-05-12',
+    week: 1,
+    season: 'pentecost',
+    holidays: ['7. NIEDZIELA PASCHALNA'],
+    passages: [
+      'Dz 1 15-17.20a.20c-26',
+      'Ps 103',
+      '1 J 4, 11-16',
+      'J 17, 11b-19',
+    ],
+    links: [
+      { name: 'Wigilia', slug: 'wniebowstapienie-nowenna-2-nieszpory' },
+      { name: 'Jutrznia', slug: 'wniebowstapienie-nowenna-3-jutrznia' },
+      { name: 'Nieszpory', slug: 'wniebowstapienie-nowenna-3-nieszpory' },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/7-niedziela-zmartwychwstania-panskiego-rok-b',
+      },
+    ],
+  },
+  {
+    date: '2024-05-13',
+    week: 1,
+    season: 'pentecost',
+    passages: ['Dz 19, 1-8', 'Ps 68', 'J 16, 29-33'],
+    links: [
+      { name: 'Jutrznia', slug: 'wniebowstapienie-nowenna-4-jutrznia' },
+      { name: 'Nieszpory', slug: 'wniebowstapienie-nowenna-4-nieszpory' },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+    ],
+  },
+  {
+    date: '2024-05-14',
+    week: 1,
+    season: 'pentecost',
+    notes: ['[HTML]MACIEJA[HTML] ap.'],
+    passages: ['Ps 145, 10-11.12-13ab.17-18', 'J 15, 9-17'],
+    links: [
+      { name: 'Jutrznia', slug: 'wniebowstapienie-nowenna-5-jutrznia' },
+      { name: 'Nieszpory', slug: 'wniebowstapienie-nowenna-5-nieszpory' },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+    ],
+  },
+  {
+    date: '2024-05-15',
+    week: 1,
+    season: 'pentecost',
+    passages: ['Dz 20, 28-38', 'Ps 68', 'J 17, 11b-19'],
+    notes: ['[HTML]Róży Marii Czackiej[HTML] zak.'],
+    links: [
+      { name: 'Jutrznia', slug: 'wniebowstapienie-nowenna-6-jutrznia' },
+      { name: 'Nieszpory', slug: 'wniebowstapienie-nowenna-6-nieszpory' },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+    ],
+  },
+  {
+    date: '2024-05-16',
+    week: 1,
+    season: 'pentecost',
+    passages: ['Dz 22, 30; 23, 6-11', 'Ps 16', 'J 17, 20-26'],
+    links: [
+      { name: 'Jutrznia', slug: 'wniebowstapienie-nowenna-7-jutrznia' },
+      { name: 'Nieszpory', slug: 'wniebowstapienie-nowenna-7-nieszpory' },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+    ],
+  },
+  {
+    date: '2024-05-17',
+    week: 1,
+    season: 'pentecost',
+    passages: ['Iz 61, 1-3a', 'Ps 102, 2-7.13-14', 'Rz 16, 1-7', 'J 15, 1-8'],
+    notes: ['[HTML]Junii[HTML]ap.', '[HTML]Andronika[HTML] ap.'],
+    links: [
+      { name: 'Jutrznia', slug: 'wniebowstapienie-nowenna-8-jutrznia' },
+      { name: 'Nieszpory', slug: 'wniebowstapienie-nowenna-8-nieszpory' },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+    ],
+  },
+  {
+    date: '2024-05-18',
+    week: 1,
+    season: 'pentecost',
+    passages: ['Dz 28, 16-20.30-31', 'Ps 11', 'J 21, 20-25'],
+    holidays: ['WIGILIA PIĘĆDZIESIĄTNICY'],
+    links: [
+      { name: 'Jutrznia', slug: 'wniebowstapienie-nowenna-9-jutrznia' },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/zeslanie-ducha-swietego-wigilia',
+      },
+    ],
+    notes: [
+      'Wieczorem rozpoczyna się Wigilia Pięćdziesiątnicy. Czytania wg lekcjonarza Tom 2 str. 209-218',
+    ],
+  },
+  {
+    date: '2024-05-19',
+    week: 2,
+    season: 'pentecost',
+    passages: ['Dz 2, 1-11', 'Ps 104', '1 Kor 12, 3b-7, 12-13', 'J 20, 19-23'],
+    holidays: ['PIĘĆDZIESIĄTNICA'],
+    links: [
+      { name: 'Wigilia', slug: 'zeslanie-ducha-swietego-wigilia' },
+      { name: 'Jutrznia', slug: 'zeslanie-ducha-swietego-jutrznia' },
+      { name: 'Nieszpory', slug: 'zeslanie-ducha-swietego-nieszpory' },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/zeslanie-ducha-swietego',
+      },
+    ],
+  },
+  {
+    date: '2024-05-20',
+    week: 2,
+    season: 'pentecost',
+    holidays: ['OKTAWA PIĘĆDZIESIĄTNICY'],
+    passages: ['Dz 10, 34, 42-48', 'Ps 87', 'J 3, 16-21'],
+    lauds: {
+      sigla: 'Rz 8, 14-16',
+      passage:
+        'Ci, których prowadzi Duch Boży, są synami Bożymi. Nie przyjęliście przecież ducha niewoli, aby trwać w lęku, ale przyjęliście Ducha, który czyni was dziećmi. W Nim wołamy: Abba, Ojcze! Właśnie ten Duch zaświadcza naszemu duchowi, że jesteśmy dziećmi Boga.',
+      antiphon:
+        'Bóg tak umiłował świat,* że Syna jednorodzonego wydał, / aby każdy wierzący w Niego nie zginął, / lecz otrzymał życie wieczne. / alleluja.',
+    },
+    vespers: {
+      sigla: 'Dz 2,2-4',
+      passage:
+        'Kiedy nadszedł dzień Pięćdziesiątnicy, byli wszyscy w tym samym miejscu. Nagle rozległ się szum z nieba, jakby gwałtownie wiejącego wiatru. I napełnił cały dom, w którym przebywali. Zobaczyli też języki podobne do ognia. Rozdzielały się one i na każdym z nich spoczął jeden. Wszystkich napełnił Duch Święty. Zaczęli mówić różnymi językami, tak jak im Duch pozwalał się wypowiadać.',
+      antiphon:
+        'Przybądź, Duchu Święty,* ześlij z nieba wzięty / światła Twego strumień. / Alleluja.',
+    },
+    variant: 1,
+    links: [
+      { name: 'Jutrznia', slug: 'oktawa-zeslania-jutrznia' },
+      { name: 'Nieszpory', slug: 'oktawa-zeslania-nieszpory' },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/poniedzialek-w-oktawie-zeslania-ducha-swietego',
+      },
+    ],
+    notes: [
+      '[HTML]Wspomnienie I Soboru Ekumenicznego w Nicei  (325 r.)[HTML]',
+      'Tego dnia uroczyście odmawiamy Konstantynopolitańskie Wyznanie Wiary',
+    ],
+  },
+  {
+    date: '2024-05-21',
+    week: 2,
+    season: 'pentecost',
+    holidays: ['OKTAWA PIĘĆDZIESIĄTNICY'],
+    passages: ['Dz 8, 14-17', 'Ps 33', 'J 10, 1-10'],
+    lauds: {
+      sigla: 'Ef 4,23-27',
+      passage:
+        'Pozwólcie się odnawiać Duchowi w waszym myśleniu i przyobleczcie się w nowego człowieka, stworzonego zamysłem Boga w sprawiedliwości i prawdziwej świętości. Dlatego odrzućcie kłamstwo i niech każdy mówi prawdę swemu bliźniemu, bo wszyscy tworzymy jedno. Gniewajcie się, ale nie grzeszcie: niech słońce nie zachodzi nad waszym gniewem. I nie dawajcie miejsca diabłu!',
+      antiphon:
+        'Chrystus jest bramą owiec * przyszedł, aby życie miały / i by miały bogato. / alleluja.',
+    },
+    vespers: {
+      sigla: '1Kor 2,9-12',
+      passage:
+        'Napisano bowiem: czego oko nie widziało ani ucho nie słyszało, w serce człowieka nie wstąpiło, to przygotował Bóg tym, którzy Go miłują. Bóg objawił nam to przez Ducha, a Duch przenika wszystko, nawet głębokości Boga. Któż bowiem wie, co kryje się w człowieku, jeśli nie duch człowieka, który w nim jest? Tak i Boga nikt by nie poznał, gdyby nie Duch Boży. My nie otrzymaliśmy ducha od świata, ale Ducha od Boga, aby poznać dary otrzymane od Boga.	',
+      antiphon:
+        'Przyjdź, Ojcze ubogich,* przyjdź, Dawco łask drogich, / przyjdź, Światłości sumień. / Alleluja.',
+    },
+    variant: 2,
+    links: [
+      { name: 'Jutrznia', slug: 'oktawa-zeslania-jutrznia' },
+      { name: 'Nieszpory', slug: 'oktawa-zeslania-nieszpory' },
+      { name: 'Litanie maryjne', slug: 'litanie-maryjne' },
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/wtorek-w-oktawie-zeslania-ducha-swietego',
+      },
+    ],
+    notes: ['[HTML]Jana Nepomucena[HTML] prezb. i m.'],
+  },
+];
--- src/calendar/2024/index.ts
+++ src/calendar/2024/index.ts
@@ -2,5 +2,12 @@ import { January2024 } from './2024-01';
 import { February2024 } from './2024-02';
 import { March2024 } from './2024-03';
 import { April2024 } from './2024-04';
+import { May2024 } from './2024-05';
 
-export const Calendar2024 = [January2024, February2024, March2024, April2024];
+export const Calendar2024 = [
+  January2024,
+  February2024,
+  March2024,
+  April2024,
+  May2024,
+];
