920654390b5f921424eff9604682d789bef5fbe6
Anna Wróbel <wro.emails@gmail.com>
2024-05-31 21:16:00 +0200

    📆 June 2024

--- src/calendar/2024/index.ts
+++ src/calendar/2024/index.ts
@@ -3,6 +3,7 @@ import { February2024 } from './2024-02';
 import { March2024 } from './2024-03';
 import { April2024 } from './2024-04';
 import { May2024 } from './2024-05';
+import { June2024 } from './2024-06';
 
 export const Calendar2024 = [
   January2024,
@@ -10,4 +11,5 @@ export const Calendar2024 = [
   March2024,
   April2024,
   May2024,
+  June2024,
 ];
