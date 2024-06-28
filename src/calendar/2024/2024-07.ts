b94bd79856d35ccf76b4a199346d07bc9ff2b9c7
Marek Mazur <marek@codesmart.tech>
2024-06-28 19:52:55 +0200

    data(calendar): Calendar for July 2024

---
+++ src/calendar/2024/2024-07.ts
@@ -0,0 +1,289 @@
+import type { CalendarType } from '../calendar';
+
+export const July2024: CalendarType = [
+  {
+    date: '2024-07-01',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Am 2, 6-10, 13-16', 'Ps 50', 'Mt 8, 18-22'],
+    notes: [
+      '[HTML]Adolfa Thürlingsa[HTML] prezb.',
+      '[HTML]Antonio Rosimini-Serbatiego[HTML] prezb.',
+    ],
+  },
+  {
+    date: '2024-07-02',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Am 3, 1-8; 4, 11-12', 'Ps 5', 'Mt 8, 23-27'],
+    notes: ['[HTML]Jana z Szanghaju i San Francisco[HTML] bp.'],
+  },
+  {
+    date: '2024-07-03',
+    week: 2,
+    season: 'ordinary',
+    holidays: ['TOMASZA APOSTOŁA'],
+    passages: ['Ef 2, 19-22', 'Ps 117', 'J 20, 24-29'],
+    links: [
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/tomasza-apostola-3-lipca',
+      },
+    ],
+  },
+  {
+    date: '2024-07-04',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Am 7, 10-17', 'Ps 19', 'Mt 9, 1-8'],
+    notes: ['[HTML]Carlosa Duarte Costy[HTML] bp.'],
+  },
+  {
+    date: '2024-07-05',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Am 8, 4-6, 9-12', 'Ps 119', 'Mt 9, 9-13'],
+  },
+  {
+    date: '2024-07-06',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Am 9, 11-15', 'Ps 85', 'Mt 9, 14-17'],
+    notes: ['[HTML]Jana Husa[HTML] prezb. m.'],
+  },
+  {
+    date: '2024-07-07',
+    week: 1,
+    holidays: ['7. NIEDZIELA PO PIĘĆDZIESIĄTNICY'],
+    season: 'ordinary',
+    passages: ['Ez 2, 2-5', 'Ps 123', '2 Kor 12, 7-10', 'Mk 6, 1-6'],
+    notes: ['[HTML]Atemagprasa I [HTML] bp.'],
+    links: [
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/14-niedziela-w-ciagu-roku-rok-b',
+      },
+    ],
+  },
+  {
+    date: '2024-07-08',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Oz 2, 16, 17c-18, 21-22', 'Ps 145', 'Mt 9, 18-26'],
+  },
+  {
+    date: '2024-07-09',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Oz 8, 4-7, 11-13', 'Ps 115', 'Mt 9, 32-38'],
+  },
+  {
+    date: '2024-07-10',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Oz 10, 1-3, 7-8, 12', 'Ps 105', 'Mt 10, 1-7'],
+    links: [
+      {
+        name: 'wigilia dnia Św. Benedykta',
+        slug: 'benedykt-wigilia',
+      },
+    ],
+  },
+  {
+    date: '2024-07-11',
+    week: 1,
+    season: 'ordinary',
+    holidays: ['BENEDYKTA Z NURSJI'],
+    passages: ['Prz 2, 1-9', 'Ps 34', 'Mt 19, 27-29'],
+    notes: ['Święto Patronalne Benedyktynów Mądrości Bożej'],
+    links: [
+      { name: 'Liturgia Słowa', slug: 'czytania/benedykta-z-nursji-11-lipca' },
+      { name: 'jutrznia', slug: 'benedykt-jutrznia' },
+      { name: 'nieszpory', slug: 'benedykt-nieszpory' },
+    ],
+  },
+  {
+    date: '2024-07-12',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Oz 14, 2-10', 'Ps 51', 'Mt 10, 16-23'],
+    notes: [
+      '[HTML]Brunona z Kwerfurtu[HTML] bp. m.',
+      '[HTML]Sergiusza Bułgakowa[HTML] teo.',
+    ],
+    links: [
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/brunona-z-kwerfurtu-i-sergiusza-bulgakowa-12-lipca',
+      },
+    ],
+  },
+  {
+    date: '2024-07-13',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Iz 6, 1-8', 'Ps 93', 'Mt 10, 24-33'],
+  },
+  {
+    date: '2024-07-14',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Am 7, 12-15', 'Ps 85', 'Ef 1, 3-14', 'Mk 6, 7-13'],
+    notes: ['[HTML]Cicely Saunders[HTML]'],
+    holidays: ['8. NIEDZIELA PO PIĘĆDZIESIĄTNICY'],
+    links: [
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/15-niedziela-w-ciagu-roku-rok-b',
+      },
+    ],
+  },
+  {
+    date: '2024-07-15',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Iz 1, 10-17', 'Ps 50', 'Mt 10, 34-11, 1'],
+    notes: ['[HTML]Bonawentury[HTML] zak. teo.'],
+  },
+  {
+    date: '2024-07-16',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Iz 7, 1-9', 'Ps 48', 'Mt 11, 20-24'],
+  },
+  {
+    date: '2024-07-17',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Iz 10, 5-7, 13b-16', 'Ps 94', 'Mt 11, 25-27'],
+    notes: ['[HTML]Andrzeja Świerada[HTML] mn.', '[HTML]Benedykta[HTML] mn.'],
+  },
+  {
+    date: '2024-07-18',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Iz 26, 7-9, 12, 16-19', 'Ps 102', 'Mt 11, 28-30'],
+  },
+  {
+    date: '2024-07-19',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Iz 38, 1-6, 21-22, 7-8', 'Iz 38', 'Mt 12, 1-8'],
+    notes: [
+      '[HTML]Grzegorza z Nyssy[HTML] bp.',
+      '[HTML]Makaryny[HTML] dk.',
+      '[HTML]Serafina z Sarowa[HTML] mn.',
+    ],
+    isFastingDay: true,
+  },
+  {
+    date: '2024-07-20',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Mi 2, 1-5', 'Ps 10', 'Mt 12, 14-21'],
+    notes: ['[HTML]Eliasza[HTML]'],
+  },
+  {
+    date: '2024-07-21',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Jr 23, 1-6', 'Ps 23', 'Ef 2, 13-18', 'Mk 6, 30-34'],
+    holidays: ['9. NIEDZIELA PO PIĘĆDZIESIĄTNICY'],
+    links: [
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/16-niedziela-w-ciagu-roku-rok-b',
+      },
+    ],
+  },
+  {
+    date: '2024-07-22',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Pnp 3, 1-4b', 'Ps 63', 'J 20, 1-2.11-18'],
+    holidays: ['MARII MAGDALENY APOSTOŁKI APOSTOŁÓW'],
+    links: [
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/marii-magdaleny-apostolki-apostolow-22-lipca',
+      },
+    ],
+  },
+  {
+    date: '2024-07-23',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Mi 7, 14-15, 18-20', 'Ps 85', 'Mt 12, 46-50'],
+    notes: [
+      '[HTML]Jana Kasjana[HTML] mn.',
+      '[HTML]Brygidy Szwedzkiej[HTML] mn.',
+    ],
+  },
+  {
+    date: '2024-07-24',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Jr 1, 1, 4-10', 'Ps 71', 'Mt 13, 1-9'],
+  },
+  {
+    date: '2024-07-25',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Iz 49, 1-6', 'Ps 19', 'Mt 4, 18-22'],
+    holidays: [' JAKUBA WIĘKSZEGO APOSTOŁA'],
+    notes: ['[HTML]Olimpii[HTML] dk.'],
+    links: [
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/jakuba-wiekszego-apostola-25-lipca',
+      },
+    ],
+  },
+  {
+    date: '2024-07-26',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Jr 3, 14-17', 'Jr 31', 'Mt 13, 18-23'],
+    notes: ['[HTML]Rodziców Bogurodzicy[HTML]'],
+  },
+  {
+    date: '2024-07-27',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Jr 7, 1-11', 'Ps 84', 'Mt 13, 24-30'],
+  },
+  {
+    date: '2024-07-28',
+    week: 2,
+    season: 'ordinary',
+    passages: ['2 Krl 4, 42-44', 'Ps 145', 'Ef 4, 1-6', 'J 6, 1-15'],
+    holidays: ['10. NIEDZIELA PO PIĘĆDZIESIĄTNICY'],
+    links: [
+      {
+        name: 'Liturgia słowa',
+        slug: 'czytania/17-niedziela-w-ciagu-roku-rok-b',
+      },
+    ],
+  },
+  {
+    date: '2024-07-29',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Jr 13, 1-11', 'Pwt 32', 'Mt 13, 31-35'],
+    notes: ['[HTML]Marii, Marty i Łazarza z Betanii[HTML]'],
+  },
+  {
+    date: '2024-07-30',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Jr 14, 17-22', 'Ps 79', 'Mt 13, 36-43'],
+    notes: ['[HTML]Piotra Chryzologa[HTML] bp. teo.'],
+  },
+  {
+    date: '2024-07-31',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Jr 15, 10, 16-21', 'Ps 59', 'Mt 13, 44-46'],
+    notes: ['[HTML]Bartoloméa de Las Casa[HTML] zak.'],
+  },
+];
