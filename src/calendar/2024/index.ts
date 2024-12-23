ee58b6c4ead9d80534540fdf559212732a8593aa
Marek M. Mazur <marek@codesmart.tech>
2024-12-23 11:24:52 +0100

    data(calendar): added everything before lent (#358)

--- src/calendar/2024/index.ts
+++ src/calendar/2024/index.ts
@@ -10,7 +10,6 @@ import { September2024 } from './2024-09';
 import { October2024 } from './2024-10';
 import { November2024 } from './2024-11';
 import { December2024 } from './2024-12';
-import { January2025 } from './2025-01';
 
 export const Calendar2024 = [
   January2024,
@@ -25,5 +24,4 @@ export const Calendar2024 = [
   October2024,
   November2024,
   December2024,
-  January2025,
 ];
---
+++ src/calendar/2025/2025-03.ts
@@ -0,0 +1,34 @@
+import type { CalendarType } from '../calendar';
+
+export const March2025: CalendarType = [
+  {
+    date: '2025-03-01',
+    week: 1,
+    season: 'ordinary',
+    passages: ['Syr 17, 1-15', 'Ps 103', 'Mk 10, 13-16'],
+  },
+  {
+    date: '2025-03-02',
+    week: 2,
+    season: 'ordinary',
+    holidays: ['8. NIEDZIELA PO EPIFANII'],
+    passages: ['Syr 27, 4-7', 'Ps 92', '1 Kor 15, 54-58', 'Łk 6, 39-45'],
+  },
+  {
+    date: '2025-03-03',
+    week: 2,
+    season: 'ordinary',
+    notes: [
+      '[HTML]Jana Wesleya[HTML] prezb.',
+      '[HTML]Karola Wesleya[HTML] prezb.',
+    ],
+    passages: ['Syr 17, 20-24', 'Ps 32', 'Mk 10, 17-27'],
+  },
+  {
+    date: '2025-03-04',
+    week: 2,
+    season: 'ordinary',
+    notes: ['[HTML]Natalii i Adriana z Nikomedii[HTML] m.'],
+    passages: ['Syr 35, 1-12', 'Ps 50', 'Mk 10, 28-31'],
+  },
+];
