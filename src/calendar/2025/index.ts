3f741e3628b7d2d0630d6302b7e82f6dbf709ad0
Marek Mazur <marek@codesmart.tech>
2025-02-18 08:41:44 +0100

    data(calendar): lent 2025

--- src/calendar/2025/index.ts
+++ src/calendar/2025/index.ts
@@ -1,5 +1,6 @@
 import { January2025 } from './2025-01';
 import { February2025 } from './2025-02';
 import { March2025 } from './2025-03';
+import { April2025 } from './2025-04';
 
-export const Calendar2025 = [January2025, February2025, March2025];
+export const Calendar2025 = [January2025, February2025, March2025, April2025];
