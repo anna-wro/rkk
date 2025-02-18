ca73a13578f284e28153a716f0ef5d5074109d09
Marek M. Mazur <marek@codesmart.tech>
2025-02-18 17:52:52 +0100

    data(calendar): lent 2025 (#363)
    * further improvements while having a cold
    
    * data(calendar): lent 2025

---
+++ src/calendar/2025/2025-04.ts
@@ -0,0 +1,88 @@
+import type { CalendarType } from '../calendar';
+
+export const April2025: CalendarType = [
+  {
+    date: '2025-04-01',
+    week: 2,
+    season: 'lent',
+    notes: [
+      '[HTML]Marii Egipcjanki[HTML] mn.',
+      '[HTML]Melitona z Sardes[HTML] bp.',
+    ],
+    passages: ['Ez 47, 1-9, 12', 'Ps 46', 'J 5, 1-3a, 5-16'],
+  },
+  {
+    date: '2025-04-02',
+    week: 2,
+    season: 'lent',
+    notes: ['[HTML]Franciszka z Paoli[HTML] mn.'],
+    passages: ['Iz 49, 8-15', 'Ps 145', 'J 5, 17-30'],
+  },
+  {
+    date: '2025-04-03',
+    week: 2,
+    season: 'lent',
+    passages: ['Wj 32, 7-14', 'Ps 106', 'J 5, 31-47'],
+  },
+  {
+    date: '2025-04-04',
+    week: 2,
+    season: 'lent',
+    notes: ['[HTML]Izydora z Sewilli[HTML] bp.'],
+    passages: ['Mdr 2, 1a, 12-22', 'Ps 34', 'J 7, 1-2, 10, 25-30'],
+  },
+  {
+    date: '2025-04-05',
+    week: 2,
+    season: 'lent',
+    passages: ['Jr 11, 18-20', 'Ps 7', 'J 7, 40-53'],
+  },
+  {
+    date: '2025-04-06',
+    week: 1,
+    season: 'lent',
+    holidays: ['5. NIEDZIELA PRZYGOTOWANIA PASCHALNEGO'],
+    notes: ['[HTML]Jacquesa Fescha[HTML] wyz.'],
+    passages: ['Ez 37, 12-14', 'Ps 130', 'Rz 8, 8-11', 'J 11, 1-45'],
+  },
+  {
+    date: '2025-04-07',
+    week: 1,
+    season: 'lent',
+    passages: ['Dn 13, 1-9, 15-17, 19-30, 33-62', 'Ps 23', 'J 8, 1-11'],
+  },
+  {
+    date: '2025-04-08',
+    week: 1,
+    season: 'lent',
+    passages: ['Lb 21, 4-9', 'Ps 102', 'J 8, 21-30'],
+  },
+  {
+    date: '2025-04-09',
+    week: 1,
+    season: 'lent',
+    notes: [
+      '[HTML]Dietricha Bonhoeffera[HTML] m.',
+      '[HTML]Achacego z Amidy[HTML] bp.',
+    ],
+    passages: ['Dn 3, 14-20, 91-92, 95', 'Dn 3', 'J 8, 31-42'],
+  },
+  {
+    date: '2025-04-10',
+    week: 1,
+    season: 'lent',
+    passages: ['Rdz 17, 3-9', 'Ps 105', 'J 8, 51-59'],
+  },
+  {
+    date: '2025-04-11',
+    week: 1,
+    season: 'lent',
+    passages: ['Jr 20, 10-13', 'Ps 18', 'J 10, 31-42'],
+  },
+  {
+    date: '2025-04-12',
+    week: 1,
+    season: 'lent',
+    passages: ['Ez 37, 21-28', 'Jr 31', 'J 11, 45-56'],
+  },
+];
