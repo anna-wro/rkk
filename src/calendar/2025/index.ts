8d3936abc14d934e8bfb4c848f22bddba7b8598f
Petraszka <klaudia.sowiak@gmail.com>
2025-04-10 20:47:56 +0200

    April, May, June added

--- src/calendar/2025/index.ts
+++ src/calendar/2025/index.ts
@@ -2,5 +2,7 @@ import { January2025 } from './2025-01';
 import { February2025 } from './2025-02';
 import { March2025 } from './2025-03';
 import { April2025 } from './2025-04';
+import { May2025 } from './2025-05';
+import { June2025 } from './2025-06';
 
-export const Calendar2025 = [January2025, February2025, March2025, April2025];
+export const Calendar2025 = [January2025, February2025, March2025, April2025, May2025, June2025];
