defa4271d4abe082a0cf971c4da4cfd01760aa45
Marek M. Mazur <marek@codesmart.tech>
2025-04-17 09:18:51 +0200

    data(calendar): update weeks and seasons for March, April, and May 2025 (#366)

--- src/calendar/2025/2025-06.ts
+++ src/calendar/2025/2025-06.ts
@@ -4,7 +4,7 @@ export const June2025: CalendarType = [
   {
     date: '2025-06-01',
     week: 1,
-    season: 'pascha',
+    season: 'pentecost',
     holidays: ['7. NIEDZIELA PASCHALNA'],
     notes: ['[HTML]Justyna[HTML] m.'],
     passages: [
@@ -17,40 +17,40 @@ export const June2025: CalendarType = [
   {
     date: '2025-06-02',
     week: 1,
-    season: 'pascha',
+    season: 'pentecost',
     holidays: ['JORDANA Z POZNANIA'],
     passages: ['Iz 52, 7-10', 'Ps 19', '2 Kor 5, 14-20', 'Łk 5, 1-11'],
   },
   {
     date: '2025-06-03',
     week: 1,
-    season: 'pascha',
+    season: 'pentecost',
     passages: ['Dz 20, 17-27', 'Ps 68', 'J 17, 1-11a'],
   },
   {
     date: '2025-06-04',
     week: 1,
-    season: 'pascha',
+    season: 'pentecost',
     passages: ['Dz 20, 28-38', 'Ps 68', 'J 17, 11b-19'],
   },
   {
     date: '2025-06-05',
     week: 1,
-    season: 'pascha',
+    season: 'pentecost',
     notes: ['[HTML]Bonifacego Winfryda bp. m.[HTML] bp. m.'],
     passages: ['Dz 22, 30; 23, 6-11', 'Ps 16', 'J 17, 20-26'],
   },
   {
     date: '2025-06-06',
     week: 1,
-    season: 'pascha',
+    season: 'pentecost',
     notes: ['[HTML]Marii Karłowskiej zak.[HTML] zak.'],
     passages: ['Dz 25, 13b-21', 'Ps 103', 'J 21, 15-19'],
   },
   {
     date: '2025-06-07',
     week: 1,
-    season: 'pascha',
+    season: 'pentecost',
     passages: ['Dz 28, 16-20, 30-31', 'Ps 11', 'J 21, 20-25'],
   },
 ];
