c2a0f7472ff865abb5cfe6646e1e19c4be5d55a1
Marek M. Mazur <marek@codesmart.tech>
2025-05-31 09:50:47 +0200

    data(calendar): update January, February, April, May, June, July, August, September, October, November, and December 2025 entries (#372)

--- src/calendar/2025/2025-06.ts
+++ src/calendar/2025/2025-06.ts
@@ -13,6 +13,10 @@ export const June2025: CalendarType = [
       'Ap 22, 12-14, 16-17, 20',
       'J 17, 20-26',
     ],
+    links: [
+      { name: 'jutrznia', slug: 'wniebowstapienie-nowenna-3-jutrznia' },
+      { name: 'nieszpory', slug: 'wniebowstapienie-nowenna-3-nieszpory' },
+    ],
   },
   {
     date: '2025-06-02',
@@ -26,12 +30,20 @@ export const June2025: CalendarType = [
     week: 1,
     season: 'pentecost',
     passages: ['Dz 20, 17-27', 'Ps 68', 'J 17, 1-11a'],
+    links: [
+      { name: 'jutrznia', slug: 'wniebowstapienie-nowenna-5-jutrznia' },
+      { name: 'nieszpory', slug: 'wniebowstapienie-nowenna-5-nieszpory' },
+    ],
   },
   {
     date: '2025-06-04',
     week: 1,
     season: 'pentecost',
     passages: ['Dz 20, 28-38', 'Ps 68', 'J 17, 11b-19'],
+    links: [
+      { name: 'jutrznia', slug: 'wniebowstapienie-nowenna-6-jutrznia' },
+      { name: 'nieszpory', slug: 'wniebowstapienie-nowenna-6-nieszpory' },
+    ],
   },
   {
     date: '2025-06-05',
@@ -39,6 +51,10 @@ export const June2025: CalendarType = [
     season: 'pentecost',
     notes: ['[HTML]Bonifacego Winfryda bp. m.[HTML] bp. m.'],
     passages: ['Dz 22, 30; 23, 6-11', 'Ps 16', 'J 17, 20-26'],
+    links: [
+      { name: 'jutrznia', slug: 'wniebowstapienie-nowenna-7-jutrznia' },
+      { name: 'nieszpory', slug: 'wniebowstapienie-nowenna-7-nieszpory' },
+    ],
   },
   {
     date: '2025-06-06',
@@ -46,11 +62,293 @@ export const June2025: CalendarType = [
     season: 'pentecost',
     notes: ['[HTML]Marii Karłowskiej zak.[HTML] zak.'],
     passages: ['Dz 25, 13b-21', 'Ps 103', 'J 21, 15-19'],
+    links: [
+      { name: 'jutrznia', slug: 'wniebowstapienie-nowenna-8-jutrznia' },
+      { name: 'nieszpory', slug: 'wniebowstapienie-nowenna-8-nieszpory' },
+    ],
   },
   {
     date: '2025-06-07',
     week: 1,
     season: 'pentecost',
+    holidays: ['WIGILIA PIĘĆDZIESIĄTNICY'],
     passages: ['Dz 28, 16-20, 30-31', 'Ps 11', 'J 21, 20-25'],
+    links: [{ name: 'jutrznia', slug: 'wniebowstapienie-nowenna-9-jutrznia' }],
+  },
+  {
+    date: '2025-06-08',
+    week: 2,
+    season: 'pentecost',
+    holidays: ['PIĘĆDZIESIĄTNICA'],
+    passages: ['Dz 2, 1-11', 'Ps 104', '1 Kor 12, 3b-7, 12-13', 'J 20, 19-23'],
+    links: [
+      { name: 'wigilia', slug: 'zeslanie-ducha-swietego-wigilia' },
+      { name: 'jutrznia', slug: 'zeslanie-ducha-swietego-jutrznia' },
+      { name: 'nieszpory', slug: 'zeslanie-ducha-swietego-nieszpory' },
+    ],
+  },
+  {
+    date: '2025-06-09',
+    week: 2,
+    season: 'pentecost',
+    holidays: ['OKTAWA PIĘĆDZIESIĄTNICY'],
+    passages: ['Dz 10, 34, 42-48', 'Ps 87', 'J 3, 16-21'],
+    links: [
+      { name: 'jutrznia', slug: 'oktawa-zeslania-jutrznia' },
+      { name: 'nieszpory', slug: 'oktawa-zeslania-nieszpory' },
+    ],
+    vespers: {
+      antiphon:
+        'Przybądź, Duchu Święty,* ześlij z nieba wzięty / światła Twego strumień. / Alleluja.',
+      sigla: 'Dz 2,2-4',
+      passage:
+        'Kiedy nadszedł dzień Pięćdziesiątnicy, byli wszyscy w tym samym miejscu. Nagle rozległ się szum z nieba, jakby gwałtownie wiejącego wiatru. I napełnił cały dom, w którym przebywali. Zobaczyli też języki podobne do ognia. Rozdzielały się one i na każdym z nich spoczął jeden. Wszystkich napełnił Duch Święty. Zaczęli mówić różnymi językami, tak jak im Duch pozwalał się wypowiadać.',
+    },
+    lauds: {
+      antiphon:
+        'Bóg tak umiłował świat,* że Syna jednorodzonego wydał, / aby każdy wierzący w Niego nie zginął, / lecz otrzymał życie wieczne. / alleluja.',
+      sigla: 'Rz 8, 14-16',
+      passage:
+        'Ci, których prowadzi Duch Boży, są synami Bożymi. Nie przyjęliście przecież ducha niewoli, aby trwać w lęku, ale przyjęliście Ducha, który czyni was dziećmi. W Nim wołamy: Abba, Ojcze! Właśnie ten Duch zaświadcza naszemu duchowi, że jesteśmy dziećmi Boga.',
+    },
+  },
+  {
+    date: '2025-06-10',
+    week: 2,
+    season: 'pentecost',
+    holidays: ['OKTAWA PIĘĆDZIESIĄTNICY'],
+    notes: [
+      '[HTML]Efrema Syryjczyka[HTML] dk.',
+      '[HTML]Bogumiła z Dobrowa[HTML] bp.',
+    ],
+    passages: ['Dz 8, 14-17', 'Ps 33', 'J 10, 1-10'],
+    links: [
+      { name: 'jutrznia', slug: 'oktawa-zeslania-jutrznia' },
+      { name: 'nieszpory', slug: 'oktawa-zeslania-nieszpory' },
+    ],
+    vespers: {
+      antiphon:
+        'Przyjdź, Ojcze ubogich,* przyjdź, Dawco łask drogich, / przyjdź, Światłości sumień. / Alleluja.',
+      sigla: '1Kor 2,9-12',
+      passage:
+        'Napisano bowiem: czego oko nie widziało ani ucho nie słyszało, w serce człowieka nie wstąpiło, to przygotował Bóg tym, którzy Go miłują. Bóg objawił nam to przez Ducha, a Duch przenika wszystko, nawet głębokości Boga. Któż bowiem wie, co kryje się w człowieku, jeśli nie duch człowieka, który w nim jest? Tak i Boga nikt by nie poznał, gdyby nie Duch Boży. My nie otrzymaliśmy ducha od świata, ale Ducha od Boga, aby poznać dary otrzymane od Boga.	',
+    },
+    lauds: {
+      antiphon:
+        'Chrystus jest bramą owiec * przyszedł, aby życie miały / i by miały bogato. / alleluja.',
+      sigla: 'Ef 4,23-27',
+      passage:
+        'Pozwólcie się odnawiać Duchowi w waszym myśleniu i przyobleczcie się w nowego człowieka, stworzonego zamysłem Boga w sprawiedliwości i prawdziwej świętości. Dlatego odrzućcie kłamstwo i niech każdy mówi prawdę swemu bliźniemu, bo wszyscy tworzymy jedno. Gniewajcie się, ale nie grzeszcie: niech słońce nie zachodzi nad waszym gniewem. I nie dawajcie miejsca diabłu!',
+    },
+  },
+  {
+    date: '2025-06-11',
+    week: 2,
+    season: 'pentecost',
+    holidays: ['OKTAWA PIĘĆDZIESIĄTNICY'],
+    passages: ['Dz 2, 14-21', 'Ps 33', 'J 6, 44-52'],
+    links: [
+      { name: 'jutrznia', slug: 'oktawa-zeslania-jutrznia' },
+      { name: 'nieszpory', slug: 'oktawa-zeslania-nieszpory' },
+    ],
+    vespers: {
+      antiphon:
+        'Światłości najświętsza,* serc wierzących wnętrza poddaj Twej potędze. / Alleluja.',
+      sigla: 'Rz 12,11-14',
+      passage:
+        'Nie traćcie gorliwości, bądźcie płomiennego ducha, służcie Panu. Radujcie się nadzieją, w ucisku bądźcie cierpliwi, w modlitwie wytrwali. Wspomagajcie świętych, przestrzegajcie gościnności. Błogosławcie tych, którzy was prześladują; błogosławcie, a nie złorzeczcie.	',
+    },
+    lauds: {
+      antiphon:
+        'Ja jestem chlebem żywym, który z nieba zstąpił. * Jeśli ktoś będzie z tego chleba spożywał, / na wieki żyć będzie. / alleluja.',
+      sigla: '1 J 3,23-24',
+      passage:
+        'A to jest Jego przykazanie, abyśmy uwierzyli w imię Jego Syna Jezusa Chrystusai miłowali się wzajemnie zgodnie z przykazaniem, jakie nam dał. Kto więc przestrzega Jego przykazań, trwa w Bogu, a Bóg w nim. To, że Bóg w nas przebywa, rozpoznajemy dzięki Duchowi, którego nam dał.',
+    },
+  },
+  {
+    date: '2025-06-12',
+    week: 2,
+    season: 'pentecost',
+    holidays: ['OKTAWA PIĘĆDZIESIĄTNICY'],
+    passages: ['Dz 8, 5-8', 'Ps 104', 'Łk 9, 1-6'],
+    links: [
+      { name: 'jutrznia', slug: 'oktawa-zeslania-jutrznia' },
+      { name: 'nieszpory', slug: 'oktawa-zeslania-nieszpory' },
+    ],
+    vespers: {
+      antiphon:
+        'Duchu Święty, obmyj, co nieświęte,* oschłym wlej zachętę, ulecz serca ranę. / Alleluja.',
+      sigla: '1 Kor 12,3-7',
+      passage:
+        'Nikt pod wpływem Ducha Bożego nie może mówić: „Niech Jezu będzie przeklęty” i nikt bez Ducha Świętego nie może mówić: „Jezus jest Panem”. Różne są dary, ale Duch jest jeden. Różne są posługi, ale Pan jest jeden. Różne są działania, ale jeden jest Bóg, który sprawia wszystko we wszystkich. W każdym uwidacznia się działanie Ducha dla wspólnego dobra.',
+    },
+    lauds: {
+      antiphon:
+        'Jezus posłał nas,* abyśmy nieśli wiadomość o królestwie Bożym. / alleluja.',
+      sigla: 'Rz 8,10-11',
+      passage:
+        'Lecz jeśli Chrystus jest w was, to chociaż ciało jest śmiertelne z powodu grzechu, Duch daje życie dzięki usprawiedliwieniu. Jeśli więc mieszka w was Duch Tego, który wskrzesił Jezusa z martwych, to Ten, który wskrzesił Chrystusa z martwych, ożywi wasze śmiertelne ciała dzięki mieszkającemu w was Jego Duchowi.',
+    },
+  },
+  {
+    date: '2025-06-13',
+    week: 2,
+    season: 'pentecost',
+    holidays: ['OKTAWA PIĘĆDZIESIĄTNICY'],
+    notes: ['[HTML]Antoniego z Padwy[HTML] prezb. zak.'],
+    passages: ['Jl 2, 23-24, 26-27', 'Mdr 11', 'Łk 5, 17-26'],
+    links: [
+      { name: 'jutrznia', slug: 'oktawa-zeslania-jutrznia' },
+      { name: 'nieszpory', slug: 'oktawa-zeslania-nieszpory' },
+    ],
+    vespers: {
+      antiphon:
+        'Wszystkich ogarnęło zdumienie i wysławiali Boga,* napełnieni bojaźnią mówili: Dzisiaj zobaczyliśmy coś nieprawdopodobnego. / Alleluja.',
+      sigla: '2 Tes 2,13-14',
+      passage:
+        'Umiłowani przez Pana! Powinniśmy dziękować Bogu zawsze za was wszystkich, za to, że wybrał was jako pierwszych, aby was zbawić przez uświęcenie w Duchu i wiarę w prawdę. Bóg wezwał was przez naszą Ewangelię do udziału w chwale naszego Pana, Jezusa Chrystusa. ',
+    },
+    lauds: {
+      antiphon:
+        'Odpuszczone są tobie twoje grzechy, * wstań i chodź. / alleluja.',
+      sigla: 'Ga 5,16; 22-25',
+      passage:
+        'Otóż powiadam: Postępujcie według Ducha, a nie będziecie spełniali pożądań ciała. owocem Ducha jest: miłość, radość, pokój, cierpliwość, dobroć, życzliwość, wierność, łagodność, opanowanie. Prawo tu nie jest potrzebne. A ci, którzy należą do Chrystusa Jezusa, ukrzyżowali swoje ciało razem z namiętnościami i pożądliwościami. Skoro więc żyjemy mocą Ducha, postępujmy według Ducha.',
+    },
+  },
+  {
+    date: '2025-06-14',
+    week: 2,
+    season: 'pentecost',
+    holidays: ['OKTAWA PIĘĆDZIESIĄTNICY'],
+    passages: ['Rz 5, 1-5', 'Tb 13', 'Łk 4, 38-44'],
+    links: [
+      { name: 'jutrznia', slug: 'oktawa-zeslania-jutrznia' },
+      { name: 'nieszpory', slug: 'oktawa-zeslania-nieszpory' },
+      { name: 'wigilia', slug: 'niedziela-trojcy-swietej-wigilia' },
+    ],
+    lauds: {
+      antiphon:
+        'Jezus został posłany,* aby głosić radosną nowinę o królestwie Boga. / alleluja.',
+      sigla: 'Ga 6,1-2',
+      passage:
+        'Siostry i bracia, nawet jeśli ktoś popadnie w grzech, wy, którzy trwacie w Duchu, podźwignijcie go z całą łagodnością. Uważaj jednak, abyś i ty nie uległ pokusie. Jedni drugim noście brzemiona, a tak wypełnicie prawo Chrystusa. ',
+    },
+  },
+  {
+    date: '2025-06-15',
+    week: 1,
+    season: 'pentecost',
+    holidays: ['NAJŚWIĘTSZEJ TRÓJCY'],
+    notes: ['[HTML]Jolanty z Gniezna[HTML] mn.'],
+    passages: ['Prz 8, 22-31', 'Ps 8', 'Rz 5, 1-5', 'J 16, 12-15'],
+    links: [
+      { name: 'jutrznia', slug: 'niedziela-trojcy-swietej-jutrznia' },
+      { name: 'nieszpory', slug: 'niedziela-trojcy-swietej-nieszpory' },
+    ],
+  },
+  {
+    date: '2025-06-16',
+    week: 1,
+    season: 'ordinary',
+    holidays: ['BARNABY APOSTOŁA'],
+    passages: ['Dz 11, 21b-26', '13, 1-3', 'Ps 98', 'Mt 10, 7-13'],
+  },
+  {
+    date: '2025-06-17',
+    week: 1,
+    season: 'ordinary',
+    notes: ['[HTML]Alberta Chmielowskiego[HTML] zak.'],
+    passages: ['2 Kor 8, 1-9', 'Ps 146', 'Mt 5, 43-48'],
+  },
+  {
+    date: '2025-06-18',
+    week: 1,
+    season: 'ordinary',
+    notes: [],
+    passages: ['2 Kor 9, 6-11', 'Ps 112', 'Mt 6, 1-6, 16-18'],
+  },
+  {
+    date: '2025-06-19',
+    week: 1,
+    season: 'ordinary',
+    holidays: ['NAJŚWIĘTSZEGO CIAŁA I KRWI PAŃSKIEJ'],
+    notes: ['[HTML]Romualda[HTML] mn.'],
+    passages: ['Rdz 14, 18-20', 'Ps 110', '1 Kor 11, 23-26', 'Łk 9, 11b-17'],
+  },
+  {
+    date: '2025-06-20',
+    week: 1,
+    season: 'ordinary',
+    passages: ['2 Kor 11, 18, 21-30', 'Ps 34', 'Mt 6, 19-23'],
+  },
+  {
+    date: '2025-06-21',
+    week: 1,
+    season: 'ordinary',
+    passages: ['2 Kor 12, 1-10', 'Ps 34', 'Mt 6, 24-34'],
+  },
+  {
+    date: '2025-06-22',
+    week: 2,
+    season: 'ordinary',
+    holidays: ['2. NIEDZIELA PO PIĘĆDZIESIĄTNICY'],
+    passages: ['Za 12, 10-11', 'Ps 63', 'Ga 3, 26-29', 'Łk 9, 18-24'],
+  },
+  {
+    date: '2025-06-23',
+    week: 2,
+    season: 'ordinary',
+    notes: ['[HTML]222 męczenników chińskich[HTML]'],
+    passages: ['Rdz 12, 1-9', 'Ps 33', 'Mt 7, 1-5'],
+  },
+  {
+    date: '2025-06-24',
+    week: 2,
+    season: 'ordinary',
+    holidays: ['NARODZENIE JANA CHRZCICIELA'],
+    passages: ['Iz 49, 1-6', 'Ps 139', 'Dz 13, 22-26', 'Łk 1, 57-66. 80'],
+  },
+  {
+    date: '2025-06-25',
+    week: 2,
+    season: 'ordinary',
+    notes: ['[HTML]Doroty z Mątowów[HTML] rekl.'],
+    passages: ['Rdz 15, 1-12, 17-18', 'Ps 105', 'Mt 7, 15-20'],
+  },
+  {
+    date: '2025-06-26',
+    week: 2,
+    season: 'ordinary',
+    passages: ['Rdz 16, 1-12, 15-16', 'Ps 106', 'Mt 7, 21-29'],
+  },
+  {
+    date: '2025-06-27',
+    week: 2,
+    season: 'ordinary',
+    notes: ['[HTML]Cyryla z Aleksandrii[HTML] bp.'],
+    passages: ['Rdz 17, 1, 9-10, 15-22', 'Ps 128', 'Mt 8, 1-4'],
+  },
+  {
+    date: '2025-06-28',
+    week: 2,
+    season: 'ordinary',
+    notes: ['[HTML]Ireneusza[HTML] bp. m.'],
+    passages: ['Rdz 18, 1-15', 'Łk 1', 'Mt 8, 5-17'],
+  },
+  {
+    date: '2025-06-29',
+    week: 1,
+    season: 'ordinary',
+    holidays: ['PIOTRA I PAWŁA APOSTOŁÓW'],
+    passages: ['Dz 12, 1-11', 'Ps 34', '2 Tm 4, 6-9. 17-18', 'Mt 16, 13-19'],
+  },
+  {
+    date: '2025-06-30',
+    week: 1,
+    season: 'ordinary',
+    notes: ['[HTML]Ottona z Bambergu[HTML] bp.'],
+    passages: ['Rdz 18, 16-33', 'Ps 103', 'Mt 8, 18-22'],
   },
 ];
--- src/calendar/2025/index.ts
+++ src/calendar/2025/index.ts
@@ -4,6 +4,12 @@ import { March2025 } from './2025-03';
 import { April2025 } from './2025-04';
 import { May2025 } from './2025-05';
 import { June2025 } from './2025-06';
+import { July2025 } from './2025-07';
+import { August2025 } from './2025-08';
+import { September2025 } from './2025-09';
+import { October2025 } from './2025-10';
+import { November2025 } from './2025-11';
+import { December2025 } from './2025-12';
 
 export const Calendar2025 = [
   January2025,
@@ -12,4 +18,10 @@ export const Calendar2025 = [
   April2025,
   May2025,
   June2025,
+  July2025,
+  August2025,
+  September2025,
+  October2025,
+  November2025,
+  December2025,
 ];
